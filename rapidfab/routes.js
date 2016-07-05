d64fbaaa577d67695cc727924c8ea603d3a2d90a
Joshua Cloward <Joshua@authentise.com>
2016-07-05 11:45:20 -0600

    Add route setup for navigating pages
    This allows us to correctly populate pages depending on the url the user
    has typed in.
    
    AE-2058 #time 45m

---
+++ rapidfab/routes.js
@@ -0,0 +1,62 @@
+import _     from 'lodash';
+import React from 'react';
+import PathToRegexp from 'path-to-regexp';
+
+import Index from 'rapidfab/components/index'
+import About from 'rapidfab/components/about'
+
+var Router = React.createClass({
+  routes: {
+    "/"             : Index,
+    "/about"        : About,
+  },
+  render: function() {
+    var toRender = null;
+    for(var path in this.routes) {
+      var element = this.routes[path];
+      var keys = [];
+      var pattern = PathToRegexp(path, keys);
+      var match = pattern.exec(this.props.hash);
+      if(match) {
+        if(!!toRender) {
+          console.warn("Matched more than one route. First route was", toRender.path, " this match is ", path);
+        }
+        var route = {};
+        for(var i = 0; i < keys.length; i++) {
+          let key = keys[i];
+          route[key.name] = match[i+1];
+        }
+        var props = _.assign({}, this.props, {route: route});
+        toRender = {
+          element : React.createElement(element, props),
+          path    : path
+        }
+      }
+    }
+    if(!toRender) {
+      return (
+        <div className="router">
+          <p>You seem to have reached a link that doesn't go anywhere. Maybe you want <a href="#/">to go back to the beginning?</a></p>
+        </div>
+      );
+    } else {
+      return (
+        <div className="router">
+          {toRender.element}
+        </div>
+      );
+    }
+  }
+});
+
+var Routes = React.createClass({
+  render: function() {
+    var hash = this.props.url.location.hash.substr(1);
+
+    return (
+      <Router hash={hash} {...this.props}/>
+    );
+  }
+});
+
+module.exports = Routes
