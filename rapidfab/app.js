5f8ff04e672ad900b2419984b501934c75485cbb
Joshua Cloward <Joshua@authentise.com>
2016-07-05 11:52:00 -0600

    Add routes to app and include new packages
    Now that we have the url/route setup all we need to do is integrate it
    with app.js
    
    AE-2058 #time 30m

--- rapidfab/app.js
+++ rapidfab/app.js
@@ -1,18 +1,34 @@
 import 'rapidfab/app.scss';
 
+import * as URL from 'rapidfab/actions/url';
+
 import React, { Component }     from "react";
 import ReactDOM                 from 'react-dom';
+
+import Routes             from 'rapidfab/routes';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      url: {
+        location: {
+          hash: window.location.hash
+        }
+      }
+    };
+  }
+
+  componentWillMount() {
+    window.onhashchange = event => {
+      if(!event) return;
+      URL.change(event.oldURL, event.newURL || window.location.hash);
+    }
   }
 
   render() {
     return (
-      <div>
-        App
-      </div>
+      <Routes {...this.state}/>
     );
   }
 }
