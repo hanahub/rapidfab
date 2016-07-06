4ee9542ac733df14cf6e83bf56eff4a6e5ade3f0
Joshua Cloward <Joshua@authentise.com>
2016-07-06 15:36:34 -0600

    Fix spacing
    AE-2060 #time 5m

--- rapidfab/components/index.js
+++ rapidfab/components/index.js
@@ -3,7 +3,7 @@ import React, { Component }     from "react";
 export default class Index extends Component {
   render() {
     return (
-        <div> Index </div>
+      <div> Index </div>
     );
   }
 }
