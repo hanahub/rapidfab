423d502f8a44b27e6b8aa9eb66e32fd3d25e8dae
Joshua Cloward <Joshua@authentise.com>
2016-07-14 15:15:06 -0600

    Add correct materials page to routes
    This will now display the correct page we want to see.
    
    AE-1945 #time 5m

--- rapidfab/routes.js
+++ rapidfab/routes.js
@@ -3,13 +3,12 @@ import About                  from 'rapidfab/components/about';
 
 import InventoryLocation      from 'rapidfab/components/location'
 import InventoryManufacturer  from 'rapidfab/components/inventory/manufacturer';
-import InventoryMaterial      from 'rapidfab/components/material';
+import InventoryMaterial      from 'rapidfab/components/inventory/material';
 import InventoryUser          from 'rapidfab/components/inventory/user'
 
 const Routes = {
   "/"                        : Index,
   "/about"                   : About,
-
   "/inventory/material"      : InventoryMaterial,
   "/inventory/location"      : InventoryLocation,
   "/inventory/manufacturer"  : InventoryManufacturer,
