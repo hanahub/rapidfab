a28020468cbdf60fd9e8c8b6282cec494d6c22b3
Joshua Cloward <Joshua@authentise.com>
2016-07-14 15:14:42 -0600

    Add fakedata for materials page
    This allows us to fill up the materials page with some example / fake
    data.
    
    AE-1945 #time 15m

--- rapidfab/fakeData/index.js
+++ rapidfab/fakeData/index.js
@@ -1,7 +1,9 @@
 import ManufacturesData from './manufacturers';
+import MaterialsData from './materials';
 import UsersData from './users';
 
 export default {
-  manufacturers: ManufacturesData,
-  users: UsersData
+  manufacturers : ManufacturesData,
+  users         : UsersData,
+  materials     : MaterialsData
 }
