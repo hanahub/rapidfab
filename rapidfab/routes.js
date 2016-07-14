518b8ed6dcb3a44873f0364dbae4a8336b6a5e9b
Joshua Cloward <Joshua@authentise.com>
2016-07-14 16:08:05 -0600

    Add model_resource to routes
    This allows us to go to the model_resource page that we've created
    
    AE-1945 #time 1m

--- rapidfab/routes.js
+++ rapidfab/routes.js
@@ -1,18 +1,20 @@
-import Index                  from 'rapidfab/components/index';
-import About                  from 'rapidfab/components/about';
+import Index                      from 'rapidfab/components/index';
+import About                      from 'rapidfab/components/about';
 
-import InventoryLocation      from 'rapidfab/components/location'
-import InventoryManufacturer  from 'rapidfab/components/inventory/manufacturer';
-import InventoryMaterial      from 'rapidfab/components/inventory/material';
-import InventoryUser          from 'rapidfab/components/inventory/user'
+import InventoryLocation          from 'rapidfab/components/location'
+import InventoryManufacturer      from 'rapidfab/components/inventory/manufacturer';
+import InventoryMaterial          from 'rapidfab/components/inventory/material';
+import InventoryMaterialResource  from 'rapidfab/components/inventory/material_resource';
+import InventoryUser              from 'rapidfab/components/inventory/user'
 
 const Routes = {
-  "/"                        : Index,
-  "/about"                   : About,
-  "/inventory/material"      : InventoryMaterial,
-  "/inventory/location"      : InventoryLocation,
-  "/inventory/manufacturer"  : InventoryManufacturer,
-  "/inventory/user"          : InventoryUser
+  "/"                             : Index,
+  "/about"                        : About,
+  "/inventory/material"           : InventoryMaterial,
+  "/inventory/material-resource"  : InventoryMaterialResource,
+  "/inventory/location"           : InventoryLocation,
+  "/inventory/manufacturer"       : InventoryManufacturer,
+  "/inventory/user"               : InventoryUser
 }
 
 export default Routes
