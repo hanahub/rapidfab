07544df3705d6385ac16b9ca855e1349f0531c58
Joshua Cloward <Joshua@authentise.com>
2016-07-20 10:51:59 -0600

    Update location page to match rest of project
    Location page was never updated this updates it to have the same nice
    setup as other pages
    
    AE-2082 #time 1h

--- rapidfab/routes.js
+++ rapidfab/routes.js
@@ -1,7 +1,7 @@
 import Home                         from 'rapidfab/components/home';
 import About                        from 'rapidfab/components/about';
 
-import InventoryLocation            from 'rapidfab/components/location'
+import InventoryLocation            from 'rapidfab/components/inventory/location';
 import InventoryManufacturer        from 'rapidfab/components/inventory/manufacturer';
 import InventoryMaterial            from 'rapidfab/components/inventory/material';
 import InventoryMaterialResource    from 'rapidfab/components/inventory/material_resource';
