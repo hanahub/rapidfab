fddfba6df48882f24f7529805c2c389ccef1ccc3
Nick Cloward <nick@authentise.com>
2016-07-21 10:13:05 -0600

    Add orders and order routes
    AE-1954 #time 10m
    
    This adds the routes for order records and order lists.

--- rapidfab/routes.js
+++ rapidfab/routes.js
@@ -1,13 +1,18 @@
-import Home                         from 'rapidfab/components/home';
-import About                        from 'rapidfab/components/about';
+import Home                         from 'rapidfab/components/home'
+import About                        from 'rapidfab/components/about'
 
-import InventoryLocation            from 'rapidfab/components/inventory/location';
-import InventoryManufacturer        from 'rapidfab/components/inventory/manufacturer';
-import InventoryMaterial            from 'rapidfab/components/inventory/material';
-import InventoryMaterialResource    from 'rapidfab/components/inventory/material_resource';
-import InventoryPostProcessing      from 'rapidfab/components/inventory/post_processing';
-import InventoryThirdPartyProvider  from 'rapidfab/components/inventory/third_party_provider';
-import InventoryUser                from 'rapidfab/components/inventory/user';
+import InventoryLocation            from 'rapidfab/components/inventory/location'
+import InventoryManufacturer        from 'rapidfab/components/inventory/manufacturer'
+import InventoryMaterial            from 'rapidfab/components/inventory/material'
+import InventoryMaterialResource    from 'rapidfab/components/inventory/material_resource'
+import InventoryPostProcessing      from 'rapidfab/components/inventory/post_processing'
+import InventoryThirdPartyProvider  from 'rapidfab/components/inventory/third_party_provider'
+import InventoryUser                from 'rapidfab/components/inventory/user'
+
+import PlanOrders                   from 'rapidfab/containers/plan/orders'
+
+import Order                        from 'rapidfab/containers/records/order'
+>>>>>>> Add orders and order routes
 
 const Routes = {
   "/"                               : Home,
@@ -20,6 +25,11 @@ const Routes = {
   "/inventory/post-processing"      : InventoryPostProcessing,
   "/inventory/third-party-provider" : InventoryThirdPartyProvider,
   "/inventory/user"                 : InventoryUser,
+
+  "/plan/orders"                    : PlanOrders,
+
+  "/records/order"                  : Order,
+  "/records/order/:uuid"            : Order
 }
 
 export default Routes
