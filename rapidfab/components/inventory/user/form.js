d5094c01761a2212cfe0de4b7927d2d30b95865f
Joshua Cloward <Joshua@authentise.com>
2016-07-21 13:59:50 -0600

    Allow user form/grid to switch texts
    This allows us to change text easily between english and japanese
    text on the user page
    
    AE-2084 #time 30m

--- rapidfab/components/inventory/user/form.js
+++ rapidfab/components/inventory/user/form.js
@@ -1,23 +1,24 @@
 import React, { PropTypes }     from "react";
 import * as BS                  from 'react-bootstrap';
+import { FormattedMessage }     from 'react-intl';
 
 const ManufacturersForm = ({  }) => (
   <BS.Row>
     <BS.Col xs={12}>
       <BS.FormGroup controlId="uxName">
-        <BS.ControlLabel>Name:</BS.ControlLabel>
+        <BS.ControlLabel><FormattedMessage id="field.name" defaultMessage='Name'/>:</BS.ControlLabel>
         <BS.FormControl name="name" componentClass="text"/>
       </BS.FormGroup>
       <BS.FormGroup controlId="uxEmail">
-        <BS.ControlLabel>Email:</BS.ControlLabel>
+        <BS.ControlLabel><FormattedMessage id="field.email" defaultMessage='Email'/>:</BS.ControlLabel>
         <BS.FormControl name="email" componentClass="email"/>
       </BS.FormGroup>
       <BS.FormGroup controlId="uxLocation">
-        <BS.ControlLabel>Location:</BS.ControlLabel>
+        <BS.ControlLabel><FormattedMessage id="field.location" defaultMessage='Location'/>:</BS.ControlLabel>
         <BS.FormControl name="location" componentClass="text"/>
       </BS.FormGroup>
       <BS.Checkbox name="locationAccess">
-        Access to all locations
+        <FormattedMessage id="field.accessToAllLocations" defaultMessage='Access to All Locations'/>:
       </BS.Checkbox>
     </BS.Col>
   </BS.Row>
