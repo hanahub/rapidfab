170ddfe2f769a8b50e28694a9ff842f176c7587e
Joshua Cloward <Joshua@authentise.com>
2016-07-21 15:28:31 -0600

    Allow location form/grid to switch texts
    This allows us to change text easily between english and japanese
    text on the location page
    
    AE-2084 #time 30m

--- rapidfab/components/inventory/location/form.js
+++ rapidfab/components/inventory/location/form.js
@@ -1,32 +1,42 @@
 import React, { PropTypes }     from "react";
 import * as BS                  from 'react-bootstrap';
+import { FormattedMessage }     from 'react-intl';
+
 
 const Form = ({  }) => (
   <BS.Row>
     <BS.Col xs={12}>
       <BS.FormGroup controlId="uxName">
-        <BS.ControlLabel>Name:</BS.ControlLabel>
+        <BS.ControlLabel>
+          <FormattedMessage id="field.name" defaultMessage='Name'/>:
+        </BS.ControlLabel>
         <BS.FormControl name="name" componentClass="text"/>
       </BS.FormGroup>
       <BS.FormGroup controlId="uxContact">
-        <BS.ControlLabel>Contact:</BS.ControlLabel>
+        <BS.ControlLabel>
+          <FormattedMessage id="field.contact" defaultMessage='Contact'/>:
+        </BS.ControlLabel>
         <BS.FormControl name="contact" componentClass="text"/>
       </BS.FormGroup>
       <BS.FormGroup controlId="uxPhone">
-        <BS.ControlLabel>Phone:</BS.ControlLabel>
+        <BS.ControlLabel>
+          <FormattedMessage id="field.phone" defaultMessage='Phone'/>:
+        </BS.ControlLabel>
         <BS.FormControl name="phone" componentClass="text"/>
       </BS.FormGroup>
       <BS.FormGroup controlId="uxAddress">
-        <BS.ControlLabel>Address:</BS.ControlLabel>
+        <BS.ControlLabel>
+          <FormattedMessage id="field.address" defaultMessage='Address'/>:
+        </BS.ControlLabel>
         <BS.FormControl name="address" componentClass="text"/>
       </BS.FormGroup>
       <BS.Table bordered hover>
         <thead>
           <tr>
             <th></th>
-            <th>Material</th>
-            <th>Type</th>
-            <th>Manufacturer</th>
+            <th><FormattedMessage id="field.material" defaultMessage='Material'/></th>
+            <th><FormattedMessage id="field.type" defaultMessage='Type'/></th>
+            <th><FormattedMessage id="field.manufacturer" defaultMessage='Manufacture'/></th>
           </tr>
         </thead>
         <tbody>
