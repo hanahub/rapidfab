dd48016f00a1e8802418d83b8907a5363ab53f0b
Nick Cloward <nick@authentise.com>
2016-07-27 15:27:09 -0600

    Cleanup routes for inventory manufacturer
    AE-2115 #time 10m
    
    Changes inventory/manufacturer to inventory/manufacturers because its
    a list of manufacturers.  Adds route for new upcoming
    record/manufacturers container and components.

--- rapidfab/routes.js
+++ rapidfab/routes.js
@@ -2,7 +2,7 @@ import Home                         from 'rapidfab/components/home'
 import About                        from 'rapidfab/components/about'
 
 import InventoryLocation            from 'rapidfab/components/inventory/location'
-import InventoryManufacturer        from 'rapidfab/components/inventory/manufacturer'
+import InventoryManufacturers       from 'rapidfab/containers/inventory/manufacturers'
 import InventoryMaterial            from 'rapidfab/components/inventory/material'
 import InventoryMaterialResource    from 'rapidfab/components/inventory/materialResource'
 import InventoryPostProcessing      from 'rapidfab/components/inventory/postProcessing'
@@ -12,6 +12,7 @@ import InventoryUser                from 'rapidfab/components/inventory/user'
 import PlanOrders                   from 'rapidfab/containers/plan/orders'
 
 import Order                        from 'rapidfab/containers/records/order'
+import Manufacturer                 from 'rapidfab/containers/records/manufacturer'
 
 const Routes = {
   "/"                               : Home,
@@ -20,7 +21,7 @@ const Routes = {
   "/inventory/material"             : InventoryMaterial,
   "/inventory/resource"             : InventoryMaterialResource,
   "/inventory/location"             : InventoryLocation,
-  "/inventory/manufacturer"         : InventoryManufacturer,
+  "/inventory/manufacturers"        : InventoryManufacturers,
   "/inventory/post-processing"      : InventoryPostProcessing,
   "/inventory/third-party-provider" : InventoryThirdPartyProvider,
   "/inventory/user"                 : InventoryUser,
@@ -28,7 +29,9 @@ const Routes = {
   "/plan/orders"                    : PlanOrders,
 
   "/records/order"                  : Order,
-  "/records/order/:uuid"            : Order
+  "/records/order/:uuid"            : Order,
+  "/records/manufacturer"           : Manufacturer,
+  "/records/manufacturer/:uuid"     : Manufacturer
 }
 
 export default Routes
