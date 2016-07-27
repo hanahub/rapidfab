826c4e944b0d208f9ddd0df4859c028a3030d317
Nick Cloward <nick@authentise.com>
2016-07-27 15:11:36 -0600

    Add API hosts config
    AE-2115 #time 4m
    
    Adds a config section so the API generation can generate the correct
    urls for the endpoints.

--- rapidfab/config.js
+++ rapidfab/config.js
@@ -1,22 +1,34 @@
 switch(process.env.NODE_ENV) {
   case "production":
     module.exports = {
-      EVENT_HOST        : 'https://events.authentise.com/',
-      PAO_HOST          : 'https://users.authentise.com',
       SENTRY_DSN        : 'https://5dec153715544b259ba254582e1f9fa2@sentry.authentise.com/24',
+      HOST              : {
+        PAO             : 'https://users.authentise.com',
+        WYATT           : 'https://erp.authentise.com',
+        EVENT           : 'https://events.authentise.com/',
+        HOTH            : 'https://models.authentise.com/',
+      },
     };
     break;
   case "development":
     module.exports = {
-      EVENT_HOST        : 'https://events.dev-auth.com/',
-      PAO_HOST          : 'https://users.dev-auth.com',
       SENTRY_DSN        : 'https://3d4d1e8404ca40e4abda58d1a090d5b7@sentry.authentise.com/12',
+      HOST              : {
+        PAO             : 'https://users.dev-auth.com',
+        WYATT           : 'https://erp.dev-auth.com',
+        EVENT           : 'https://events.dev-auth.com/',
+        HOTH            : 'https://models.dev-auth.com/',
+      },
     };
     break;
   default:
     module.exports = {
-      EVENT_HOST        : 'https://events.dev-auth.com/',
-      PAO_HOST          : 'https://users.dev-auth.com',
       SENTRY_DSN        : null,
+      HOST              : {
+        PAO             : 'https://users.dev-auth.com',
+        WYATT           : 'https://erp.dev-auth.com',
+        EVENT           : 'https://events.dev-auth.com/',
+        HOTH            : 'https://models.dev-auth.com/',
+      },
     };
 }
