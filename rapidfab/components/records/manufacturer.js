3502225075029ef27d07cbecad9acbbc3f648d27
Nick Cloward <nick@authentise.com>
2016-08-02 13:15:37 -0600

    Implement manufacturers form
    AE-2115 #time 4h
    
    This allows the form to function as a form.  It will save, update, and
    delete manufacturer records.

--- rapidfab/components/records/manufacturer.js
+++ rapidfab/components/records/manufacturer.js
@@ -2,85 +2,86 @@ import React, { PropTypes }     from "react";
 import * as BS                  from 'react-bootstrap';
 import Fa                       from 'react-fontawesome';
 import { FormattedMessage }     from 'react-intl';
-import { reduxForm }            from 'redux-form';
 
-const SaveButtonTitle = ({ uri, uuid, record }) => (
+
+const SaveButtonTitle = ({  }) => (
   <span>
     <Fa name='floppy-o'/> <FormattedMessage id="button.save" defaultMessage='Save'/>
   </span>
 )
 
-const ManufacturerForm = ({ uuid, record, fetching, errors, onSave, onDelete }) => (
-  <BS.Grid>
+const ManufacturerForm = ({ fields, handleSubmit, load, submitting, onDelete }) => (
+  <form onSubmit={handleSubmit}>
+    <BS.Grid>
+      <BS.Row>
+        <BS.Col xs={12}>
+          <BS.Breadcrumb>
+            <BS.Breadcrumb.Item href="#/inventory">
+              <Fa name='list'/> <FormattedMessage id="inventory" defaultMessage='Inventory'/>
+            </BS.Breadcrumb.Item>
+            <BS.Breadcrumb.Item href="#/inventory/manufacturer">
+              <Fa name='industry'/> <FormattedMessage id="inventory.manufacturers" defaultMessage='Manufacturers'/>
+            </BS.Breadcrumb.Item>
+            <BS.Breadcrumb.Item>
+              <Fa name='industry'/> {fields.id.value || <FormattedMessage id="record.newManufacturer" defaultMessage='New Manufacturer'/>}
+            </BS.Breadcrumb.Item>
+          </BS.Breadcrumb>
+        </BS.Col>
+      </BS.Row>
 
-    <BS.Row>
-      <BS.Col xs={12}>
-        <BS.Breadcrumb>
-          <BS.Breadcrumb.Item href="#/inventory">
-            <Fa name='list'/> <FormattedMessage id="inventory" defaultMessage='Inventory'/>
-          </BS.Breadcrumb.Item>
-          <BS.Breadcrumb.Item href="#/inventory/manufacturer">
-            <Fa name='industry'/> <FormattedMessage id="inventory.manufacturers" defaultMessage='Manufacturers'/>
-          </BS.Breadcrumb.Item>
-          <BS.Breadcrumb.Item>
-            <Fa name='industry'/> {uuid || <FormattedMessage id="record.newManufacturer" defaultMessage='New Manufacturer'/>}
-          </BS.Breadcrumb.Item>
-        </BS.Breadcrumb>
-      </BS.Col>
-    </BS.Row>
+      <BS.Row>
+        <BS.Col xs={6}>
+          <BS.Button href="#/inventory/manufacturers" bsSize="small">
+            <Fa name='arrow-left'/> <FormattedMessage id="inventory.manufacturers" defaultMessage='Manufacturers'/>
+          </BS.Button>
+        </BS.Col>
+        <BS.Col xs={6}>
+          <BS.ButtonToolbar className="pull-right">
+            <BS.SplitButton id="uxSaveDropdown" type="submit" bsStyle="success" bsSize="small" title={<SaveButtonTitle />} pullRight>
+              <BS.MenuItem eventKey={1} onClick={() => onDelete(fields.uuid.value)} disabled={!fields.id.value}>
+                <Fa name='ban'/> <FormattedMessage id="button.delete" defaultMessage='Delete'/>
+              </BS.MenuItem>
+            </BS.SplitButton>
+          </BS.ButtonToolbar>
+        </BS.Col>
+      </BS.Row>
 
-    <BS.Row>
-      <BS.Col xs={6}>
-        <BS.Button href="#/inventory/manufacturers" bsSize="small">
-          <Fa name='arrow-left'/> <FormattedMessage id="inventory.manufacturers" defaultMessage='Manufacturers'/>
-        </BS.Button>
-      </BS.Col>
-      <BS.Col xs={6}>
-        <BS.ButtonToolbar className="pull-right">
-          <BS.SplitButton id="uxSaveDropdown" bsStyle="success" bsSize="small" title={<SaveButtonTitle />} pullRight>
-            <BS.MenuItem eventKey={1}>
-              <Fa name='ban'/> <FormattedMessage id="button.delete" defaultMessage='Delete'/>
-            </BS.MenuItem>
-          </BS.SplitButton>
-        </BS.ButtonToolbar>
-      </BS.Col>
-    </BS.Row>
+      <hr/>
 
-    <hr/>
-
-    <BS.Row>
-      <BS.Col xs={12}>
-        <BS.FormGroup controlId="uxName">
-          <BS.ControlLabel>Name:</BS.ControlLabel>
-          <BS.FormControl name="name" componentClass="text"/>
-        </BS.FormGroup>
-        <BS.FormGroup controlId="uxCommercialContact">
-          <BS.ControlLabel>Commercial Contact:</BS.ControlLabel>
-          <BS.FormControl name="commercialContact" componentClass="text"/>
-        </BS.FormGroup>
-        <BS.FormGroup controlId="uxCommercialPhone">
-          <BS.ControlLabel>Commercial Phone:</BS.ControlLabel>
-          <BS.FormControl name="commercialPhone" componentClass="tel"/>
-        </BS.FormGroup>
-        <BS.FormGroup controlId="uxSupportContact">
-          <BS.ControlLabel>Support Contact:</BS.ControlLabel>
-          <BS.FormControl name="supportContact" componentClass="text"/>
-        </BS.FormGroup>
-        <BS.FormGroup controlId="uxSupportPhone">
-          <BS.ControlLabel>Support Phone:</BS.ControlLabel>
-          <BS.FormControl name="supportPhone" componentClass="tel"/>
-        </BS.FormGroup>
-        <BS.FormGroup controlId="uxAddress">
-          <BS.ControlLabel>Address:</BS.ControlLabel>
-          <BS.FormControl name="address" componentClass="textarea"/>
-        </BS.FormGroup>
-        <BS.FormGroup controlId="uxNotes">
-          <BS.ControlLabel>Notes:</BS.ControlLabel>
-          <BS.FormControl name="notes" componentClass="textarea"/>
-        </BS.FormGroup>
-      </BS.Col>
-    </BS.Row>
-  </BS.Grid>
+      <BS.Row>
+        <BS.Col xs={12}>
+          <BS.FormGroup controlId="uxName">
+            <BS.ControlLabel>Name:</BS.ControlLabel>
+            <BS.FormControl name="name" type="text" required {...fields.name}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxCommercialContact">
+            <BS.ControlLabel>Commercial Contact:</BS.ControlLabel>
+            <BS.FormControl name="commercialContact" type="text" required {...fields.contact.name}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxCommercialPhone">
+            <BS.ControlLabel>Commercial Phone:</BS.ControlLabel>
+            <BS.FormControl name="commercialPhone" type="tel" required {...fields.contact.phone}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxSupportContact">
+            <BS.ControlLabel>Support Contact:</BS.ControlLabel>
+            <BS.FormControl name="supportContact" type="text" required {...fields.support.name}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxSupportPhone">
+            <BS.ControlLabel>Support Phone:</BS.ControlLabel>
+            <BS.FormControl name="supportPhone" type="tel" required {...fields.support.phone}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxAddress">
+            <BS.ControlLabel>Address:</BS.ControlLabel>
+            <BS.FormControl name="address" type="text" componentClass="textarea" required {...fields.address}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxNotes">
+            <BS.ControlLabel>Notes:</BS.ControlLabel>
+            <BS.FormControl name="notes" type="text" componentClass="textarea" required {...fields.notes}/>
+          </BS.FormGroup>
+        </BS.Col>
+      </BS.Row>
+    </BS.Grid>
+  </form>
 )
 
 export default ManufacturerForm
