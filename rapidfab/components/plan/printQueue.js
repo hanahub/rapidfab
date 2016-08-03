5d2ef22bfce637c1024cd9a6300d09e02a1131e2
Joshua Cloward <Joshua@authentise.com>
2016-08-02 22:21:49 -0600

    Add the ability to add jobs to an order
    This allows the user to add a job from the job list to an order. Also
    incorporate the reset button to reset the slate of the order.
    
    AE-2130 #time 4h

--- rapidfab/components/plan/printQueue.js
+++ rapidfab/components/plan/printQueue.js
@@ -1,4 +1,5 @@
-import React, { PropTypes }                   from "react";
+import React, { PropTypes, Component }        from "react";
+import _                                      from "lodash";
 import * as BS                                from 'react-bootstrap';
 import Fa                                     from 'react-fontawesome';
 import { FormattedMessage, FormattedDate }                   from 'react-intl';
@@ -10,105 +11,89 @@ import Grid, {
   VolumeColumn
 } from 'rapidfab/components/grid';
 
+class EditColumn extends Component {
+    constructor(props) {
+      super(props);
+      this.handleEdit = this.handleEdit.bind(this);
+    }
+    handleEdit(e) {
+      this.props.metadata.onEdit(e, this.props.data, this.props.rowData);
+    }
+    render() {
+      return (
+        <BS.Button bsStyle="success" bsSize="small" onClick={this.handleEdit}>
+          <Fa name='arrow-right'/>
+        </BS.Button>
+      );
+    }
+}
 
-const IdColumn = ({ data, rowData }) => (
-  <a href={`#/records/order/${encodeURIComponent(rowData.uuid)}`}>
-    {data}
-  </a>
-)
+export default class PrintQueue extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {orderList : []};
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleOrderList = this.handleOrderList.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
 
-const PrintQueue = ({ records }) => (
-  <BS.Grid>
+  handleAdd(e, data, rowData) {
+    this.setState({
+      orderList: _.concat(this.state.orderList, [data])
+    });
+  }
 
-    <BS.Row>
-      <BS.Col xs={6}>
-        <BS.Breadcrumb>
-          <BS.Breadcrumb.Item href="#/plan">
-            <Fa name='road'/> <FormattedMessage id="plan" defaultMessage='Plan'/>
-          </BS.Breadcrumb.Item>
-          <BS.Breadcrumb.Item href="#/plan/print-queue">
-            <Fa name='files-o'/> <FormattedMessage id="plan.printQueue" defaultMessage='Print Queue'/>
-          </BS.Breadcrumb.Item>
-        </BS.Breadcrumb>
+  handleOrderList() {
+    return _.map(this.state.orderList, orderUuid => (
+      <BS.Col key={orderUuid} xs={4}>
+        {console.log(orderUuid)}
+        <span> Order: {this.props.records[orderUuid].order}</span>
+        <img src="https://placehold.it/125x75"/>
       </BS.Col>
-    </BS.Row>
+    ));
+  }
 
-    <BS.Row>
-      <BS.Col xs={6} xsOffset={6}>
-        <BS.ButtonToolbar className="pull-right">
-          <BS.Button bsSize="small" bsStyle="danger">
-            <FormattedMessage id="button.reset" defaultMessage='Reset'/>
-          </BS.Button>
-          <BS.Button bsSize="small" bsStyle="primary">
-            <FormattedMessage id="button.save" defaultMessage='Save'/>
-          </BS.Button>
-        </BS.ButtonToolbar>
-      </BS.Col>
-    </BS.Row>
+  handleReset() {
+    this.setState({
+      orderList: []
+    });
+  }
 
-    <hr/>
+  render() {
+    let records = _.omit(this.props.records, this.state.orderList)
+    let fromDate = new Date();
+    fromDate.setDate(fromDate.getDate() - 7);
+    let toDate = new Date();
+    return (
+      <BS.Grid>
+        <BS.Row>
+          <BS.Col xs={6}>
+            <BS.Breadcrumb>
+              <BS.Breadcrumb.Item href="#/plan">
+                <Fa name='road'/> <FormattedMessage id="plan" defaultMessage='Plan'/>
+              </BS.Breadcrumb.Item>
+              <BS.Breadcrumb.Item href="#/plan/print-queue">
+                <Fa name='files-o'/> <FormattedMessage id="plan.printQueue" defaultMessage='Print Queue'/>
+              </BS.Breadcrumb.Item>
+            </BS.Breadcrumb>
+          </BS.Col>
+        </BS.Row>
 
-    <BS.Row>
-      <BS.Col xs={5} xsOffset={1}>
-        <form className="form-inline date-range">
-          <BS.Input type="date"/>
-          -
-          <BS.Input type="date"/>
-        </form>
-      </BS.Col>
-      <BS.Col xs={3}>
-        <BS.FormGroup controlId="uxPrinter">
-          <BS.FormControl componentClass="select" placeholder="select">
-            <option value="select">Printer</option>
-            <option value="other">...</option>
-          </BS.FormControl>
-        </BS.FormGroup>
-      </BS.Col>
-      <BS.Col xs={2} xsOffset={1}>
-        <h4> 30% </h4>
-      </BS.Col>
-    </BS.Row>
+        <BS.Row>
+          <BS.Col xs={6} xsOffset={6}>
+            <BS.ButtonToolbar className="pull-right">
+              <BS.Button bsSize="small" bsStyle="danger" onClick={this.handleReset}>
+                <FormattedMessage id="button.reset" defaultMessage='Reset'/>
+              </BS.Button>
+              <BS.Button bsSize="small" bsStyle="primary">
+                <FormattedMessage id="button.save" defaultMessage='Save'/>
+              </BS.Button>
+            </BS.ButtonToolbar>
+          </BS.Col>
+        </BS.Row>
 
-    <BS.Row>
-      <BS.Col xs={6}>
-        <Grid
-          data={records}
-          columns={[
-            "orderSnapshot",
-            "order",
-            "material",
-            "color",
-            "et",
-            "volume",
-          ]}
-          columnMeta={[{
-            customComponent: ImageColumn,
-            columnName: "orderSnapshot",
-            displayName: <FormattedMessage id="field.order" defaultMessage='Order'/>
-          }, {
-            customComponent: CapitalizeColumn,
-            columnName: "order",
-            displayName: <FormattedMessage id="field.order" defaultMessage='Order'/>
-          }, {
-            customComponent: CapitalizeColumn,
-            columnName: "material",
-            displayName: <FormattedMessage id="field.material" defaultMessage='Material'/>
-          }, {
-            customComponent: CapitalizeColumn,
-            columnName: "color",
-            displayName: <FormattedMessage id="field.color" defaultMessage='Color'/>
-          }, {
-            customComponent: DateColumn,
-            columnName: "et",
-            displayName: <FormattedMessage id="field.et" defaultMessage='ET'/>
-          }, {
-            customComponent: VolumeColumn,
-            columnName: "volume",
-            displayName: <FormattedMessage id="field.volume" defaultMessage='Volume'/>
-          }]}
-        />
-      </BS.Col>
-      <BS.Col xs={6}>
+        <hr/>
+
         <BS.Row>
           <BS.Col xs={5} xsOffset={1}>
             <form className="form-inline date-range">
@@ -125,14 +110,69 @@ const PrintQueue = ({ records }) => (
               </BS.FormControl>
             </BS.FormGroup>
           </BS.Col>
-          <BS.Col xs={4}>
-            <span> Order # 123 </span>
-            <img src="https://placekitten.com/125/75"/>
+          <BS.Col xs={2} xsOffset={1}>
+            <h4> {this.state.orderList.length * 10}% </h4>
           </BS.Col>
         </BS.Row>
-      </BS.Col>
-    </BS.Row>
-  </BS.Grid>
-);
+
+        <BS.Row>
+          <BS.Col xs={6}>
+            <Grid
+              data={_.values(records)}
+              columns={[
+                "orderSnapshot",
+                "order",
+                "material",
+                "color",
+                "et",
+                "volume",
+                "uuid",
+              ]}
+              columnMeta={[{
+                customComponent: ImageColumn,
+                columnName: "orderSnapshot",
+                displayName: <FormattedMessage id="field.order" defaultMessage='Order'/>
+              }, {
+                customComponent: CapitalizeColumn,
+                columnName: "order",
+                displayName: <FormattedMessage id="field.order" defaultMessage='Order'/>
+              }, {
+                customComponent: CapitalizeColumn,
+                columnName: "material",
+                displayName: <FormattedMessage id="field.material" defaultMessage='Material'/>
+              }, {
+                customComponent: CapitalizeColumn,
+                columnName: "color",
+                displayName: <FormattedMessage id="field.color" defaultMessage='Color'/>
+              }, {
+                customComponent: DateColumn,
+                columnName: "et",
+                displayName: <FormattedMessage id="field.et" defaultMessage='ET'/>
+              }, {
+                customComponent: VolumeColumn,
+                columnName: "volume",
+                displayName: <FormattedMessage id="field.volume" defaultMessage='Volume'/>
+              }, {
+                order: 999,
+                columnName: "uuid",
+                visible: true,
+                locked: true,
+                sortable: false,
+                onEdit: this.handleAdd,
+                displayName: "Add",
+                customComponent: EditColumn,
+              }]}
+            />
+          </BS.Col>
+          <BS.Col xs={6}>
+            <BS.Row>
+              {this.handleOrderList()}
+            </BS.Row>
+          </BS.Col>
+        </BS.Row>
+      </BS.Grid>
+    );
+  }
+}
 
 export default PrintQueue
