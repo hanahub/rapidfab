bc8d7396a4797fec3cb713bdeb6b8fc8013edea5
Joshua Cloward <Joshua@authentise.com>
2016-08-04 16:43:28 -0600

    Have error alert be full horizontal length of screen
    Since its going to be used more places we will just have it be the full
    screen
    
    AE-2091 #time 5m

--- rapidfab/components/error.js
+++ rapidfab/components/error.js
@@ -4,13 +4,9 @@ import * as BS                                from 'react-bootstrap';
 
 const Error = ({ errors }) => {
   let errorList = _.map(errors, error => (
-    <BS.Row>
-      <BS.Col xs={8} xsOffset={2}>
-        <BS.Alert bsStyle="danger" className='error-alert'>
-          <p>{error}</p>
-        </BS.Alert>
-      </BS.Col>
-    </BS.Row>
+    <BS.Alert bsStyle="danger" className='error-alert'>
+      <p>{error}</p>
+    </BS.Alert>
   ))
   return (
     <div>{errorList}</div>
