0c308c3a0577f51e0f50c62bfe79e141770c4bbe
Nick Cloward <nick@authentise.com>
2016-08-05 18:44:13 -0600

    Add material record form
    AE-2148 #time 2h

---
+++ rapidfab/components/records/material.js
@@ -0,0 +1,99 @@
+import React, { PropTypes }     from "react";
+import * as BS                  from 'react-bootstrap';
+import Fa                       from 'react-fontawesome';
+import { FormattedMessage }     from 'react-intl';
+
+
+const SaveButtonTitle = ({  }) => (
+  <span>
+    <Fa name='floppy-o'/> <FormattedMessage id="button.save" defaultMessage='Save'/>
+  </span>
+)
+
+const MaterialForm = ({ fields, handleSubmit, load, submitting, onDelete }) => (
+  <form onSubmit={handleSubmit}>
+    <BS.Grid>
+      <BS.Row>
+        <BS.Col xs={12}>
+          <BS.Breadcrumb>
+            <BS.Breadcrumb.Item href="#/inventory">
+              <Fa name='book'/> <FormattedMessage id="inventory" defaultMessage='Inventory'/>
+            </BS.Breadcrumb.Item>
+            <BS.Breadcrumb.Item href="#/inventory/materials">
+              <Fa name='object-group'/> <FormattedMessage id="inventory.materials" defaultMessage='Materials'/>
+            </BS.Breadcrumb.Item>
+            <BS.Breadcrumb.Item>
+              <Fa name='object-ungroup'/> {fields.id.value || <FormattedMessage id="record.newMaterial" defaultMessage='New Material'/>}
+            </BS.Breadcrumb.Item>
+          </BS.Breadcrumb>
+        </BS.Col>
+      </BS.Row>
+
+      <BS.Row>
+        <BS.Col xs={6}>
+          <BS.Button href="#/inventory/materials" bsSize="small">
+            <Fa name='arrow-left'/> <FormattedMessage id="inventory.materials" defaultMessage='Materials'/>
+          </BS.Button>
+        </BS.Col>
+        <BS.Col xs={6}>
+          <BS.ButtonToolbar className="pull-right">
+            <BS.SplitButton id="uxSaveDropdown" type="submit" bsStyle="success" bsSize="small" title={<SaveButtonTitle />} pullRight>
+              <BS.MenuItem eventKey={1} onClick={() => onDelete(fields.uuid.value)} disabled={!fields.id.value}>
+                <Fa name='ban'/> <FormattedMessage id="button.delete" defaultMessage='Delete'/>
+              </BS.MenuItem>
+            </BS.SplitButton>
+          </BS.ButtonToolbar>
+        </BS.Col>
+      </BS.Row>
+
+      <hr/>
+
+      <BS.Row>
+        <BS.Col xs={12}>
+          <BS.FormGroup controlId="uxName">
+            <BS.ControlLabel><FormattedMessage id="field.name" defaultMessage='Name'/>:</BS.ControlLabel>
+            <BS.FormControl type="text" required {...fields.name}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxDescription">
+            <BS.ControlLabel><FormattedMessage id="field.description" defaultMessage='Description'/>:</BS.ControlLabel>
+            <BS.FormControl componentClass="textarea" required {...fields.description}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxManufacturer">
+            <BS.ControlLabel><FormattedMessage id="field.manufacturer" defaultMessage='Manufacturer'/>:</BS.ControlLabel>
+            <BS.FormControl type="text" required {...fields.manufacturer}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxColor">
+            <BS.ControlLabel>Color:</BS.ControlLabel>
+            <BS.FormControl type="text" required {...fields.color}/>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxType">
+            <BS.ControlLabel><FormattedMessage id="field.materialType" defaultMessage='Material Type'/>:</BS.ControlLabel>
+            <BS.FormControl componentClass="select" required {...fields.type}>
+              <option value="base">Base</option>
+              <option value="support">Support</option>
+            </BS.FormControl>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxRetailPriceCurrency">
+            <BS.ControlLabel>Retail Price Currency:</BS.ControlLabel>
+            <BS.FormControl componentClass="select" required {...fields.retail_price.currency}>
+              <option value="USD">USD</option>
+            </BS.FormControl>
+          </BS.FormGroup>
+          <BS.FormGroup controlId="uxRetailPrice">
+            <BS.ControlLabel><FormattedMessage id="field.retailPrice" defaultMessage='Retail Price'/>:</BS.ControlLabel>
+            <BS.FormControl type="number" required {...fields.retail_price.amount}/>
+          </BS.FormGroup>
+          <BS.Checkbox {...fields.third_party_fulfillment}>
+            <FormattedMessage id="field.fullfilledByThirdParty" defaultMessage='Fullfilled by Third Party'/>
+          </BS.Checkbox>
+          <BS.FormGroup controlId="uxProcessingTime">
+            <BS.ControlLabel><FormattedMessage id="field.processingTime" defaultMessage='Processing Time'/>:</BS.ControlLabel>
+            <BS.FormControl type="number" required {...fields.post_processing_seconds}/>
+          </BS.FormGroup>
+        </BS.Col>
+      </BS.Row>
+    </BS.Grid>
+  </form>
+)
+
+export default MaterialForm
---
+++ rapidfab/containers/records/material.js
@@ -0,0 +1,72 @@
+import React, { Component, PropTypes }    from "react"
+import { connect }                        from 'react-redux'
+import _                                  from "lodash"
+import Actions                            from "rapidfab/actions"
+import MaterialComponent                  from 'rapidfab/components/records/material'
+import { reduxForm }                      from 'redux-form'
+
+const fields = [
+  'id',
+  'uri',
+  'uuid',
+  'name',
+  'description',
+  'color',
+  'type',
+  'manufacturer',
+  'retail_price.currency',
+  'retail_price.amount',
+  'third_party_fulfillment',
+  'post_processing_seconds'
+]
+
+class MaterialContainer extends Component {
+  componentWillMount() {
+    this.props.onInitialize(this.props.uuid)
+  }
+
+  render() {
+    return <MaterialComponent {...this.props}/>
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onInitialize: uuid => {
+      if(uuid) {
+        dispatch(Actions.Api.wyatt.material.get(uuid))
+      }
+    },
+    onSubmit: payload => {
+      if(payload.uuid) {
+        dispatch(Actions.Api.wyatt.material.put(payload.uuid, payload))
+        window.location.hash = "#/inventory/materials"
+      } else {
+        dispatch(Actions.Api.wyatt.material.post(payload))
+        window.location.hash = "#/inventory/materials"
+      }
+    },
+    onDelete: uuid => {
+      if(uuid) {
+        dispatch(Actions.Api.wyatt.material.delete(uuid))
+        window.location.hash = "#/inventory/materials"
+      }
+    }
+  }
+}
+
+function mapStateToProps(state, props) {
+  const {
+    material
+  } = state;
+
+  return {
+    uuid            : props.route.uuid,
+    initialValues   : material[props.route.uuid],
+  }
+}
+
+export default reduxForm({
+  form: 'record.material',
+  fields
+}, mapStateToProps, mapDispatchToProps)(MaterialContainer)
