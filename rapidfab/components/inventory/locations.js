c6561b9425baa4a336f8ed19c30ad23f49ee0a25
Joshua Cloward <Joshua@authentise.com>
2016-08-10 16:35:05 -0600

    Add locations page display
    This shows the list of the locations in a neat looking table. It also
    manages displaying the username of the locations contact.
    
    AE-2149 #time 2h

---
+++ rapidfab/components/inventory/locations.js
@@ -0,0 +1,90 @@
+import React, { PropTypes }   from "react";
+import * as BS                from 'react-bootstrap';
+import Fa                     from 'react-fontawesome';
+import { extractUuid }        from 'rapidfab/reducers/makeApiReducers'
+import { FormattedMessage }   from 'react-intl';
+import Grid, { IdColumn }     from 'rapidfab/components/grid';
+
+export const ContactColumn = ({ data, rowData, metadata }) => {
+    let uuid = extractUuid(data)
+    let record = metadata.records[uuid]
+    if(!record) return <Fa name="spinner" spin/>
+    return (
+      <span>{record.username}</span>
+    )
+}
+
+const LocationsGrid = ({ locations, users }) => (
+  <Grid
+    data={locations}
+    columns={[
+      "id",
+      "name",
+      "phone",
+      "address",
+      "contact",
+    ]}
+    columnMeta={[{
+      displayName: <FormattedMessage id="field.id" defaultMessage='Id'/>,
+      columnName: "id",
+      customComponent: IdColumn("location"),
+      locked: true
+    }, {
+      columnName: "name",
+      displayName: <FormattedMessage id="field.name" defaultMessage='Name'/>
+    }, {
+      columnName: "address",
+      displayName: <FormattedMessage id="field.address" defaultMessage='Address'/>
+    }, {
+      columnName: "phone",
+      displayName: <FormattedMessage id="field.phone" defaultMessage='Phone'/>
+    }, {
+      columnName: "contact",
+      displayName: <FormattedMessage id='field.contact' defaultMessage="contact"/>,
+      customComponent: ContactColumn,
+      records: users,
+    }]}
+  />
+)
+
+const Loading = () => (
+  <div style={{ textAlign: "center" }}>
+    <Fa name="spinner" spin size='2x' />
+  </div>
+)
+
+const Locations = ({ locations, users, fetching, errors }) => (
+  <BS.Grid>
+    <BS.Row>
+      <BS.Col xs={12}>
+        <BS.Breadcrumb>
+          <BS.Breadcrumb.Item href="#/inventory">
+            <Fa name='list'/> <FormattedMessage id="inventory" defaultMessage='Inventory'/>
+          </BS.Breadcrumb.Item>
+          <BS.Breadcrumb.Item href="#/inventory/location">
+            <Fa name='industry'/> <FormattedMessage id="inventory.locations" defaultMessage='poop'/>
+          </BS.Breadcrumb.Item>
+        </BS.Breadcrumb>
+      </BS.Col>
+    </BS.Row>
+
+    <BS.Row>
+      <BS.Col xs={12}>
+        <BS.Button bsStyle="primary" bsSize="small" href="#/records/location" className="pull-right">
+          <Fa name='plus'/> <FormattedMessage id="record.location.add" defaultMessage='Add Location'/>
+        </BS.Button>
+      </BS.Col>
+    </BS.Row>
+
+    <hr/>
+
+    <BS.Row>
+      <BS.Col xs={12}>
+        {fetching ? <Loading/> : <LocationsGrid locations={locations} users={users}/>}
+      </BS.Col>
+    </BS.Row>
+
+  </BS.Grid>
+);
+
+export default Locations
