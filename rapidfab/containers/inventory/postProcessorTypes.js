658b99c7886d7a72efbbbbc2df3ea49afc00ad4d
Joshua Cloward <Joshua@authentise.com>
2016-08-16 11:40:40 -0600

    Add container for handling data and requests for postProcessorTypes
    This handles sending the request out and esnuring the component has the
    necessary data from those.
    
    AE-2165 #time 2h

---
+++ rapidfab/containers/inventory/postProcessorTypes.js
@@ -0,0 +1,43 @@
+import _                        from "lodash"
+import React, { Component }     from "react"
+import Actions                  from 'rapidfab/actions'
+import { connect }              from 'react-redux'
+import PostProcessorTypesComponent  from 'rapidfab/components/inventory/postProcessorTypes'
+
+
+class PostProcessorTypesContainer extends Component {
+  componentWillMount() {
+    this.props.onInitialize()
+  }
+  render() {
+    return <PostProcessorTypesComponent {...this.props}/>
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onInitialize: () => {
+      dispatch(Actions.Api.wyatt.manufacturer.list())
+      dispatch(Actions.Api.wyatt.material.list())
+      dispatch(Actions.Api.wyatt['post-processor-type'].list())
+    }
+  }
+}
+
+function mapStateToProps(state) {
+  const postProcessorType = state['post-processor-type']
+  const {
+    manufacturer,
+    material,
+  } = state;
+
+  return {
+    manufacturers      : _.omit(manufacturer,     ['uxFetching', 'uxErrors']),
+    materials          : _.omit(material,          ['uxFetching', 'uxErrors']),
+    postProcessorTypes : _.omit(postProcessorType, ['uxFetching', 'uxErrors']),
+    fetching           : manufacturer.uxFetching || material.uxFetching || postProcessorType.uxFetching,
+    errors : manufacturer.uxErrors || material.uxErrors || postProcessorType.uxErrors,
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostProcessorTypesContainer)
