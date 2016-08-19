3ae0e043ae8aeee80000d2bd8d2d27b4a2e34895
Joshua Cloward <Joshua@authentise.com>
2016-08-19 10:23:07 -0600

    Add container for postProcessors page
    This handles sending requests and setting the data up for the display
    page.
    
    AE-2168 #time 2h

---
+++ rapidfab/containers/inventory/postProcessors.js
@@ -0,0 +1,43 @@
+import _                        from "lodash"
+import React, { Component }     from "react"
+import Actions                  from 'rapidfab/actions'
+import { connect }              from 'react-redux'
+import PostProcessorsComponent  from 'rapidfab/components/inventory/postProcessors'
+
+
+class PostProcessorsContainer extends Component {
+  componentDidMount() {
+    this.props.onInitialize()
+  }
+  render() {
+    return <PostProcessorsComponent {...this.props}/>
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onInitialize: () => {
+      dispatch(Actions.Api.wyatt['post-processor-type'].list())
+      dispatch(Actions.Api.wyatt.location.list())
+      dispatch(Actions.Api.wyatt['post-processor'].list())
+    }
+  }
+}
+
+function mapStateToProps(state) {
+  const postProcessor = state['post-processor']
+  const postProcessorType = state['post-processor-type']
+  const {
+    location,
+  } = state;
+
+  return {
+    postProcessors     : _.omit(postProcessor,     ['uxFetching', 'uxErrors']),
+    locations          : _.omit(location,          ['uxFetching', 'uxErrors']),
+    postProcessorTypes : _.omit(postProcessorType, ['uxFetching', 'uxErrors']),
+    fetching           : location.uxFetching || postProcessorType.uxFetching || postProcessor.uxFetching,
+    errors             : location.uxErrors || postProcessorType.uxErrors || postProcessor.uxErrors
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostProcessorsContainer)
