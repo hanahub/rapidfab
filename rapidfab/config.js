ecdf17460aaf7c1722091f0d60ee529a34013fb0
Joshua Cloward <Joshua@authentise.com>
2016-08-22 15:00:42 -0600

    Add container for adding/editing users on records
    This handles sending a specific get request and/or sending a post/put for
    updating users.
    
    We then send out a post to the membership endpoint to
    add the user we just created to a group setup in the Config.GROUP.
    
    Also we simply remove the user from the group instead of deleting the
    user completely on delete. Currently Permissions doesn't allow this to
    work but will be changed in the future.
    
    AE-2169 #time 4h

--- rapidfab/config.js
+++ rapidfab/config.js
@@ -3,6 +3,7 @@ switch(process.env.NODE_ENV) {
     module.exports = {
       SENTRY_DSN        : 'https://5dec153715544b259ba254582e1f9fa2@sentry.authentise.com/24',
       BUREAU            : 'https://erp.authentise.com/bureau/16473d32-54c3-43c3-b635-83c86e009f03/',
+      GROUP             : 'https://users.authentise.com/groups/932f1dfc-44c8-43b3-aa40-d0d4e7163707/',
       HOST              : {
         PAO             : 'https://users.authentise.com',
         WYATT           : 'https://erp.authentise.com',
@@ -16,6 +17,7 @@ switch(process.env.NODE_ENV) {
     module.exports = {
       SENTRY_DSN        : 'https://3d4d1e8404ca40e4abda58d1a090d5b7@sentry.authentise.com/12',
       BUREAU            : 'https://erp.dev-auth.com/bureau/3c5e5cf4-0805-4fd8-a17d-3b873cbcedf7/',
+      GROUP             : 'https://users.dev-auth.com/groups/9be439f9-b957-4004-9769-ee392c6a70ec/',
       HOST              : {
         PAO             : 'https://users.dev-auth.com',
         WYATT           : 'https://erp.dev-auth.com',
@@ -29,6 +31,7 @@ switch(process.env.NODE_ENV) {
     module.exports = {
       SENTRY_DSN        : null,
       BUREAU            : 'https://erp.dev-auth.com/bureau/3c5e5cf4-0805-4fd8-a17d-3b873cbcedf7/',
+      GROUP             : 'https://users.dev-auth.com/groups/9be439f9-b957-4004-9769-ee392c6a70ec/',
       HOST              : {
         PAO             : 'https://users.dev-auth.com',
         WYATT           : 'https://erp.dev-auth.com',
