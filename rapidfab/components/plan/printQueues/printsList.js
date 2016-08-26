537a0f6d37350985f27bd14aa5bcc253a4d03d18
Nick Cloward <nick@authentise.com>
2016-08-26 13:47:10 -0600

    Add printQueue component and container
    AE-2192 #time 4h
    
    This adds the structure for the printQueue component along with the
    printQueue container.

---
+++ rapidfab/components/plan/printQueues/printsList.js
@@ -0,0 +1,81 @@
+import React, { PropTypes }                   from "react"
+import _                                      from "lodash"
+import * as BS                                from 'react-bootstrap'
+import { FormattedMessage, FormattedDate }    from 'react-intl';
+
+const listBodyStyle = {
+  height: 500,
+  verflowY: "scroll",
+  overflowX: "hidden"
+}
+
+const PrintItem = ({ print, selected, onSelect }) => (
+  <BS.ListGroupItem>
+    <BS.Row>
+      <BS.Col xs={1}>
+        <input
+          type="checkbox"
+          onChange={() => onSelect(print)}
+          checked={selected && selected.uri === print.uri}
+        />
+      </BS.Col>
+      <BS.Col xs={3}>
+        <a href={`#/records/order/${print.order.uuid}`}>
+          {print.order.id}
+        </a>
+      </BS.Col>
+      <BS.Col xs={3}>
+        <a href={`#/records/material/${print.order.materials.base.uuid}`}>
+          {print.order.materials.base.name}
+        </a>
+      </BS.Col>
+      <BS.Col xs={3}>
+        <FormattedDate value={print.order.created}/>
+      </BS.Col>
+    </BS.Row>
+  </BS.ListGroupItem>
+)
+
+const PrintsList = ({ prints, selected, onSelect }) => (
+  <BS.Panel header="Pending Order Prints">
+    <BS.ListGroup fill>
+      <BS.ListGroupItem style={{ borderBottomWidth: 2 }} key="header">
+        <BS.Row>
+          <BS.Col xs={1}>
+          </BS.Col>
+          <BS.Col xs={3}>
+            Order
+          </BS.Col>
+          <BS.Col xs={3}>
+            Material
+          </BS.Col>
+          <BS.Col xs={3}>
+            Created
+          </BS.Col>
+        </BS.Row>
+      </BS.ListGroupItem>
+      <div style={listBodyStyle}>
+        {_.map(prints, print => (
+          <PrintItem
+            key={print.uuid}
+            selected={selected}
+            print={print}
+            onSelect={onSelect}
+          />
+        ))}
+      </div>
+    </BS.ListGroup>
+  </BS.Panel>
+)
+
+PrintsList.propTypes = {
+  prints: PropTypes.object.isRequired,
+  selected: PropTypes.object,
+  onSelect: PropTypes.func,
+}
+
+PrintsList.defaultProps = {
+  onSelect: () => true
+}
+
+export default PrintsList
