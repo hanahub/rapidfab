cf1dd3138010d066cd38ae7cb0629fa2e1216fa4
Nick Cloward <nick@authentise.com>
2016-09-07 14:02:48 -0600

    Add resource selectors for all resources
    AE-2214 #time 20m

--- rapidfab/selectors/index.js
+++ rapidfab/selectors/index.js
@@ -1,13 +1,19 @@
 import _                  from "lodash"
 import { createSelector } from 'reselect'
 
-const getStateResources   = state => state.resources
+const getStateResources           = state => state.resources
 
-const getStateSessions        = state => state.api.pao.sessions
-const getStateLocations       = state => state.api.wyatt.location
-const getStateMaterials       = state => state.api.wyatt.material
-const getStateManufacturers   = state => state.api.wyatt.manufacturer
-const getStateUsers           = state => state.api.pao.users
+const getStateModels              = state => state.api.hoth.model
+const getStateMemberships         = state => state.api.pao.memberships
+const getStateSessions            = state => state.api.pao.sessions
+const getStateUsers               = state => state.api.pao.users
+const getStateLocations           = state => state.api.wyatt.location
+const getStatePostProcessorTypes  = state => state.api.wyatt['post-processor-type']
+const getStatePostProcessors      = state => state.api.wyatt['post-processor']
+const getStateManufacturers       = state => state.api.wyatt.manufacturer
+const getStateMaterials           = state => state.api.wyatt.material
+const getStateStocks              = state => state.api.wyatt.stock
+const getStateOrders              = state => state.api.wyatt.order
 
 export const getSession = createSelector(
   [ getStateSessions, getStateResources ],
@@ -18,8 +24,18 @@ export const getSession = createSelector(
   }
 )
 
-export const getLocations = createSelector(
-  [ getStateLocations, getStateResources ],
+export const getModels = createSelector(
+  [ getStateModels, getStateResources ],
+  (uuids, resources) => _.map(uuids, uuid => resources[uuid])
+)
+
+export const getMemberships = createSelector(
+  [ getStateMemberships, getStateResources ],
+  (uuids, resources) => _.map(uuids, uuid => resources[uuid])
+)
+
+export const getSessions = createSelector(
+  [ getStateSessions, getStateResources ],
   (uuids, resources) => _.map(uuids, uuid => resources[uuid])
 )
 
@@ -28,12 +44,37 @@ export const getUsers = createSelector(
   (uuids, resources) => _.map(uuids, uuid => resources[uuid])
 )
 
-export const getMaterials = createSelector(
-  [ getStateMaterials, getStateResources ],
+export const getLocations = createSelector(
+  [ getStateLocations, getStateResources ],
+  (uuids, resources) => _.map(uuids, uuid => resources[uuid])
+)
+
+export const getPostProcessorTypes = createSelector(
+  [ getStatePostProcessorTypes, getStateResources ],
+  (uuids, resources) => _.map(uuids, uuid => resources[uuid])
+)
+
+export const getPostProcessors = createSelector(
+  [ getStatePostProcessors, getStateResources ],
   (uuids, resources) => _.map(uuids, uuid => resources[uuid])
 )
 
 export const getManufacturers = createSelector(
   [ getStateManufacturers, getStateResources ],
+  (uuids, resources) => _.map(uuids, uuid => resources[uuid])
+)
+
+export const getMaterials = createSelector(
+  [ getStateMaterials, getStateResources ],
+  (uuids, resources) => _.map(uuids, uuid => resources[uuid])
+)
+
+export const getStocks = createSelector(
+  [ getStateStocks, getStateResources ],
+  (uuids, resources) => _.map(uuids, uuid => resources[uuid])
+)
+
+export const getOrders = createSelector(
+  [ getStateOrders, getStateResources ],
   (uuids, resources) => _.map(uuids, uuid => resources[uuid])
 )
