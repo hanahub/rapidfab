1a63f9f97302a6c0c9ec0893feb8adf52a34e153
Nick Cloward <nick@authentise.com>
2016-09-11 13:22:05 -0600

    Add printer types admin page
    AE-2261 #time 1h
    
    Add a printer type admin page so we can manage printer types.

---
+++ rapidfab/containers/inventory/printerTypes.js
@@ -0,0 +1,42 @@
+import _                        from "lodash"
+import React, { Component }     from "react"
+import Actions                  from 'rapidfab/actions'
+import { connect }              from 'react-redux'
+import PrinterTypesComponent    from 'rapidfab/components/inventory/printerTypes'
+import * as Selectors           from 'rapidfab/selectors'
+
+
+class PrinterTypesContainer extends Component {
+  componentWillMount() {
+    this.props.onInitialize()
+  }
+
+  render() {
+    return <PrinterTypesComponent {...this.props}/>
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onInitialize: () => {
+      dispatch(Actions.Api.wyatt.manufacturer.list())
+      dispatch(Actions.Api.wyatt['printer-type'].list())
+    }
+  }
+}
+
+function mapStateToProps(state) {
+  const printerType = state.ui.wyatt['printer-type']
+  const {
+    manufacturer,
+  } = state.ui.wyatt
+
+  return {
+    manufacturers : Selectors.getManufacturers(state),
+    printerTypes  : Selectors.getPrinterTypes(state),
+    fetching      : manufacturer.list.fetching || printerType.list.fetching,
+    apiErrors     : _.concat(manufacturer.list.errors, printerType.list.errors)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PrinterTypesContainer)
---
+++ rapidfab/containers/records/printerType.js
@@ -0,0 +1,74 @@
+import React, { Component, PropTypes }    from "react"
+import { connect }                        from 'react-redux'
+import _                                  from "lodash"
+import Actions                            from "rapidfab/actions"
+import PrinterTypeComponent               from 'rapidfab/components/records/printerType'
+import { reduxForm }                      from 'redux-form'
+import * as Selectors                     from 'rapidfab/selectors'
+
+const fields = [
+  'id',
+  'uri',
+  'uuid',
+  'name',
+  'description',
+  'manufacturer',
+  'build_volume.x',
+  'build_volume.y',
+  'build_volume.z',
+  'materials'
+]
+
+class PrinterTypeContainer extends Component {
+  componentWillMount() {
+    this.props.onInitialize(this.props.uuid)
+  }
+
+  render() {
+    return <PrinterTypeComponent {...this.props}/>
+  }
+}
+
+function redirect() {
+  window.location.hash = "#/inventory/printer-types"
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onInitialize: uuid => {
+      dispatch(Actions.Api.wyatt.material.list())
+      dispatch(Actions.Api.wyatt.manufacturer.list())
+      if(uuid) {
+        dispatch(Actions.Api.wyatt['printer-type'].get(uuid))
+      }
+    },
+    onSubmit: payload => {
+      if(payload.uuid) {
+        dispatch(Actions.Api.wyatt['printer-type'].put(payload.uuid, payload)).then(redirect)
+      } else {
+        dispatch(Actions.Api.wyatt['printer-type'].post(payload)).then(redirect)
+      }
+    },
+    onDelete: uuid => {
+      if(uuid) {
+        dispatch(Actions.Api.wyatt['printer-type'].delete(uuid)).then(redirect)
+      }
+    }
+  }
+}
+
+function mapStateToProps(state, props) {
+  return {
+    uuid            : Selectors.getRoute(state, props).uuid,
+    initialValues   : Selectors.getRouteResource(state, props),
+    submitting      : Selectors.getResourceFetching(state, "wyatt.printer-type"),
+    apiErrors       : Selectors.getResourceErrors(state, "wyatt.printer-type"),
+    manufacturers   : Selectors.getManufacturers(state),
+    materials       : Selectors.getMaterials(state),
+  }
+}
+
+export default reduxForm({
+  form: 'record.printerType',
+  fields
+}, mapStateToProps, mapDispatchToProps)(PrinterTypeContainer)
