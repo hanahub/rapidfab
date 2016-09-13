87924321a829427b3b0f8ad34770ff84ba7bf125
Yani Iliev <yani@authentise.com>
2016-09-14 00:02:43 +0700

    Add shipping.name and tracking to new order
    I've reordered the fields a bit that make more sense when filling a new
    order. I've also introduced a new fields third party provider that is a
    list of all providers. I've made support material, shipping, and third
    party optional fields as they should be.
    
    AE-2239 #time 2h
    
    Signed-off-by: Yani Iliev <yani@authentise.com>

--- rapidfab/components/records/newOrder.js
+++ rapidfab/components/records/newOrder.js
@@ -29,13 +29,24 @@ const BreadCrumbs = ({ fields }) => (
   </BS.Row>
 )
 
-const NewOrderForm = ({ fields, materials, model }) => (
+const NewOrderForm = ({ fields, materials, model, providers }) => (
   <BS.Row>
     <BS.Col xs={12}>
       <BS.FormGroup controlId="uxName">
         <BS.ControlLabel><FormattedMessage id="field.name" defaultMessage='Name'/>:</BS.ControlLabel>
         <BS.FormControl type="text" required {...fields.name}/>
       </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxModel">
+        <BS.ControlLabel><FormattedMessage id="field.model" defaultMessage='Model'/>:</BS.ControlLabel>
+        <BS.FormControl type="file" {...fields.model} value={null} required />
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxQuantity">
+        <BS.ControlLabel><FormattedMessage id="field.quantity" defaultMessage='Quantity'/>:</BS.ControlLabel>
+        <BS.FormControl type="number" required {...fields.quantity}/>
+      </BS.FormGroup>
+
       <BS.FormGroup controlId="uxBaseMaterials">
         <BS.ControlLabel><FormattedMessage id="field.baseMaterials" defaultMessage='Base Materials'/>:</BS.ControlLabel>
         <BS.FormControl componentClass="select" required {...fields.materials.base}>
@@ -45,32 +56,47 @@ const NewOrderForm = ({ fields, materials, model }) => (
           ))}
         </BS.FormControl>
       </BS.FormGroup>
+
       <BS.FormGroup controlId="uxSupportMaterials">
         <BS.ControlLabel><FormattedMessage id="field.supportMaterials" defaultMessage='Support Materials'/>:</BS.ControlLabel>
-        <BS.FormControl componentClass="select" required {...fields.materials.support}>
+        <BS.FormControl componentClass="select" {...fields.materials.support}>
           <option key="placeholder" value="" selected disabled>Select a Support Material</option>
           {_.map(materials, material => (
             <option key={material.uri} value={material.uri}>{material.name}</option>
           ))}
         </BS.FormControl>
       </BS.FormGroup>
-      <BS.FormGroup controlId="uxAddress">
-        <BS.ControlLabel><FormattedMessage id="field.address" defaultMessage='Address'/>:</BS.ControlLabel>
-        <BS.FormControl type="text" required {...fields.shipping.address}/>
+
+      <BS.FormGroup controlId="uxShippingName">
+        <BS.ControlLabel><FormattedMessage id="field.shipping_name" defaultMessage='Shipping Name'/>:</BS.ControlLabel>
+        <BS.FormControl type="text" {...fields.shipping.name}/>
       </BS.FormGroup>
-      <BS.FormGroup controlId="uxQuantity">
-        <BS.ControlLabel><FormattedMessage id="field.quantity" defaultMessage='Quantity'/>:</BS.ControlLabel>
-        <BS.FormControl type="number" required {...fields.quantity}/>
+
+      <BS.FormGroup controlId="uxShippingAddress">
+        <BS.ControlLabel><FormattedMessage id="field.shipping_address" defaultMessage='Shipping Address'/>:</BS.ControlLabel>
+        <BS.FormControl type="text" {...fields.shipping.address}/>
       </BS.FormGroup>
-      <BS.FormGroup controlId="uxModel">
-        <BS.ControlLabel><FormattedMessage id="field.model" defaultMessage='Model'/>:</BS.ControlLabel>
-        <BS.FormControl type="file" {...fields.model} value={null} required />
+
+      <BS.FormGroup controlId="uxTrackingNumber">
+        <BS.ControlLabel><FormattedMessage id="field.tracking_number" defaultMessage='Tracking Number'/>:</BS.ControlLabel>
+        <BS.FormControl type="text" {...fields.shipping.tracking}/>
       </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxThirdPartyProvider">
+        <BS.ControlLabel><FormattedMessage id="field.third_party_provider" defaultMessage='Third Party Provider'/>:</BS.ControlLabel>
+        <BS.FormControl componentClass="select" {...fields.third_party_provider}>
+          <option key="placeholder" value="" selected disabled>Select a Third Party Provider</option>
+          {_.map(providers, provider => (
+            <option key={provider.uri} value={provider.uri}>{provider.name}</option>
+          ))}
+        </BS.FormControl>
+      </BS.FormGroup>
+
     </BS.Col>
   </BS.Row>
 )
 
-const NewOrder = ({ fields, handleSubmit, load, submitting, onDelete, materials, model, uploadModel }) => (
+const NewOrder = ({ fields, handleSubmit, load, submitting, onDelete, materials, model, uploadModel, providers }) => (
   <div>
     <BreadCrumbs fields={fields} />
     <form onSubmit={handleSubmit}>
@@ -94,7 +120,7 @@ const NewOrder = ({ fields, handleSubmit, load, submitting, onDelete, materials,
 
         { uploadModel.percent > 0 ?
         <BS.ProgressBar active now={uploadModel.percent} /> :
-        <NewOrderForm fields={fields} materials={materials} model={model} /> }
+        <NewOrderForm fields={fields} materials={materials} model={model} providers={providers} /> }
 
       </BS.Grid>
     </form>
