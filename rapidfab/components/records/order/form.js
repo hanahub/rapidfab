ce09d71e9fdfb6e337244bea58896fa46a1177c7
Eli Ribble <eli@authentise.com>
2016-09-15 07:51:45 -0600

    Factor out a common form row component
    Everything is using this pattern so if we factor it out we can reduce
    the overall code size and improve readability
    
    AE-2324 #time 10m

--- rapidfab/components/records/order/form.js
+++ rapidfab/components/records/order/form.js
@@ -12,127 +12,83 @@ import {
 } from 'rapidfab/components/formTools';
 
 
+const FormRow = ({controlId, id, defaultMessage, children}) => (
+  <BS.FormGroup>
+    <BS.Col xs={3} controlId={controlId}>
+      <BS.ControlLabel><FormattedMessage id={id} defaultMessage={defaultMessage}/>:</BS.ControlLabel>
+    </BS.Col>
+    <BS.Col xs={9}>
+      {children}
+    </BS.Col>
+  </BS.FormGroup>
+)
+
 const OrderForm = ({ handleSubmit, fields, materials, models, providers }) => (
   <div>
-    <BS.FormGroup>
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.id" defaultMessage='Id'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <BS.FormControl.Static>
-          {fields.id.value}
-        </BS.FormControl.Static>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxId" id="field.id" defaultMessage="ID">
+      <BS.FormControl.Static>
+        {fields.id.value}
+      </BS.FormControl.Static>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxName">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.name" defaultMessage='Name'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <BS.FormControl type="text" required {...fields.name}/>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxName" id="field.name" defaultMessage="Name">
+      <BS.FormControl type="text" required {...fields.name}/>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxModel">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.model" defaultMessage='Model'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <BS.FormControl componentClass="select" required {...fields.model}>
-          <option value="" disabled>Select a Model</option>
-          {_.map(models, model => (<option key={model.uri} value={model.uri}>{`${model.id} - ${model.name}`}</option>))}
-        </BS.FormControl>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxModel" id="field.model" defaultMessage="Model">
+      <BS.FormControl componentClass="select" required {...fields.model}>
+        <option value="" disabled>Select a Model</option>
+        {_.map(models, model => (<option key={model.uri} value={model.uri}>{`${model.id} - ${model.name}`}</option>))}
+      </BS.FormControl>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxQuantity">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.quantity" defaultMessage='Quantity'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <BS.FormControl type="number" required {...fields.quantity}/>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxQuantity" id="field.quantity" defaultMessage="Quantity">
+      <BS.FormControl type="number" required {...fields.quantity}/>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxBaseMaterial">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.baseMaterial" defaultMessage='Base Material'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <BS.FormControl componentClass="select" required {...fields.materials.base}>
-          <option value="" disabled>Select a Material</option>
-          {_.map(materials, material => (<option key={material.uri} value={material.uri}>{`${material.id} - ${material.name}`}</option>))}
-        </BS.FormControl>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxBaseMaterial" id="field.baseMaterial" defaultMessage="Base Material">
+      <BS.FormControl componentClass="select" required {...fields.materials.base}>
+        <option value="" disabled>Select a Material</option>
+        {_.map(materials, material => (<option key={material.uri} value={material.uri}>{`${material.id} - ${material.name}`}</option>))}
+      </BS.FormControl>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxSupportMaterial">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.supportMaterial" defaultMessage='Support Material'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <BS.FormControl componentClass="select" {...fields.materials.support}>
-          <option value="">None</option>
-          {_.map(materials, material => (<option key={material.uri} value={material.uri}>{`${material.id} - ${material.name}`}</option>))}
-        </BS.FormControl>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxSupportMaterial" id="field.supportMaterial" defaultMessage="Support Material">
+      <BS.FormControl componentClass="select" {...fields.materials.support}>
+        <option value="">None</option>
+        {_.map(materials, material => (<option key={material.uri} value={material.uri}>{`${material.id} - ${material.name}`}</option>))}
+      </BS.FormControl>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxShippingName">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.shippingName" defaultMessage='Shipping Name'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <FormControlTextCareful {...fields.shipping.name}/>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxShippingName" id="field.shippingName" defaultMessage="Shipping Name">
+      <FormControlTextCareful {...fields.shipping.name}/>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxShippingAddress">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.shippingAddress" defaultMessage='Shipping Address'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <FormControlTextArea {...fields.shipping.address}/>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxShippingAddress" id="field.shippingAddress" defaultMessage="Shipping Address">
+      <FormControlTextArea {...fields.shipping.address}/>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxTrackingNumber">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.trackingNumber" defaultMessage='Tracking Number'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <FormControlTextCareful {...fields.shipping.tracking}/>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxTrackingNumber" id="field.trackingNumber" defaultMessage="Tracking Number">
+      <FormControlTextCareful {...fields.shipping.tracking}/>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxThirdPartyProvider">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.thirdPartyProvider" defaultMessage='Third Party Provider'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <BS.FormControl componentClass="select" {...fields.third_party_provider}>
-          <option key="placeholder" value="" selected disabled>Select a Third Party Provider</option>
-          {_.map(providers, provider => (
-            <option key={provider.uri} value={provider.uri}>{provider.name}</option>
-          ))}
-        </BS.FormControl>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxThirdPartyProvider" id="field.thirdPartyProvider" defaultMessage="Third-Party Provider">
+      <BS.FormControl componentClass="select" {...fields.third_party_provider}>
+        <option key="placeholder" value="" selected disabled>Select a Third Party Provider</option>
+        {_.map(providers, provider => (
+          <option key={provider.uri} value={provider.uri}>{provider.name}</option>
+        ))}
+      </BS.FormControl>
+    </FormRow>
 
-    <BS.FormGroup controlId="uxCreated">
-      <BS.Col xs={3}>
-        <BS.ControlLabel><FormattedMessage id="field.created" defaultMessage='Created'/>:</BS.ControlLabel>
-      </BS.Col>
-      <BS.Col xs={9}>
-        <BS.FormControl.Static>
-          {fields.created.value ?
-            <FormattedDate value={fields.created.value}/> :
-              (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
-          }
-        </BS.FormControl.Static>
-      </BS.Col>
-    </BS.FormGroup>
+    <FormRow controlId="uxCreated" id="field.created" defaultMessage="Created">
+      <BS.FormControl.Static>
+        {fields.created.value ?
+          <FormattedDate value={fields.created.value}/> :
+            (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
+        }
+      </BS.FormControl.Static>
+    </FormRow>
   </div>
 )
 
