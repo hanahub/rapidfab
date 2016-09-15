06b600c61b18835e4dcf4c6b7f735ea3ec845c22
Eli Ribble <eli@authentise.com>
2016-09-15 14:51:24 -0600

    Redirect page no after model upload, but after successful creation
    Without this we always redirect away from the order creation form even
    if we didn't create the order, we just uploaded and created the model
    
    AE-2322 #time 30m

--- rapidfab/containers/records/newOrder.js
+++ rapidfab/containers/records/newOrder.js
@@ -56,7 +56,9 @@ function mapDispatchToProps(dispatch, props) {
       .then( args => {
         dispatch(Actions.UploadModel.upload(args.headers.uploadLocation, payload.model[0]))
         payload.model = args.headers.location;
-        dispatch(Actions.Api.wyatt.order.post(payload))
+        dispatch(Actions.Api.wyatt.order.post(payload)).then(() => {
+          window.location.hash = '#/plan/orders';
+        })
       })
     },
   }
@@ -69,11 +71,6 @@ function mapStateToProps(state, props) {
     model,
     order,
   } = state.ui.wyatt;
-
-  if(uploadModel.percent >= 100) {
-    uploadModel.percent = 0
-    window.location.hash = '#/plan/orders'
-  }
 
   const fetching =
     material.list.fetching ||
