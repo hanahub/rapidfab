7aecb9c68382d19f24ddcdf44990d800a3bd832d
Joeseph Rodrigues <dowhilegeek@gmail.com>
2016-10-17 15:08:18 -0600

    add raven support

--- rapidfab/app.js
+++ rapidfab/app.js
@@ -3,12 +3,18 @@ import 'font-awesome-webpack'
 import 'rapidfab/styles/main.less'
 import 'rapidfab/styles/flags.less'
 
+import Config             		from 'rapidfab/config'
+import Raven              		from 'raven-js';
 import React                            from "react"
 import ReactDOM                         from 'react-dom'
 import { Provider }                     from 'react-redux'
 import initializeStore                  from 'rapidfab/reducers/initializeStore'
 import AppContainer                     from 'rapidfab/containers/app'
 
+if(Config.SENTRY_DSN) {
+  Raven.config(Config.SENTRY_DSN, {fetchContext: true}).install();
+  console.log("Raven integration installed");
+}
 
 const App = ({ store }) => (
   <Provider store={store}>
