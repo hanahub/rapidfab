5c2482318d2426a1c79beb3f0070f861a4afe9f1
Nick Cloward <nick@authentise.com>
2016-10-18 12:03:34 -0600

    Move form into its own file
    AE-2454
    
    Move the new order form component to its own file.

---
+++ rapidfab/components/records/order/new/form.js
@@ -0,0 +1,73 @@
+import React, { PropTypes }     from "react"
+import * as BS                  from 'react-bootstrap'
+import _                        from "lodash"
+import Fa                       from 'react-fontawesome'
+import { FormattedMessage }     from 'react-intl'
+
+
+const NewOrderForm = ({ fields, materials, providers }) => (
+  <BS.Row>
+    <BS.Col xs={12}>
+      <BS.FormGroup controlId="uxName">
+        <BS.ControlLabel><FormattedMessage id="field.name" defaultMessage='Name'/>:</BS.ControlLabel>
+        <BS.FormControl type="text" required {...fields.name}/>
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxModel">
+        <BS.ControlLabel><FormattedMessage id="field.model" defaultMessage='Model'/>:</BS.ControlLabel>
+        <BS.FormControl type="file" {...fields.model} value={null} required />
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxQuantity">
+        <BS.ControlLabel><FormattedMessage id="field.quantity" defaultMessage='Quantity'/>:</BS.ControlLabel>
+        <BS.FormControl type="number" required {...fields.quantity}/>
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxBaseMaterials">
+        <BS.ControlLabel><FormattedMessage id="field.baseMaterials" defaultMessage='Base Materials'/>:</BS.ControlLabel>
+        <BS.FormControl componentClass="select" required {...fields.materials.base}>
+          <option key="placeholder" value="" selected disabled>Select a Base Material</option>
+          {_.map(materials, material => (
+            <option key={material.uri} value={material.uri}>{material.name}</option>
+          ))}
+        </BS.FormControl>
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxSupportMaterials">
+        <BS.ControlLabel><FormattedMessage id="field.supportMaterials" defaultMessage='Support Materials'/>:</BS.ControlLabel>
+        <BS.FormControl componentClass="select" {...fields.materials.support}>
+          <option key="placeholder" value="" selected disabled>No Support</option>
+          {_.map(materials, material => (
+            <option key={material.uri} value={material.uri}>{material.name}</option>
+          ))}
+        </BS.FormControl>
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxShippingName">
+        <BS.ControlLabel><FormattedMessage id="field.shippingName" defaultMessage='Shipping Name'/>:</BS.ControlLabel>
+        <BS.FormControl type="text" {...fields.shipping.name}/>
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxShippingAddress">
+        <BS.ControlLabel><FormattedMessage id="field.shippingAddress" defaultMessage='Shipping Address'/>:</BS.ControlLabel>
+        <BS.FormControl type="text" {...fields.shipping.address}/>
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxTrackingNumber">
+        <BS.ControlLabel><FormattedMessage id="field.trackingNumber" defaultMessage='Tracking Number'/>:</BS.ControlLabel>
+        <BS.FormControl type="text" {...fields.shipping.tracking}/>
+      </BS.FormGroup>
+
+      <BS.FormGroup controlId="uxThirdPartyProvider">
+        <BS.ControlLabel><FormattedMessage id="field.thirdPartyProvider" defaultMessage='Third Party Provider'/>:</BS.ControlLabel>
+        <BS.FormControl componentClass="select" {...fields.thirdPartyProvider}>
+          <option key="placeholder" value="" selected disabled>Select a Third Party Provider</option>
+          {_.map(providers, provider => (
+            <option key={provider.uri} value={provider.uri}>{provider.name}</option>
+          ))}
+        </BS.FormControl>
+      </BS.FormGroup>
+
+    </BS.Col>
+  </BS.Row>
+)
--- rapidfab/components/records/order/new/index.js
+++ rapidfab/components/records/order/new/index.js
@@ -1,10 +1,10 @@
-import React, { PropTypes }     from "react";
-import * as BS                  from 'react-bootstrap';
+import React, { PropTypes }     from "react"
+import * as BS                  from 'react-bootstrap'
 import _                        from "lodash"
-import Fa                       from 'react-fontawesome';
-import { FormattedMessage }     from 'react-intl';
-import Error                    from 'rapidfab/components/error';
-
+import Fa                       from 'react-fontawesome'
+import { FormattedMessage }     from 'react-intl'
+import Error                    from 'rapidfab/components/error'
+import NewOrderForm             from './form'
 
 const SaveButtonTitle = ({  }) => (
   <span>
@@ -26,73 +26,6 @@ const BreadCrumbs = ({ fields }) => (
           <Fa name='object-ungroup'/> {fields.id.value || <FormattedMessage id="record.order.new" defaultMessage='New Order'/>}
         </BS.Breadcrumb.Item>
       </BS.Breadcrumb>
-    </BS.Col>
-  </BS.Row>
-)
-
-const NewOrderForm = ({ fields, materials, providers }) => (
-  <BS.Row>
-    <BS.Col xs={12}>
-      <BS.FormGroup controlId="uxName">
-        <BS.ControlLabel><FormattedMessage id="field.name" defaultMessage='Name'/>:</BS.ControlLabel>
-        <BS.FormControl type="text" required {...fields.name}/>
-      </BS.FormGroup>
-
-      <BS.FormGroup controlId="uxModel">
-        <BS.ControlLabel><FormattedMessage id="field.model" defaultMessage='Model'/>:</BS.ControlLabel>
-        <BS.FormControl type="file" {...fields.model} value={null} required />
-      </BS.FormGroup>
-
-      <BS.FormGroup controlId="uxQuantity">
-        <BS.ControlLabel><FormattedMessage id="field.quantity" defaultMessage='Quantity'/>:</BS.ControlLabel>
-        <BS.FormControl type="number" required {...fields.quantity}/>
-      </BS.FormGroup>
-
-      <BS.FormGroup controlId="uxBaseMaterials">
-        <BS.ControlLabel><FormattedMessage id="field.baseMaterials" defaultMessage='Base Materials'/>:</BS.ControlLabel>
-        <BS.FormControl componentClass="select" required {...fields.materials.base}>
-          <option key="placeholder" value="" selected disabled>Select a Base Material</option>
-          {_.map(materials, material => (
-            <option key={material.uri} value={material.uri}>{material.name}</option>
-          ))}
-        </BS.FormControl>
-      </BS.FormGroup>
-
-      <BS.FormGroup controlId="uxSupportMaterials">
-        <BS.ControlLabel><FormattedMessage id="field.supportMaterials" defaultMessage='Support Materials'/>:</BS.ControlLabel>
-        <BS.FormControl componentClass="select" {...fields.materials.support}>
-          <option key="placeholder" value="" selected disabled>No Support</option>
-          {_.map(materials, material => (
-            <option key={material.uri} value={material.uri}>{material.name}</option>
-          ))}
-        </BS.FormControl>
-      </BS.FormGroup>
-
-      <BS.FormGroup controlId="uxShippingName">
-        <BS.ControlLabel><FormattedMessage id="field.shippingName" defaultMessage='Shipping Name'/>:</BS.ControlLabel>
-        <BS.FormControl type="text" {...fields.shipping.name}/>
-      </BS.FormGroup>
-
-      <BS.FormGroup controlId="uxShippingAddress">
-        <BS.ControlLabel><FormattedMessage id="field.shippingAddress" defaultMessage='Shipping Address'/>:</BS.ControlLabel>
-        <BS.FormControl type="text" {...fields.shipping.address}/>
-      </BS.FormGroup>
-
-      <BS.FormGroup controlId="uxTrackingNumber">
-        <BS.ControlLabel><FormattedMessage id="field.trackingNumber" defaultMessage='Tracking Number'/>:</BS.ControlLabel>
-        <BS.FormControl type="text" {...fields.shipping.tracking}/>
-      </BS.FormGroup>
-
-      <BS.FormGroup controlId="uxThirdPartyProvider">
-        <BS.ControlLabel><FormattedMessage id="field.thirdPartyProvider" defaultMessage='Third Party Provider'/>:</BS.ControlLabel>
-        <BS.FormControl componentClass="select" {...fields.thirdPartyProvider}>
-          <option key="placeholder" value="" selected disabled>Select a Third Party Provider</option>
-          {_.map(providers, provider => (
-            <option key={provider.uri} value={provider.uri}>{provider.name}</option>
-          ))}
-        </BS.FormControl>
-      </BS.FormGroup>
-
     </BS.Col>
   </BS.Row>
 )
