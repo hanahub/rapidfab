0c065a16d323a46b06b22ec594aef4cf2ec29052
Joeseph Rodrigues <dowhilegeek@gmail.com>
2016-10-27 12:16:27 -0600

    Add model failure component to new orders
    AE-2474

--- rapidfab/components/records/order/new/index.js
+++ rapidfab/components/records/order/new/index.js
@@ -5,6 +5,7 @@ import Fa                       from 'react-fontawesome'
 import { FormattedMessage }     from 'react-intl'
 import Error                    from 'rapidfab/components/error'
 import NewOrderForm             from './form'
+import Chart, { SeriesStyle }   from 'rapidfab/components/chart'
 
 const BreadCrumbs = ({  }) => (
   <BS.Row>
@@ -64,20 +65,75 @@ const Processing = ({ model, percent }) => {
   )
 }
 
+const modelError = model => {
+  let pass = <Fa name="check" style={{ color: SeriesStyle.Success.color  }}/>
+  let fail = <Fa name="times" style={{ color: SeriesStyle.Danger.color }}/>
+
+  let issues = _.keys(model.analyses).map(function(key) {
+    if(key === "manifold") {
+      return(
+        <span>
+          {model.analyses[key] ? pass : fail}
+          <FormattedMessage id="orderFailure.manifold" defaultMessage="Manifold"/>
+        </span>
+      )
+    }
+  })
+
+  return(
+    <BS.Row>
+      <BS.Row>
+        <BS.Col xsOffset={5} xs={2} style={{ textAlign: "center" }}>
+          <Fa name="ban" size="5x" style={{ color: SeriesStyle.Danger.color }}/>
+        </BS.Col>
+      </BS.Row>
+      <BS.Row>
+        <BS.Col xsOffset={4} xs={4} style={{ textAlign: "center" }}>
+          <h4><FormattedMessage id="orderFailure.header" defaultMessage="Unprintable model"/></h4>
+          <FormattedMessage id="orderFailure.description" defaultMessage="Some of the printability checks listed below failed. Please correct them and try again."/>
+        </BS.Col>
+      </BS.Row>
+      <BS.Row>
+        <BS.Col xsOffset={4} xs={4} style={{ textAlign: "center" }}>
+          {issues}
+        </BS.Col>
+      </BS.Row>
+    </BS.Row>
+  )
+}
+
+const orderForm = props => {
+  return(
+    <NewOrderForm
+      fields={props.fields}
+      materials={props.materials}
+      providers={props.providers}
+      handleSubmit={props.handleSubmit}
+    />
+  )
+}
+
+const Content = ({ props }) => {
+  let model = props.model
+  let uploadModel = props.uploadModel
+  let percent = props.uploadModel.percent
+
+  if(model && model.status === "processed" && !model.analyses.manifold) {
+    return(modelError(model))
+  } else if(uploadModel.uploading) {
+    return(Processing({model: model, percent: percent}))
+  } else {
+    return(orderForm(props))
+  }
+
+}
+
+
 const NewOrder = props => (
   <BS.Grid fluid>
     <BreadCrumbs />
     <ApiErrors />
-    { props.uploadModel.uploading ? (
-      <Processing percent={props.uploadModel.percent} model={props.model} />
-    ) : (
-      <NewOrderForm
-        fields={props.fields}
-        materials={props.materials}
-        providers={props.providers}
-        handleSubmit={props.handleSubmit}
-      />
-    ) }
+    <Content props={props}/>
   </BS.Grid>
 )
 
