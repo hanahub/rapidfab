57336112e780b1d71070da47c4cc97545af894bb
Joeseph Rodrigues <dowhilegeek@gmail.com>
2016-11-09 15:53:01 -0700

    grab the ones I forgot

--- rapidfab/components/records/postProcessorType.js
+++ rapidfab/components/records/postProcessorType.js
@@ -16,7 +16,7 @@ const PostProcessorTypeForm = ({ fields, handleSubmit, load, submitting, onDelet
       <BS.Row>
         <BS.Col xs={12}>
           <BS.Breadcrumb>
-            <BS.Breadcrumb.Item>
+            <BS.Breadcrumb.Item active={true}>
               <Fa name='list'/> <FormattedMessage id="inventory" defaultMessage='Inventory'/>
             </BS.Breadcrumb.Item>
             <BS.Breadcrumb.Item href="#/inventory/post-processor-types">
--- rapidfab/components/records/thirdPartyProvider.js
+++ rapidfab/components/records/thirdPartyProvider.js
@@ -41,7 +41,7 @@ const ThirdPartyProvider = ({ fields, handleSubmit, submitting, onDelete, apiErr
       <BS.Row>
         <BS.Col xs={12}>
           <BS.Breadcrumb>
-            <BS.Breadcrumb.Item>
+            <BS.Breadcrumb.Item active={true}>
               <Fa name='list'/> <FormattedMessage id="inventory" defaultMessage='Inventory'/>
             </BS.Breadcrumb.Item>
             <BS.Breadcrumb.Item href="#/inventory/third-party-providers">
