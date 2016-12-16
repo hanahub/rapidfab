5e8021effe7dc674a95660564c69f8bd666cc2fb
Joeseph Rodrigues <dowhilegeek@gmail.com>
2016-12-16 14:58:13 -0700

    Add 404 component
    AE-2527

---
+++ rapidfab/components/404.js
@@ -0,0 +1,13 @@
+import React, { Component }     from "react";
+import Version                  from "rapidfab/version";
+
+export default class NotFound extends Component {
+  render() {
+    return (
+      <div className="text-center">
+        <h1>404</h1>
+        <p>You seem to have reached a link that doesn't go anywhere. Maybe you want <a href="#/">to go back to the beginning?</a></p>
+      </div>
+    );
+  }
+}
