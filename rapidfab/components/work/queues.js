2cdf7c1034a77940529733bc1857a5cc30ab9c3d
Nick Cloward <nick@authentise.com>
2017-01-06 12:56:59 -0700

    Add full-calendar scheduler to queues page
    AE-3602
    
    This implements the full calendar's scheduler control into the queues
    page.  Some important things to note are the fact that its a jQuery
    plugin so we have to do some plumbing to format things correctly and
    pass in props correctly.

--- rapidfab/components/work/queues.js
+++ rapidfab/components/work/queues.js
@@ -1,202 +1,108 @@
-import React, { PropTypes }                   from "react";
-import * as BS                                from 'react-bootstrap';
-import Fa                                     from 'react-fontawesome';
-import Error                                  from 'rapidfab/components/error'
-import { FormattedMessage }                   from 'react-intl';
+import React, { PropTypes, Component }        from 'react'
+import * as BS                                from 'react-bootstrap'
 
-const Colors = {
-  Warning : { color: "#e4d836", hover: "#ccbf1b" },
-  Info    : { color: "#9f86ff", hover: "#7753ff" },
-  Danger  : { color: "#e64759", hover: "#dc1e33" },
-  Success : { color: "#1bc98e", hover: "#159c6e" },
-  Default : { color: "#ffffff", hover: "#e6e6e6" },
-  Primary : { color: "#1ca8dd", hover: "#1686b0" }
-};
 
-const containerStyle = {
-  position: "relative",
-  width: "100%",
-  height: "100%",
-  display: "inline-block"
-}
+class Queues extends Component {
+  constructor(props) {
+    super(props);
 
-const rightStyle = {
-  overflowY: "hidden",
-  overflowX: "scroll",
-}
-
-const headerStyle = {
-  width: 100,
-}
-
-const cellStyle = {
-  whiteSpace: "nowrap",
-  borderBottom: "1px solid #434857",
-  height: "40px",
-}
-
-const tableStyle = {
-  width: "inherit",
-  border: "1px solid #434857",
-  float: "left",
-}
-
-function getResourceType(resource) {
-  if(resource.printer_type) {
-    return "printer"
-  } else if (resource.post_processor_type) {
-    return "post-processor"
-  } else if (resource.layout) {
-    return "run"
+    this.fetchResources = this.fetchResources.bind(this);
+    this.fetchEvents = this.fetchEvents.bind(this);
   }
-  throw new Error("Could not determine resource type")
-}
 
-function getTimelineColCount(resources) {
-  const resourceTimes = _.map(resources, resource => {
-    return _.sumBy(resource.queue, run => {
-      const { print, post_processing } = run.estimates.time
-      return (print > post_processing ? print : post_processing) + 3600
+  fetchResources(callback) {
+    let machines = _.map(this.props.machines, machine => {
+      let type = machine.printer_type ? "printer" : "post-processor"
+      return {
+        id: machine.uri,
+        title: machine.name,
+        url: `#/records/${type}/${machine.uuid}`,
+        status: null,
+        type,
+      }
     })
-  })
-  const max = _.max(resourceTimes)
-  return Math.round(max / 3600, 0)
-}
-
-const ResourceLink = ({ resource }) => {
-  let resourceType = getResourceType(resource)
-  return (
-    <a href={`#/records/${resourceType}/${resource.uuid}`}>
-      <Fa name={resourceType === "printer" ? "print" : "qrcode"}/> {resource.name ? resource.name : resource.id}
-    </a>
-  )
-}
-
-const ItemHeader = ({ index }) => {
-  return (
-    <th>
-      <div style={headerStyle}>
-        {`${index}:00`}
-      </div>
-    </th>
-  )
-}
-
-const Item = ({ resource, colCount }) => {
-  const { queue } = resource
-  const resourceType = getResourceType(resource)
-  let queueRuns = _.map(queue, value => {
-    const estimatedTime = resourceType === "printer" ? value.estimates.time.print : value.estimates.time.post_processing
-    const colSpan = Math.round(estimatedTime / 3600, 0)
-    const printingStyle = {
-      backgroundColor: Colors.Success.color,
-    }
-    const postProcessorStyle = {
-      backgroundColor: Colors.Info.color,
-    }
-    const linkStyle = {
-      padding: "0 10px",
-      color: "#fff",
-      display: "block"
-    }
-    return (
-      <td style={cellStyle} colSpan={colSpan}>
-        <div style={resourceType === "printer" ? printingStyle : postProcessorStyle}>
-          <a href={`#/records/run/${value.uuid}`} style={linkStyle}>
-            <abbr title={value.uuid} style={{ cursor: "pointer" }}>
-              {value.uuid.substr(value.uuid.length - 6)}
-            </abbr>
-          </a>
-        </div>
-      </td>
-    )
-  })
-  if(queueRuns.length < colCount) {
-    queueRuns.push((
-      <td style={cellStyle} colSpan={colCount - queueRuns.length}>
-      </td>
-    ))
+    callback(machines)
   }
-  return (
-    <tr>
-      {queueRuns}
-    </tr>
-  )
-}
 
-const Loading = () => (
-  <div style={{ textAlign: "center" }}>
-    <Fa name="spinner" spin size='2x' />
-  </div>
-)
+  fetchEvents(start, end, timezone, callback) {
+    let events = _.map(this.props.runs, run => {
+      return {
+        id: run.uri,
+        resourceId: run.printer || run.post_processor,
+        title: run.id,
+        url: `#/records/run/${run.uuid}`,
+        start: run.actuals.start || run.estimates.start,
+        end: run.actuals.end || run.estimates.end,
+      }
+    })
+    callback(events)
+  }
 
-const Queues = ({ resources, apiErrors, fetching }) => {
-  const colCount = fetching ? 0 : getTimelineColCount(resources)
-  return (
-    <BS.Grid fluid>
+  componentDidMount() {
+    jQuery('#scheduler').fullCalendar({
+      editable: false,
+      aspectRatio: 1.8,
+      scrollTime: '00:00',
+      header: {
+        left: 'today prev,next',
+        center: 'title',
+        right: 'timelineDay,timelineWeek'
+      },
+      defaultView: 'timelineDay',
+      views: {
+        timelineDay: {
+          type: 'timeline',
+          buttonText: 'Day',
+          slotDuration: '00:30',
+          slotLabelFormat: [
+            'MMMM DD',
+            'HH:mm'
+          ]
+        },
+        timelineWeek: {
+          type: 'timeline',
+          buttonText: 'Week',
+          slotDuration: '01:00',
+          slotLabelFormat: [
+            'MMMM DD',
+            'HH:mm'
+          ]
+        },
+      },
+      timezone: "local",
+      maxTime: "23:59",
+      minTime: "00:00",
+      navLinks: true,
+      resourceAreaWidth: '20%',
+      resourceLabelText: 'Machines',
+      resources: this.fetchResources,
+      events: this.fetchEvents,
+      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+      resourceRender: (resourceObj, labelTds, bodyTds) => {
+        let icon = resourceObj.type === "printer" ? "print" : "qrcode"
+        labelTds.find('.fc-cell-text')
+                .wrapInner(`<a href="${resourceObj.url}">`)
+                .prepend(`<span class="fa fa-${icon} machine-status ${resourceObj.status}" /> `)
+      },
+    })
+  }
 
-      <BS.Row>
-        <BS.Col xs={12}>
-          <BS.Breadcrumb>
-            <BS.Breadcrumb.Item>
-              <Fa name='wrench'/> <FormattedMessage id="work" defaultMessage='Work'/>
-            </BS.Breadcrumb.Item>
-            <BS.Breadcrumb.Item href="#/work/queues">
-              <Fa name='list'/> <FormattedMessage id="work.queues" defaultMessage='Queues'/>
-            </BS.Breadcrumb.Item>
-          </BS.Breadcrumb>
-        </BS.Col>
-      </BS.Row>
+  componentDidUpdate(prevProps, prevState) {
+    if(prevProps.machines !== this.props.machines) {
+      jQuery('#scheduler').fullCalendar('refetchResources')
+    }
+    if(prevProps.runs !== this.props.runs) {
+      jQuery('#scheduler').fullCalendar('refetchEvents')
+    }
+  }
 
-      <BS.Row>
-        <BS.Col xs={12}>
-          <Error errors={apiErrors}/>
-        </BS.Col>
-      </BS.Row>
+  componentWillUnmount() {
+    jQuery('#scheduler').fullCalendar('destroy')
+  }
 
-      <BS.Row>
-        <BS.Col xs={12}>
-          {fetching ? <Loading/> : (
-            <div style={containerStyle}>
-              <div>
-                <BS.Table style={tableStyle}>
-                  <thead>
-                    <tr>
-                      <th>
-                        <FormattedMessage id="field.resources" defaultMessage='Resources'/>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {_.map(resources, resource => (
-                      <tr>
-                        <td style={cellStyle}>
-                          <ResourceLink resource={resource} />
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </BS.Table>
-              </div>
-              <div style={rightStyle}>
-                <BS.Table style={tableStyle}>
-                  <thead>
-                    <tr>
-                      {_.map(_.range(colCount), index => (<ItemHeader index={index} />))}
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {_.map(resources, resource => (<Item resource={resource} colCount={colCount}/>))}
-                  </tbody>
-                </BS.Table>
-              </div>
-            </div>
-          )}
-        </BS.Col>
-      </BS.Row>
-
-    </BS.Grid>
-  )
+  render() {
+    return <div id="scheduler" />
+  }
 }
 
 export default Queues
