ac800d819069595eef20b46a2733b6ecac07e1ee
Eli Ribble <eli@authentise.com>
2017-01-12 16:51:30 -0700

    Attempt to upload new index.html any time we publish updates
    By uploading an index.html we can make the S3 bucket work without going
    through CloudFront. This allows for much faster testing of changes in
    dev when we need it.
    
    Unfortunately I don't think this is working correctly yet because S3
    isn't showing that I get index.html uploaded. I'm checking in so Nick
    can have a look
    
    AE-3130 #time 30m

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const parallelize      = require('concurrent-transform');
 const path             = require('path');
 const awspublish       = require('gulp-awspublish');
 const cloudfront       = require('gulp-cloudfront');
+const rename           = require('gulp-rename');
 
 const knownOptions = {
   string: 'region',
@@ -13,8 +14,7 @@ const knownOptions = {
 };
 const options = minimist(process.argv.slice(2), knownOptions);
 
-gulp.task('default', ['publish']);
-gulp.task('publish', function() {
+function _getAWSPublisher() {
   if(!options.region) throw new Error('A target region is required to publish.');
   if(!options.bucket) throw new Error('A target bucket is required to publish.');
 
@@ -24,11 +24,34 @@ gulp.task('publish', function() {
       Bucket: options.bucket
     }
   };
-  var publisher = awspublish.create(aws);
+  return awspublish.create(aws);
+}
+
+gulp.task('default', ['publish']);
+gulp.task('publish', ['publish:assets', 'publish:index']);
+
+gulp.task('rename:index', function() {
+  return gulp.src(['index.*.html'])
+    .pipe(rename(function (path) {
+      path.basename = 'index';
+    }));
+});
+gulp.task('publish:index', ['rename:index'], function() {
+  var publisher = _getAWSPublisher();
+  var headers = { 'Cache-Control': 'max-age=1, no-transform, public' };
+  return gulp.src(['index.html'])
+    .pipe(awspublish.gzip())
+    .pipe(publisher.publish(headers), 10)
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter())
+});
+
+gulp.task('publish:assets', function() {
+  var publisher = _getAWSPublisher();
   var headers = { 'Cache-Control': 'max-age=3600, no-transform, public' };
   var cloudfrontConfig = {
-    bucket            : aws.bucket,
-    region            : aws.region,
+    bucket            : options.bucket,
+    region            : options.region,
     distributionId    : options.cloudfront,
     patternIndex      : new RegExp('index.' + process.env.BUILD_VERSION + '.html', 'gi')
   };
