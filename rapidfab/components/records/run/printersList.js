d221719486f93743799c73122ad1350edd199bb3
Joshua Cloward <Joshua@authentise.com>
2017-01-17 15:12:25 -0700

    Correcly display the printer status by using modeler status
    The printer status comes from the modeler attached to it and the status
    of said modeler. Now we are correctly grabbing the modeler information
    linking that up to the printer it belongs to and printing out the
    modeler status.
    
    AE-3141 #time 3h

--- rapidfab/components/records/run/printersList.js
+++ rapidfab/components/records/run/printersList.js
@@ -2,6 +2,7 @@ import React, { PropTypes }                   from "react"
 import _                                      from "lodash"
 import * as BS                                from 'react-bootstrap'
 import { FormattedMessage, FormattedDateTime }    from 'rapidfab/i18n'
+import ModelerStatus                              from 'rapidfab/components/modelerStatus'
 
 const listBodyStyle = {
   height: 150,
@@ -9,38 +10,41 @@ const listBodyStyle = {
   overflowX: "hidden"
 }
 
-const PrinterItem = ({ printer, selected, onSelect }) => (
-  <BS.ListGroupItem bsStyle={selected && selected.uri == printer.uri ? "info" : null} onClick={() => onSelect(printer)}>
-    <BS.Row>
-      <BS.Col xs={3}>
-        <a href={`#/records/printer/${printer.uuid}`}>
-          {printer.name}
-        </a>
-      </BS.Col>
-      <BS.Col xs={3}>
-        <a href={`#/records/printer-type/${printer.printer_type.uuid}`}>
-          {printer.printer_type.name}
-        </a>
-      </BS.Col>
-      <BS.Col xs={2}>
-        {`${printer.printer_type.build_volume.x}mm x ${printer.printer_type.build_volume.y}mm`}
-      </BS.Col>
-      <BS.Col xs={2}>
-        <span style={{ textTransform: "capitalize" }}>
-          {printer.status}
-        </span>
-      </BS.Col>
-      <BS.Col xs={2}>
-        {printer.leadTime ?
-          <FormattedDateTime value={printer.leadTime}/> :
-          (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
-        }
-      </BS.Col>
-    </BS.Row>
-  </BS.ListGroupItem>
-)
+const PrinterItem = ({ printer, modelers, selected, onSelect }) => {
+  let modeler = _.find(modelers, ['uri', printer.modeler])
+  return (
+    <BS.ListGroupItem bsStyle={selected && selected.uri == printer.uri ? "info" : null} onClick={() => onSelect(printer)}>
+      <BS.Row>
+        <BS.Col xs={3}>
+          <a href={`#/records/printer/${printer.uuid}`}>
+            {printer.name}
+          </a>
+        </BS.Col>
+        <BS.Col xs={3}>
+          <a href={`#/records/printer-type/${printer.printer_type.uuid}`}>
+            {printer.printer_type.name}
+          </a>
+        </BS.Col>
+        <BS.Col xs={2}>
+          {`${printer.printer_type.build_volume.x}mm x ${printer.printer_type.build_volume.y}mm`}
+        </BS.Col>
+        <BS.Col xs={2}>
+          <span style={{ textTransform: "capitalize" }}>
+            <ModelerStatus modeler={modeler} />
+          </span>
+        </BS.Col>
+        <BS.Col xs={2}>
+          {printer.leadTime ?
+            <FormattedDateTime value={printer.leadTime}/> :
+            (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
+          }
+        </BS.Col>
+      </BS.Row>
+    </BS.ListGroupItem>
+  )
+}
 
-const PrintersList = ({ printers, selected, onSelect }) => (
+const PrintersList = ({ printers, modelers, selected, onSelect }) => (
   <BS.Panel header="Printers">
     <BS.ListGroup fill>
       <BS.ListGroupItem style={{ borderBottomWidth: 2 }} key="header">
@@ -68,6 +72,7 @@ const PrintersList = ({ printers, selected, onSelect }) => (
             key={printer.uuid}
             selected={selected}
             printer={printer}
+            modelers={modelers}
             onSelect={onSelect}
           />
         ))}
