11cce6c5027f02aedc554527499af1ae7a23ff74
Joshua Cloward <Joshua@authentise.com>
2017-01-19 20:45:46 -0700

    Adjust emails to the new way emails is returned
    We just had a change to the way emails is returned on the users
    endpoint. It is now a array full of strings instead of objects.
    
    AE-3210 #time 15m

--- rapidfab/containers/records/user.js
+++ rapidfab/containers/records/user.js
@@ -59,7 +59,7 @@ function mapDispatchToProps(dispatch) {
 function mapStateToProps(state, props) {
   const initialValues = Selectors.getRouteResource(state, props)
   if(initialValues && initialValues.emails && initialValues.emails.length > 0) {
-    initialValues.email = initialValues.emails[0].email;
+    initialValues.email = initialValues.emails[0];
   }
   return {
     uuid            : Selectors.getRoute(state, props).uuid,
