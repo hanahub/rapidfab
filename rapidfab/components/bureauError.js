7ef57a75c29c0debceb830c82df9233785f50cdb
Joshua Cloward <Joshua@authentise.com>
2017-02-10 15:51:59 -0700

    Add formmatted filters to bureauError page
    AE-3180 #time 1h

--- rapidfab/components/bureauError.js
+++ rapidfab/components/bureauError.js
@@ -1,13 +1,17 @@
 import React                  from 'react'
 import * as BS                from 'react-bootstrap'
 import _                      from "lodash"
+import { FormattedMessage }   from 'react-intl'
 
 const BureauError = ({ bureaus }) => {
   let message = ''
+  let id = ''
   if(bureaus.length > 1) {
-    message = 'You belong to multiple Bureaus. Only one Bureau is allowed when using MES. Please remove one of the Bureaus listed below.'
+    id = 'bureauError.multiple'
+    message = 'You belong to multiple Bureaus. Only one Bureau is allowed when using MES. Please remove one of the Bureaus listed below. '
   } else {
-    message = 'You are not assigned to any Bureaus. A Bureau is required to use MES.'
+    id = 'bureauError.empty'
+    message = 'You are not assigned to any Bureaus. A Bureau is required to use MES. '
   }
   return (
     <BS.Grid fluid>
@@ -15,13 +19,12 @@ const BureauError = ({ bureaus }) => {
         <BS.Row>
           <BS.Col xs={12}>
             <h1>Bureau Error!</h1>
-            <p>{message}</p>
+            <FormattedMessage id={id} defaultMessage={message} />
             {_.map(bureaus, bureau => (
               <p><strong>{bureau.name}</strong></p>
             ))}
-            <p>You can find the docs for this process
+            <FormattedMessage id='bureauError.docsMessage' defaultMessage='You can find the docs for this process'/>
               <a href="http://docs.authentise.com/mes/index.html"> Here!</a>
-            </p>
           </BS.Col>
         </BS.Row>
       </BS.Jumbotron>
