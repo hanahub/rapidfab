64b1a2902847ef92b3b33ab32395f70f625c553a
Ian Wright <ian@authentise.com>
2017-05-24 11:56:03 -0600

    Add Redux Dev Tools functionality

--- rapidfab/reducers/initializeStore.js
+++ rapidfab/reducers/initializeStore.js
@@ -1,21 +1,24 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunkMiddleware                  from 'redux-thunk'
-import createLogger                     from 'redux-logger'
-import apiMiddleware                    from 'rapidfab/middleware/api'
-
-import Reducer                          from 'rapidfab/reducers'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunkMiddleware                           from 'redux-thunk'
+import createLogger                              from 'redux-logger'
+import apiMiddleware                             from 'rapidfab/middleware/api'
+import Reducer                                   from 'rapidfab/reducers'
 
 
 const loggerMiddleware = createLogger()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 
 export default function initializeStore(initialState) {
   return createStore(
     Reducer,
     initialState,
-    applyMiddleware(
-      thunkMiddleware,
-      apiMiddleware,
-      loggerMiddleware,
+    composeEnhancers(
+      applyMiddleware(
+        thunkMiddleware,
+        apiMiddleware,
+        loggerMiddleware,
+      )
     )
   )
 }
