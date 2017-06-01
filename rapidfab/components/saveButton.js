96b3cddbcb42d107137e112cb2e01fd22984017b
Ian Wright <ian@authentise.com>
2017-06-01 14:22:37 -0600

    Create breadcrumbNav and saveButton components
    These two components are reusable for a future refactor of old components.

---
+++ rapidfab/components/saveButton.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Fa from 'react-fontawesome';
+import { FormattedMessage } from 'react-intl';
+import {
+  ButtonToolbar,
+  SplitButton,
+  MenuItem,
+} from 'react-bootstrap';
+
+
+const SaveButtonTitle = () => (
+  <span>
+    <Fa name='floppy-o'/> <FormattedMessage id="button.save" defaultMessage='Save'/>
+  </span>
+);
+
+const saveButton = ({ onSubmit, onDelete }) => (
+  <SplitButton
+    id="actions"
+    type="submit"
+    bsStyle="success"
+    bsSize="small"
+    title={<SaveButtonTitle />}
+    onClick={onSubmit}
+  >
+    <MenuItem
+      eventKey={1}
+      onClick={onDelete}
+    >
+      <Fa name='ban'/>
+      <FormattedMessage id="button.delete" defaultMessage='Delete'/>
+    </MenuItem>
+  </SplitButton>
+);
+
+export default saveButton;
