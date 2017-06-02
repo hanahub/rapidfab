d4e567c0fdfa5504170b6af2d587d4d0eb0b7a37
Ian Wright <ian@authentise.com>
2017-06-02 13:28:01 -0600

    Fix and refactor prints component
    The prints component has a small bug. The status value was mapped incorrectly
    to the mappings.js file in the grid.
    
    In addition, this commit refactors the print container and component for new
    design and reusable components.
    
    AE-4053 #time 3h

--- rapidfab/containers/plan/prints.js
+++ rapidfab/containers/plan/prints.js
@@ -1,10 +1,11 @@
-import _                                from "lodash";
-import React, { Component }             from "react";
-import PropTypes                        from 'prop-types';
-import Actions                          from "rapidfab/actions";
-import { connect }                      from 'react-redux';
-import * as Selectors                   from 'rapidfab/selectors';
-import PrintsComponent                  from 'rapidfab/components/plan/prints';
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Actions from "rapidfab/actions";
+import * as Selectors from 'rapidfab/selectors';
+import Gatekeeper from 'rapidfab/components/gatekeeper';
+import PrintsComponent from 'rapidfab/components/plan/prints';
 
 class PrintsContainer extends Component {
   componentDidMount() {
@@ -12,7 +13,16 @@ class PrintsContainer extends Component {
   }
 
   render() {
-    return <PrintsComponent {...this.props}/>
+    const { prints, locations, fetching, apiErrors, handleOnChange } = this.props;
+    return(
+      <Gatekeeper errors={apiErrors} loading={fetching}>
+        <PrintsComponent
+          prints={prints}
+          locations={locations}
+          onLocationChange={handleOnChange}
+        />
+      </Gatekeeper>
+    )
   }
 }
 
@@ -36,21 +46,22 @@ function mapStateToProps(state) {
   const prints = Selectors.getPrints(state)
   const locationFilter = Selectors.getLocationFilter(state)
   let filteredPrints = null;
-  if(locationFilter) {
-     filteredPrints = _.filter(prints, ['location' , state.locationFilter.location]);
-  }
+  if(locationFilter)
+     filteredPrints = prints.filter( print => print.location === state.locationFilter.location);
+
   return {
-    prints         : filteredPrints || prints,
-    locations      : Selectors.getLocations(state),
-    locationFilter : locationFilter,
-    fetching       : print.list.fetching || location.list.fetching,
-    apiErrors      : print.list.errors || location.list.errors,
+    prints: filteredPrints || prints,
+    locations: Selectors.getLocations(state),
+    locationFilter: locationFilter,
+    fetching: print.list.fetching || location.list.fetching,
+    apiErrors: print.list.errors || location.list.errors,
   }
 }
 
 PrintsContainer.propTypes = {
   prints: PropTypes.array,
   locations: PropTypes.array,
+  locationFilter: PropTypes.string,
   fetching: PropTypes.bool,
   apiErrors: PropTypes.array,
 };
