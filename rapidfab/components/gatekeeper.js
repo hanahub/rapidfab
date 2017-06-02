0b0fde1b8fa796ef7b3021f30080219169618728
Ian Wright <ian@authentise.com>
2017-06-02 13:14:06 -0600

    Create gatekeeper component
    The gatekeeper component is an HOC component for handling error and
    loading states. It is intended for use a buffer between container and
    presentational componenets.

---
+++ rapidfab/components/gatekeeper.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Error from './error';
+import Loading from './loading';
+
+const Gatekeeper = ({ errors, loading, children }) => {
+  if (errors.length > 0)
+    return <Error errors={error} />
+  else if (loading)
+    return <Loading />
+  else
+    return children
+};
+Gatekeeper.propTypes = {
+  errors: PropTypes.array,
+  loading: PropTypes.bool,
+  children: PropTypes.element,
+}
+
+export default Gatekeeper;
