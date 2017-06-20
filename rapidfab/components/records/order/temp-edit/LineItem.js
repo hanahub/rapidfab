00f90c75d717fe832f6875316daf797cf74e1b48
Ian Wright <ian@authentise.com>
2017-06-20 16:25:25 -0600

    Create logic for line item form and refactor
    This commit refactors the LineItem component by creating a new LineItemForm
    component to handle all of the form logic, and all of the other view displays
    handled by the LineItem component.
    
    The onSubmit and onDelete functions are not implmenented as of yet, pending the
    Line Item API to be completed.
    
    This commit also includes various refactors for reducing unecessary code and
    imports.
    
    AE-4164 #time 6h

--- rapidfab/components/records/order/temp-edit/LineItem.js
+++ rapidfab/components/records/order/temp-edit/LineItem.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { reduxForm } from 'redux-form'
-import * as Selectors from 'rapidfab/selectors';
-import _ from 'lodash';
-import * as BS from 'react-bootstrap';
 import Fa from 'react-fontawesome';
+import {
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Panel,
+  Row,
+  Thumbnail,
+} from 'react-bootstrap';
+
+import { getPrintsForOrder, getModels } from 'rapidfab/selectors';
 import {
   FormattedCost,
   FormattedDate,
@@ -12,113 +18,167 @@ import {
   FormattedMessage,
   FormattedVolume
 } from 'rapidfab/i18n';
-
 import {
   FormControlTextArea,
   FormControlTextCareful
 } from 'rapidfab/components/formTools';
+import LineItemForm from './LineItemForm';
 
-const fields = [
-  'estimates',
-  'model',
-  'materials',
-  'template',
-  'quantity',
-];
-
-const Header = ( prints ) => {
-  const complete = (_.reduce(prints, (total, print) => print.status == 'complete' ? total + 1 : total, 0)).toString();
+const Header = (prints) => {
+  const complete = prints.reduce( (total, print) => {
+    return prints.status === 'complete' ? total + 1 : total;
+  }, 0).toString();
   const total = (!!prints ? prints.length : 0).toString();
   return (
-    <FormattedMessage id="record.printCompleteCount" defaultMessage={`Process Steps - {complete} / {total} complete`} values={{complete: complete, total: total}}/>
-  )
-}
-
-const PrintItem = ({ print }) => {
-  const statusMapping = {
-    created           : (<FormattedMessage id="status.created" defaultMessage="Created"/>),
-    calculating       : (<FormattedMessage id="status.calculating" defaultMessage="Calculating"/>),
-    calculated        : (<FormattedMessage id="status.calculated" defaultMessage="Calculated"/>),
-    queued            : (<FormattedMessage id="status.queued" defaultMessage="Queued"/>),
-    "in-progress"     : (<FormattedMessage id="status.in_progress" defaultMessage="In Progress"/>),
-    complete          : (<FormattedMessage id="status.complete" defaultMessage="Complete"/>),
-    error             : (<FormattedMessage id="status.error" defaultMessage="Error"/>),
-  };
-
-  return (
-    <BS.ListGroupItem>
-      <BS.Row>
-        <BS.Col xs={6}>
-          {print.id}
-        </BS.Col>
-        <BS.Col xs={6}>
-          {statusMapping[print.status]}
-        </BS.Col>
-      </BS.Row>
-    </BS.ListGroupItem>
+    <FormattedMessage
+      id="record.printCompleteCount"
+      defaultMessage={`Process Steps - {complete} / {total} complete`}
+      values={{complete: complete, total: total}}
+    />
   );
 }
 
+const statusMapping = {
+  created           : (<FormattedMessage id="status.created" defaultMessage="Created"/>),
+  calculating       : (<FormattedMessage id="status.calculating" defaultMessage="Calculating"/>),
+  calculated        : (<FormattedMessage id="status.calculated" defaultMessage="Calculated"/>),
+  queued            : (<FormattedMessage id="status.queued" defaultMessage="Queued"/>),
+  "in-progress"     : (<FormattedMessage id="status.in_progress" defaultMessage="In Progress"/>),
+  complete          : (<FormattedMessage id="status.complete" defaultMessage="Complete"/>),
+  error             : (<FormattedMessage id="status.error" defaultMessage="Error"/>),
+};
+
+const PrintItem = ({ print }) => (
+  <ListGroupItem>
+    <Row>
+      <Col xs={6}>
+        {print.id}
+      </Col>
+      <Col xs={6}>
+        {statusMapping[print.status]}
+      </Col>
+    </Row>
+  </ListGroupItem>
+);
+
 const Prints = ({ prints }) => (
-  <BS.ListGroup fill>
-    <BS.ListGroupItem style={{ borderBottomWidth: 2 }} key="header">
-      <BS.Row>
-        <BS.Col xs={6}>
-          <b><FormattedMessage id="field.id" defaultMessage="ID"/></b>
-        </BS.Col>
-        <BS.Col xs={6}>
-          <b><FormattedMessage id="field.status" defaultMessage="Status"/></b>
-        </BS.Col>
-      </BS.Row>
-    </BS.ListGroupItem>
-    <div style={{overflowY: 'scroll', height: 215}}>
+  <Panel header={Header(prints)} bsStyle="primary">
+    <ListGroup fill>
+
+      <ListGroupItem key="header">
+        <Row>
+          <Col xs={6}>
+            <b><FormattedMessage id="field.id" defaultMessage="ID"/></b>
+          </Col>
+          <Col xs={6}>
+            <b><FormattedMessage id="field.status" defaultMessage="Status"/></b>
+          </Col>
+        </Row>
+      </ListGroupItem>
+
       { prints.map( print => <PrintItem key={print.id} print={print} />)}
-    </div>
-  </BS.ListGroup>
+
+    </ListGroup>
+  </Panel>
 );
 
 const Estimates = ({ estimates, currency }) => (
-  <BS.ListGroup fill>
-    <BS.ListGroupItem header={<FormattedMessage id="field.estimatedPrintTime" defaultMessage='Estimated Print Time'/>}>
-      {estimates.print_time.value ?
-        <FormattedDuration value={estimates.print_time.value}/> :
-          (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
-      }
-    </BS.ListGroupItem>
-    <BS.ListGroupItem header={<FormattedMessage id="field.estimatedMaterialUsed" defaultMessage='Estimated Material Used'/>}>
-      {estimates.materials.base.value ?
-        <FormattedVolume value={estimates.materials.base.value}/> :
-          (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
-      }
-    </BS.ListGroupItem>
-    <BS.ListGroupItem header={<FormattedMessage id="field.estimatedSupportUsed" defaultMessage='Estimated Support Used'/>}>
-      {estimates.materials.support.value ?
-        <FormattedVolume value={estimates.materials.support.value}/> :
-          (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
-      }
-    </BS.ListGroupItem>
-    <BS.ListGroupItem header={<FormattedMessage id="field.estimatedCost" defaultMessage='Estimated Cost'/>}>
-      {estimates.cost.amount.value ?
-        <FormattedCost currency={currency.value} value={estimates.cost.amount.value} /> :
-          (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
-      }
-    </BS.ListGroupItem>
-    <BS.ListGroupItem header={<FormattedMessage id="field.estimatedShippingCost" defaultMessage='Estimated Shipping Cost'/>}>
-      {estimates.cost.shipping_amount.value ?
-        <FormattedCost currency={currency.value} value={estimates.cost.shipping_amount.value} /> :
-          (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
-      }
-    </BS.ListGroupItem>
-    <BS.ListGroupItem header={<FormattedMessage id="field.estimatedTotalCost" defaultMessage='Estimated Total Cost'/>}>
-      {estimates.cost.amount.value + estimates.cost.shipping_amount.value ?
-        <FormattedCost currency={currency.value} value={estimates.cost.amount.value + estimates.cost.shipping_amount.value} /> :
-          (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
-      }
-    </BS.ListGroupItem>
-  </BS.ListGroup>
+  <Panel bsStyle="info">
+    <ListGroup fill>
+
+      <ListGroupItem key="header">
+        <b><FormattedMessage id="estimates.estimates" defaultMessage="Estimates"/></b>
+      </ListGroupItem>
+
+      <ListGroupItem>
+        <Row>
+          <Col xs={8}>
+            <FormattedMessage id="estimates.printTime" defaultMessage='Print Time'/>
+          </Col>
+          <Col xs={4}>
+            {estimates.print_time ?
+              <FormattedDuration value={estimates.print_time}/> :
+                (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
+            }
+          </Col>
+        </Row>
+      </ListGroupItem>
+
+      <ListGroupItem>
+        <Row>
+          <Col xs={8}>
+            <FormattedMessage id="estimates.materialUsed" defaultMessage='Material Used'/>
+          </Col>
+          <Col xs={4}>
+            {estimates.materials.base ?
+              <FormattedVolume value={estimates.materials.base}/> :
+                (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
+            }
+          </Col>
+        </Row>
+      </ListGroupItem>
+
+      <ListGroupItem>
+        <Row>
+          <Col xs={8}>
+            <FormattedMessage id="estimates.supportUsed" defaultMessage='Support Used'/>
+          </Col>
+          <Col xs={4}>
+            {estimates.materials.support ?
+              <FormattedVolume value={estimates.materials.support}/> :
+                (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
+            }
+          </Col>
+        </Row>
+      </ListGroupItem>
+
+      <ListGroupItem>
+        <Row>
+          <Col xs={8}>
+            <FormattedMessage id="estimates.cost" defaultMessage='Cost'/>
+          </Col>
+          <Col xs={4}>
+            {estimates.cost.amount ?
+              <FormattedCost currency={currency} value={estimates.cost.amount} /> :
+                (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
+            }
+          </Col>
+        </Row>
+      </ListGroupItem>
+
+      <ListGroupItem>
+        <Row>
+          <Col xs={8}>
+            <FormattedMessage id="estimates.shippingCost" defaultMessage='Shipping Cost'/>
+          </Col>
+          <Col xs={4}>
+            {estimates.cost.shipping_amount ?
+              <FormattedCost currency={currency} value={estimates.cost.shipping_amount} /> :
+                (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
+            }
+          </Col>
+        </Row>
+      </ListGroupItem>
+
+      <ListGroupItem>
+        <Row>
+          <Col xs={8}>
+            <FormattedMessage id="estimates.totalCost" defaultMessage='Total Cost'/>
+          </Col>
+          <Col xs={4}>
+            {estimates.cost.amount + estimates.cost.shipping_amount ?
+              <FormattedCost currency={currency} value={estimates.cost.amount + estimates.cost.shipping_amount} /> :
+                (<em><FormattedMessage id="notAvailable" defaultMessage='N/A'/></em>)
+            }
+          </Col>
+        </Row>
+      </ListGroupItem>
+
+    </ListGroup>
+  </Panel>
 );
 
-const Thumbnail = ({src}) => {
+const ModelThumbnail = ({src}) => {
   if(!src) {
     return (
       <div>
@@ -128,174 +188,62 @@ const Thumbnail = ({src}) => {
       </div>
     );
   } else {
-    return (<BS.Thumbnail src={src} />);
+    return (<Thumbnail src={src} />);
   }
 }
 
-const FormRow = ({id, defaultMessage, children}) => (
-  <BS.FormGroup>
-    <BS.Col xs={3}>
-      <BS.ControlLabel><FormattedMessage id={id} defaultMessage={defaultMessage}/>:</BS.ControlLabel> </BS.Col>
-    <BS.Col xs={9}>
-      {children}
-    </BS.Col>
-  </BS.FormGroup>
-)
+const LineItem = ({ currency, estimates, prints, snapshot }) => (
+  <Panel header="Line Item">
 
-const ModelSelect = ({models, modelsIsFetching, field}) => {
-  if(modelsIsFetching) {
-    return (
-      <BS.FormControl.Static>
-        <FormattedMessage id="loading.model" defaultMessage="Loading models..."/>
-      </BS.FormControl.Static>
-    );
-  } else {
-    return (
-      <BS.FormControl componentClass="select" required {...field}>
-        <option value="" disabled>Select a Model</option>
-        {_.map(models, model => (<option key={model.uri} value={model.uri}>{model.name}</option>))}
-      </BS.FormControl>
-    );
-  }
-}
+    <Col xs={12} sm={4}>
+      <Row>
+        <Col xs={10} xsOffset={1} lg={6} lgOffset={3}>
+          <ModelThumbnail src={snapshot}/>
+        </Col>
+      </Row>
 
-const Printable = ({ models, uri }) => {
-  let model = _.find(models, {uri})
-  let printable = true
-
-  if(model && !model.analyses.manifold) {
-    printable = false
-  }
-
-  if(model) {
-    if(printable) {
-      return <BS.Label bsStyle="success"><FormattedMessage id="field.printable" defaultMessage='Printable'/></BS.Label>
-    } else {
-      return <BS.Label bsStyle="warning"><FormattedMessage id="field.unknown" defaultMessage='Unknown'/></BS.Label>
-    }
-  } else {
-    return <BS.Label bsStyle="info"><FormattedMessage id="field.loading" defaultMessage='Loading'/></BS.Label>
-  }
-}
-
-const LineItemForm = ({
-  models,
-  modelsIsFetching,
-  fields,
-  materials,
-  templates,
-}) => (
-  <BS.Col>
-    <FormRow id="field.printable" defaultMessage="Printable">
-      <BS.FormControl.Static>
-        <Printable models={models} uri={fields.model.value}/>
-      </BS.FormControl.Static>
-    </FormRow>
-
-    <FormRow id="field.model" defaultMessage="Model">
-      <ModelSelect models={models} modelsIsFetching={modelsIsFetching} field={fields.model}/>
-    </FormRow>
-
-    <FormRow id="field.quantity" defaultMessage="Quantity">
-      <BS.FormControl type="number" required {...fields.quantity}/>
-    </FormRow>
-
-    <FormRow id="field.baseMaterial" defaultMessage="Base Material">
-      <BS.FormControl componentClass="select" required {...fields.materials.base}>
-        <option value="" disabled>Select a Material</option>
-        {_.map(_.filter(materials, {type: "base"}), material => (<option key={material.uri} value={material.uri}>{material.name}</option>))}
-      </BS.FormControl>
-    </FormRow>
-
-    <FormRow id="field.supportMaterial" defaultMessage="Support Material">
-      <BS.FormControl componentClass="select" {...fields.materials.support}>
-        <option value="">None</option>
-        {_.map(_.filter(materials, {type: "support"}), material => (<option key={material.uri} value={material.uri}>{material.name}</option>))}
-      </BS.FormControl>
-    </FormRow>
-
-    <FormRow id="field.template" defaultMessage="Select a template">
-      <BS.FormControl componentClass="select" {...fields.template}>
-        <option key="placeholder" value="" selected>Select a Template</option>
-        {_.map(templates, template => (
-          <option key={template.uri} value={template.uri}>{template.name}</option>
-        ))}
-      </BS.FormControl>
-    </FormRow>
-  </BS.Col>
-);
-
-const LineItem = ({
-  currency,
-  estimates,
-  fields,
-  materials,
-  models,
-  modelsIsFetching,
-  prints,
-  snapshot,
-  templates,
-}) => (
-  <BS.Panel header="Line Item">
-
-    <BS.Col xs={10} xsOffset={1} sm={4} smOffset={0} lg={2} lgOffset={1}>
-      <Thumbnail src={snapshot}/>
-    </BS.Col>
-
-    <BS.Col xs={12} sm={8} lgOffset={1}>
-
-      <BS.Accordion>
-        <BS.Panel bsStyle="primary" header="Summary" eventKey="1" expanded="true">
-          <LineItemForm fields={fields} templates={templates} models={models} materials={materials} modelsIsFetching={modelsIsFetching}/>
-        </BS.Panel>
-        <BS.Panel bsStyle="primary" header="Estimates" eventKey="2">
+      <Row>
+        <Col xs={12} lg={10} lgOffset={1}>
           <Estimates estimates={estimates} currency={currency}/>
-        </BS.Panel>
-        <BS.Panel
-          header="Process Steps"
-          // header={Header(prints)}
-          bsStyle="primary" eventKey="3">
-          <Prints prints={prints} />
-        </BS.Panel>
-      </BS.Accordion>
+        </Col>
+      </Row>
+    </Col>
 
-    </BS.Col>
-  </BS.Panel>
+    <Col xs={12} sm={8}>
+      <Row>
+        <Col>
+          <LineItemForm />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={9} xsOffset={3}>
+          <Prints prints={prints} style={{padding: "20px"}}/>
+        </Col>
+      </Row>
+    </Col>
+
+  </Panel>
 );
 
 function getSnapshotFromOrder(order, models) {
-  if(!order || models.length === 0) return ''
-  const model = models.filter(model => model.uri === order.model)
-  return (model && model.length) ? model[0].snapshot_content : ''
+  if (!order || models.length === 0)
+    return ''
+
+  const model = models.filter(model => model.uri === order.model);
+
+  return (model && model.length) ? model[0].snapshot_content : '';
 }
 
 const mapStateToProps = (state) => {
   const { order } = state.ui.wyatt;
-  const { model } = state.ui.hoth;
-
   const orderResource = state.resources[state.routeUUID.uuid];
   const { currency, estimates } = orderResource;
-  const models = Selectors.getModels(state);
-  const modelsIsFetching = model.list.fetching || model.get.fetching;
-  const prints = Selectors.getPrintsForOrder(state, orderResource);
+  const prints = getPrintsForOrder(state, orderResource);
+  const models = getModels(state);
   const snapshot = getSnapshotFromOrder(orderResource, models);
-  const templates = Selectors.getTemplates(state);
-  const materials = Selectors.getMaterials(state);
 
-  return {
-    initialValues: orderResource,
-    currency,
-    estimates,
-    models,
-    modelsIsFetching,
-    materials,
-    prints,
-    snapshot,
-    templates,
-  }
+  return { currency, estimates, prints, snapshot };
 }
 
-export default reduxForm({
-  form: 'record.lineItem',
-  fields,
-},mapStateToProps)(LineItem)
+export default connect(mapStateToProps)(LineItem)
