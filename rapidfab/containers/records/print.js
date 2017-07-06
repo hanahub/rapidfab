42fa6e0705d18fcd5e2f793d9a9ba8de5900ad81
joeseph <joeseph@authentise.com>
2017-07-06 14:12:20 -0600

    wire up print ui to get events
    its basically all right there
    
    AE-4244 #time 2h

--- rapidfab/containers/records/print.js
+++ rapidfab/containers/records/print.js
@@ -14,11 +14,11 @@ class PrintContainer extends Component {
   }
 
   render() {
-    const { apiErrors, fetching, print, order, model } = this.props;
-    const loading = fetching || !print || !order;
+    const { apiErrors, fetching, print, order, lineItem, model, events } = this.props;
+    const loading = fetching || !print || !order || !lineItem || !model || !events;
     return(
       <Gatekeeper errors={apiErrors} loading={loading}>
-        <PrintComponent print={print} order={order} model={model} />
+        <PrintComponent print={print} order={order} lineItem={lineItem} model={model} events={events} />
       </Gatekeeper>
     );
   }
@@ -27,15 +27,41 @@ class PrintContainer extends Component {
 function mapDispatchToProps(dispatch) {
   return {
     onInitialize: uuid => {
-      dispatch(Actions.Api.wyatt.print.get(uuid))
+      const print = dispatch(Actions.Api.wyatt.print.get(uuid))
+
+      // GET model
+      print
         .then( response => {
-          return dispatch(Actions.Api.wyatt.order.get(extractUuid(response.json.order)));
+          return dispatch(Actions.Api.wyatt["line-item"].get(extractUuid(response.json.line_item)));
         })
         .then( response => {
-          return dispatch(Actions.Api.hoth.model.get(extractUuid(response.json.model)));
-        })
-        .catch ( err => {
-          window.location.hash = "#/plan/prints";
+          dispatch(Actions.Api.hoth.model.get(extractUuid(response.json.model)));
+        });
+
+      // GET order
+      print
+        .then( response => {
+          dispatch(Actions.Api.wyatt.order.get(extractUuid(response.json.order)));
+        });
+
+      // GET all related prints
+      const prints = print
+        .then( response => {
+          return dispatch(Actions.Api.wyatt.print.list({line_item: response.json.line_item}))
+        });
+
+      // LIST events based on collected uris
+      Promise.all([print, prints])
+        .then( promises => {
+          let uris = [
+            promises[0].json.line_item,
+            promises[0].json.order,
+            ..._.compact(promises[1].json.resources.map( resource => resource.run )),
+          ]
+
+          _.chunk(uris, 10).map( chunk => {
+            dispatch(Actions.Api.wyatt.event.list({reference: uris}))
+          })
         });
     },
   }
@@ -45,20 +71,29 @@ function mapStateToProps(state, props) {
   const uuid = Selectors.getRoute(state, props).uuid;
   const print = Selectors.getRouteResource(state, props);
   const orders = Selectors.getOrders(state, props);
+  const lineItems = Selectors.getLineItems(state, props);
   const models = Selectors.getModels(state, props);
+  const events = Selectors.getEventsForPrint(state, print);
   const apiErrors = Selectors.getResourceErrors(state, 'wyatt.print');
 
   const order = print ? orders.find( order => order.uri === print.order) : null;
-  const model = order ? models.find( model => model.uri === order.model) : null;
+  const lineItem = print ? lineItems.find( lineItem => lineItem.uri === print.line_item) : null;
+  const model = lineItem ? models.find( model => model.uri === lineItem.model) : null;
 
   const fetching =
     state.ui.wyatt.print.get.fetching ||
-    state.ui.wyatt.order.get.fetching
+    state.ui.wyatt.print.list.fetching ||
+    state.ui.wyatt.order.get.fetching ||
+    state.ui.wyatt.event.list.fetching ||
+    state.ui.wyatt['line-item'].get.fetching ||
+    state.ui.hoth.model.get.fetching
 
   return {
     uuid,
     print,
     order,
+    lineItem,
+    events,
     model,
     fetching,
     apiErrors,
