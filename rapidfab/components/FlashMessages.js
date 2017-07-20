ab47e8412507b1e80151769e3bb41b0561a3d3b4
Ian Wright <ian@authentise.com>
2017-07-20 13:38:49 -0600

    Create FlashMessages component
    Reusable component for displaying error messages and removing errors
    from state.
    
    AE-4450 #time 2h

---
+++ rapidfab/components/FlashMessages.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Alert } from 'react-bootstrap';
+
+import Actions from 'rapidfab/actions';
+
+class FlashMessages extends Component {
+
+  componentWillUnmount() {
+    this.props.dispatch(Actions.UI.clearUIState());
+  }
+
+  render() {
+    const { errors } = this.props;
+    return (
+      <div>
+        { errors.map( (error, index) => (
+          <Alert bsStyle="danger" className='error-alert' key={index}>
+            <p>{error.title}</p>
+          </Alert>
+        ))}
+      </div>
+    );
+  }
+};
+
+FlashMessages.defaultProps = { errors: [] };
+FlashMessages.propTypes = { errors: PropTypes.array };
+
+const mapStateToProps = (state) => {
+  let errors = [];
+  for (const service in state.ui) {
+    for (const resource in state.ui[service]) {
+      for (const method in state.ui[service][resource]) {
+        errors = errors.concat( state.ui[service][resource][method].errors );
+      }
+    }
+  }
+  return { errors };
+};
+
+export default connect(mapStateToProps)(FlashMessages)
