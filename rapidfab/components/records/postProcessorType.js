7c294ffb535d89bfda0ec1bb34b5a06e6d9d84d8
Ian Wright <ian@authentise.com>
2017-08-10 11:57:52 -0600

    Fix eslint errors in c/r/postProcessorType.js
    AE-4564 #time 15m

--- rapidfab/components/records/postProcessorType.js
+++ rapidfab/components/records/postProcessorType.js
@@ -1,16 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import * as BS from 'react-bootstrap';
 import Fa from 'react-fontawesome';
 import { FormattedMessage } from 'react-intl';
 import Error from 'rapidfab/components/error';
 
-const SaveButtonTitle = ({ }) => (
+const SaveButtonTitle = () => (
   <span>
     <Fa name="floppy-o" /> <FormattedMessage id="button.save" defaultMessage="Save" />
   </span>
 );
 
-const PostProcessorTypeForm = ({ fields, bureaus, handleSubmit, onDelete, manufacturers, materials, apiErrors }) => (
+const PostProcessorTypeForm = ({
+  fields,
+  bureaus,
+  handleSubmit,
+  onDelete,
+  manufacturers,
+  materials,
+  apiErrors,
+}) => (
   <form onSubmit={handleSubmit}>
     <BS.Grid fluid>
       <BS.Row>
@@ -20,10 +29,21 @@ const PostProcessorTypeForm = ({ fields, bureaus, handleSubmit, onDelete, manufa
               <Fa name="list" /> <FormattedMessage id="inventory" defaultMessage="Inventory" />
             </BS.Breadcrumb.Item>
             <BS.Breadcrumb.Item href="#/inventory/post-processor-types">
-              <Fa name="object-group" /> <FormattedMessage id="inventory.postProcessorTypes" defaultMessage="Post Processor Types" />
+              <Fa name="object-group" />
+              {' '}
+              <FormattedMessage
+                id="inventory.postProcessorTypes"
+                defaultMessage="Post Processor Types"
+              />
             </BS.Breadcrumb.Item>
             <BS.Breadcrumb.Item>
-              <Fa name="object-group" /> {fields.id.value || <FormattedMessage id="record.postProcessorType.new" defaultMessage="New Post Processor Type" />}
+              <Fa name="object-group" />
+              { fields.id.value ||
+                <FormattedMessage
+                  id="record.postProcessorType.new"
+                  defaultMessage="New Post Processor Type"
+                />
+              }
             </BS.Breadcrumb.Item>
           </BS.Breadcrumb>
         </BS.Col>
@@ -32,14 +52,32 @@ const PostProcessorTypeForm = ({ fields, bureaus, handleSubmit, onDelete, manufa
       <BS.Row>
         <BS.Col xs={6}>
           <BS.Button href="#/inventory/post-processor-types" bsSize="small">
-            <Fa name="arrow-left" /> <FormattedMessage id="inventory.postProcessorTypes" defaultMessage="Post Processor Types" />
+            <Fa name="arrow-left" />
+            {' '}
+            <FormattedMessage
+              id="inventory.postProcessorTypes"
+              defaultMessage="Post Processor Types"
+            />
           </BS.Button>
         </BS.Col>
         <BS.Col xs={6}>
           <BS.ButtonToolbar className="pull-right">
-            <BS.SplitButton id="uxSaveDropdown" type="submit" bsStyle="success" bsSize="small" title={<SaveButtonTitle />} pullRight>
-              <BS.MenuItem eventKey={1} onClick={() => onDelete(fields.uuid.value)} disabled={!fields.id.value}>
-                <Fa name="ban" /> <FormattedMessage id="button.delete" defaultMessage="Delete" />
+            <BS.SplitButton
+              id="uxSaveDropdown"
+              type="submit"
+              bsStyle="success"
+              bsSize="small"
+              title={<SaveButtonTitle />}
+              pullRight
+            >
+              <BS.MenuItem
+                eventKey={1}
+                onClick={() => onDelete(fields.uuid.value)}
+                disabled={!fields.id.value}
+              >
+                <Fa name="ban" />
+                {' '}
+                <FormattedMessage id="button.delete" defaultMessage="Delete" />
               </BS.MenuItem>
             </BS.SplitButton>
           </BS.ButtonToolbar>
@@ -57,11 +95,15 @@ const PostProcessorTypeForm = ({ fields, bureaus, handleSubmit, onDelete, manufa
       <BS.Row>
         <BS.Col xs={12}>
           <BS.FormGroup controlId="uxName">
-            <BS.ControlLabel><FormattedMessage id="field.name" defaultMessage="Name" />:</BS.ControlLabel>
+            <BS.ControlLabel>
+              <FormattedMessage id="field.name" defaultMessage="Name" />:
+            </BS.ControlLabel>
             <BS.FormControl name="name" type="text" required {...fields.name} />
           </BS.FormGroup>
           <BS.FormGroup controlId="uxDescription">
-            <BS.ControlLabel><FormattedMessage id="field.description" defaultMessage="Description" />:</BS.ControlLabel>
+            <BS.ControlLabel>
+              <FormattedMessage id="field.description" defaultMessage="Description" />:
+            </BS.ControlLabel>
             <BS.FormControl name="description" type="text" {...fields.description} />
           </BS.FormGroup>
           <BS.FormGroup controlId="uxCost">
@@ -71,26 +113,41 @@ const PostProcessorTypeForm = ({ fields, bureaus, handleSubmit, onDelete, manufa
             <BS.FormControl name="cost" type="number" {...fields.cost} />
           </BS.FormGroup>
           <BS.FormGroup controlId="uxMaterials">
-            <BS.ControlLabel><FormattedMessage id="field.materials" defaultMessage="Materials" />:</BS.ControlLabel>
+            <BS.ControlLabel>
+              <FormattedMessage id="field.materials" defaultMessage="Materials" />:
+            </BS.ControlLabel>
             <BS.FormControl componentClass="select" multiple required {...fields.materials}>
-              {_.map(materials, material => (
+              { materials.map(material => (
                 <option key={material.uri} value={material.uri}>{material.name}</option>
               ))}
             </BS.FormControl>
           </BS.FormGroup>
           <BS.FormGroup controlId="uxManufacturer">
-            <BS.ControlLabel><FormattedMessage id="field.manufacturer" defaultMessage="Manufacturer" />:</BS.ControlLabel>
-            <BS.FormControl componentClass="select" placeholder="manufacturer" required {...fields.manufacturer}>
-              <option key="placeholder" value="" selected disabled>Select a Manufacturer</option>
-              {_.map(manufacturers, manufacturer => (
-                <option key={manufacturer.uri} value={manufacturer.uri}>{manufacturer.name}</option>
+            <BS.ControlLabel>
+              <FormattedMessage id="field.manufacturer" defaultMessage="Manufacturer" />:
+            </BS.ControlLabel>
+            <BS.FormControl
+              componentClass="select"
+              placeholder="manufacturer"
+              required
+              {...fields.manufacturer}
+            >
+              <option key="placeholder" value="" selected disabled>
+                Select a Manufacturer
+              </option>
+              { manufacturers.map(manufacturer => (
+                <option key={manufacturer.uri} value={manufacturer.uri}>
+                  {manufacturer.name}
+                </option>
               ))}
             </BS.FormControl>
           </BS.FormGroup>
           <BS.FormGroup style={{ display: 'none' }} controlId="uxBureau">
-            <BS.ControlLabel><FormattedMessage id="field.bureau" defaultMessage="Bureau" />:</BS.ControlLabel>
+            <BS.ControlLabel>
+              <FormattedMessage id="field.bureau" defaultMessage="Bureau" />:
+            </BS.ControlLabel>
             <BS.FormControl componentClass="select" placeholder="bureau" {...fields.bureau}>
-              {_.map(bureaus, bureau => (
+              { bureaus.map(bureau => (
                 <option key={bureau.uri} value={bureau.uri}>{bureau.uri}</option>
               ))}
             </BS.FormControl>
@@ -100,5 +157,15 @@ const PostProcessorTypeForm = ({ fields, bureaus, handleSubmit, onDelete, manufa
     </BS.Grid>
   </form>
 );
+
+PostProcessorTypeForm.propTypes = {
+  fields: PropTypes.object,
+  bureaus: PropTypes.array,
+  handleSubmit: PropTypes.func,
+  onDelete: PropTypes.func,
+  manufacturers: PropTypes.array,
+  materials: PropTypes.array,
+  apiErrors: PropTypes.array,
+};
 
 export default PostProcessorTypeForm;
