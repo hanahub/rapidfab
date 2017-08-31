f7fa3ae22ceeac24b92a7fb6f43423e50e4efe77
Ian Wright <ian@authentise.com>
2017-08-31 10:03:49 -0600

    Fix react/no-unescaped-entities
    Apostrophes mean stuff in jsx, so try not to use them.
    
    AE-4688 #time 5m

--- rapidfab/components/404.jsx
+++ rapidfab/components/404.jsx
@@ -6,7 +6,7 @@ const NotFound = () =>
   <div className="text-center">
     <h1>404</h1>
     <p>
-      You seem to have reached a link that doesn't go anywhere. Maybe you want{' '}
+      You seem to have reached a link that does not go anywhere. Maybe you want{' '}
       <a href="#/">to go back to the beginning?</a>
     </p>
   </div>;
