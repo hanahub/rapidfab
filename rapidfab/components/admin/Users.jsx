6a87fdfd2a679bdf6d89cae81f2077859ef71a58
Ian Wright <ian@authentise.com>
2017-10-11 10:47:40 -0600

    Add styling to Users
    AE-4972 #time 15m

--- rapidfab/components/admin/Users.jsx
+++ rapidfab/components/admin/Users.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {
-  ListGroup,
-  ListGroupItem,
-} from 'react-bootstrap';
+import Fa from 'react-fontawesome';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 const Users = ({ users }) => (
   <ListGroup>
     { users.map(user =>
-      <ListGroupItem>
-        {user.name}
+      <ListGroupItem className="clearfix" onClick={ () => ({})}>
+        <Fa className="pull-left" name="user" size="2x" />
+        <span className="pull-right">{user.name}</span>
       </ListGroupItem>
     )}
   </ListGroup>
