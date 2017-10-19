355339a2f2a2ac63d18e6e215c778d36aaba60d2
Ian Wright <ian@authentise.com>
2017-10-19 14:27:43 -0600

    Remove unused import from LineItemEditFormContainer
    AE-5030 #time 5m

--- rapidfab/containers/records/order/LineItemEditFormContainer.jsx
+++ rapidfab/containers/records/order/LineItemEditFormContainer.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { extractUuid } from 'rapidfab/reducers/makeApiReducers';
 import * as Selectors from 'rapidfab/selectors';
 import Actions from 'rapidfab/actions';
 
