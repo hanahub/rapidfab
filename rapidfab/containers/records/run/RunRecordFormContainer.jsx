2eab5ac2d05ae50c3feb2751a7a0b24b25d322d3
Ian Wright <ian@authentise.com>
2017-10-26 13:15:29 -0600

    Create RunRecordForm component and container
    Got rid of Redux-Form. Needed to rewrite all of the form functions
    without it
    
    Also create helper component ResourceLink
    
    AE-5088 #time 2h

---
+++ rapidfab/containers/records/run/RunRecordFormContainer.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import Actions from 'rapidfab/actions';
+import { extractUuid } from 'rapidfab/reducers/makeApiReducers';
+import { getRouteUUID, getRouteUUIDResource } from 'rapidfab/selectors';
+
+import RunRecordForm from 'rapidfab/components/records/run/RunRecordForm';
+
+class RunRecordFormContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const { initialNotes, initialStatus } = this.props;
+    this.state = {
+      notes: initialNotes || null,
+      status: initialStatus || null,
+    };
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.dispatch(Actions.Api.wyatt.run.get(this.props.uuid))
+      .then(response => {
+        debugger
+      });
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.isFetching && !this.props.isFetching) {
+      const { initialNotes, initialStatus } = this.props;
+      this.setState({
+        notes: initialNotes,
+        status: initialStatus,
+      });
+    }
+  }
+
+  handleDelete() {
+    this.props
+      .dispatch(Actions.Api.wyatt.run.delete(this.props.uuid))
+      .then(() => {
+        window.location.hash = '#/plan/runs';
+      });
+  }
+
+  handleInputChange(event) {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit() {
+    const { notes, status } = this.state;
+    const payload = Object.assign(
+      {},
+      { notes },
+      status !== this.props.initialStatus ? { status } : null
+    );
+    this.props.dispatch(Actions.Api.wyatt.run.put(this.props.uuid, payload));
+  }
+
+  render() {
+    return (
+      <RunRecordForm
+        {...this.state}
+        {...this.props}
+        handleDelete={this.handleDelete}
+        handleInputChange={this.handleInputChange}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+  }
+}
+
+RunRecordFormContainer.defaultProps = {
+  initialNotes: null,
+  initialStatus: null,
+};
+
+RunRecordFormContainer.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  initialNotes: PropTypes.string,
+  initialStatus: PropTypes.string,
+  isFetching: PropTypes.bool.isRequired,
+  uuid: PropTypes.string,
+};
+
+const mapStateToProps = state => {
+  const uuid = getRouteUUID(state);
+  const run = getRouteUUIDResource(state);
+  return Object.assign(
+    {
+      isFetching: state.ui.wyatt.run.get.fetching,
+      isSaving: state.ui.wyatt.run.put.fetching,
+      uuid,
+    },
+    run
+      ? {
+          created: run.created,
+          initialNotes: run.notes,
+          initialStatus: run.status,
+          postProcessor: run.post_processor,
+          printerType: run.printer_type,
+          printer: run.printer,
+          model: run.model,
+        }
+      : null
+  );
+};
+
+export default connect(mapStateToProps)(RunRecordFormContainer);
