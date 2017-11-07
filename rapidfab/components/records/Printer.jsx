096e8d6ef5b011060d478a9744dc680672f79f85
Ian Wright <ian@authentise.com>
2017-11-07 12:04:05 -0700

    Add handleSubmit function for Printer
    AE-5191 30m

--- rapidfab/components/records/Printer.jsx
+++ rapidfab/components/records/Printer.jsx
@@ -15,6 +15,7 @@ import {
 
 import BreadcrumbNav from 'rapidfab/components/BreadcrumbNav';
 import FlashMessages from 'rapidfab/components/FlashMessages';
+import Loading from 'rapidfab/components/Loading';
 import SaveButtonTitle from 'rapidfab/components/SaveButtonTitle';
 
 const Printer = ({
@@ -23,6 +24,7 @@ const Printer = ({
   handleDelete,
   printerType,
   printerTypes,
+  loading,
   location,
   locations,
   modeler,
@@ -31,107 +33,105 @@ const Printer = ({
 }) => (
     <Grid fluid>
       <BreadcrumbNav breadcrumbs={['printers', uuid || 'New']} />
-      <div className="clearfix">
-        <ButtonToolbar className="pull-right">
-          <SplitButton
-            id="uxSaveDropdown"
-            type="submit"
-            bsStyle="success"
-            bsSize="small"
-            title={<SaveButtonTitle />}
-            pullRight
-          >
-            <MenuItem
-              eventKey={1}
-              onClick={handleDelete}
-              disabled={!uuid}
-            >
-              <Fa name="ban" />{' '}
-              <FormattedMessage id="button.delete" defaultMessage="Delete" />
-            </MenuItem>
-          </SplitButton>
-        </ButtonToolbar>
-      </div>
-
-      <hr />
-
       <FlashMessages />
+      {loading ? <Loading /> :
+        <form onSubmit={handleSubmit}>
+          <div className="clearfix">
+            <ButtonToolbar className="pull-right">
+              <SplitButton
+                id="uxSaveDropdown"
+                type="submit"
+                bsStyle="success"
+                bsSize="small"
+                title={<SaveButtonTitle />}
+                pullRight
+              >
+                <MenuItem
+                  eventKey={1}
+                  onClick={handleDelete}
+                  disabled={!uuid}
+                >
+                  <Fa name="ban" />{' '}
+                  <FormattedMessage id="button.delete" defaultMessage="Delete" />
+                </MenuItem>
+              </SplitButton>
+            </ButtonToolbar>
+          </div>
 
-      <form onSubmit={handleSubmit}>
+          <hr />
 
-        <FormGroup controlId="uxName">
-          <ControlLabel>
-            <FormattedMessage id="field.name" defaultMessage="Name" />
-          </ControlLabel>
-          <FormControl
-            name="name"
-            onChange={handleInputChange}
-            required
-            type="text"
-            value={name}
-          />
-        </FormGroup>
+            <FormGroup controlId="uxName">
+              <ControlLabel>
+                <FormattedMessage id="field.name" defaultMessage="Name" />
+              </ControlLabel>
+              <FormControl
+                name="name"
+                onChange={handleInputChange}
+                required
+                type="text"
+                value={name}
+              />
+            </FormGroup>
 
-        <FormGroup controlId="uxPrinterType">
-          <ControlLabel>
-            <FormattedMessage
-              id="field.printerType"
-              defaultMessage="Printer Type"
-            />
-          </ControlLabel>
-          <FormControl
-            name="printerType"
-            onChange={handleInputChange}
-            componentClass="select"
-            required
-            value={printerType}
-          >
-            {printerTypes.map(printerType => (
-              <option
-                key={printerType.uri}
-                value={printerType.uri}
-              >{`${printerType.name} - ${printerType.id}`}</option>
-            ))}
-          </FormControl>
-        </FormGroup>
+            <FormGroup controlId="uxPrinterType">
+              <ControlLabel>
+                <FormattedMessage
+                  id="field.printerType"
+                  defaultMessage="Printer Type"
+                />
+              </ControlLabel>
+              <FormControl
+                name="printerType"
+                onChange={handleInputChange}
+                componentClass="select"
+                required
+                value={printerType}
+              >
+                {printerTypes.map(printerType => (
+                  <option
+                    key={printerType.uri}
+                    value={printerType.uri}
+                  >{`${printerType.name} - ${printerType.id}`}</option>
+                ))}
+              </FormControl>
+            </FormGroup>
 
-        <FormGroup controlId="uxLocation">
-          <ControlLabel>
-            <FormattedMessage
-              id="field.location"
-              defaultMessage="Location"
-            />
-          </ControlLabel>
-          <FormControl
-            name="location"
-            componentClass="select"
-            onChange={handleInputChange}
-            required
-            value={location}
-          >
-            {locations.map(location => (
-              <option
-                key={location.uri}
-                value={location.uri}
-              >{`${location.name} - ${location.id}`}</option>
-            ))}
-          </FormControl>
-        </FormGroup>
+            <FormGroup controlId="uxLocation">
+              <ControlLabel>
+                <FormattedMessage
+                  id="field.location"
+                  defaultMessage="Location"
+                />
+              </ControlLabel>
+              <FormControl
+                name="location"
+                componentClass="select"
+                onChange={handleInputChange}
+                required
+                value={location}
+              >
+                {locations.map(location => (
+                  <option
+                    key={location.uri}
+                    value={location.uri}
+                  >{`${location.name} - ${location.id}`}</option>
+                ))}
+              </FormControl>
+            </FormGroup>
 
-        <FormGroup controlId="uxModeler">
-          <ControlLabel>
-            <FormattedMessage id="field.modeler" defaultMessage="Modeler" />
-          </ControlLabel>
-          <FormControl
-            name="modeler"
-            onChange={handleInputChange}
-            required
-            type="text"
-            value={modeler}
-          />
-        </FormGroup>
-
-      </form>
+            <FormGroup controlId="uxModeler">
+              <ControlLabel>
+                <FormattedMessage id="field.modeler" defaultMessage="Modeler" />
+              </ControlLabel>
+              <FormControl
+                name="modeler"
+                onChange={handleInputChange}
+                type="text"
+                value={modeler}
+              />
+            </FormGroup>
+        </form>
+      }
     </Grid>
 );
 
--- rapidfab/containers/records/PrinterContainer.jsx
+++ rapidfab/containers/records/PrinterContainer.jsx
@@ -16,10 +16,13 @@ class PrinterContainer extends Component {
   constructor(props) {
     super(props)
 
+    const { locations, printerTypes } = this.props;
+
     this.state = {
       name: '',
-      printerType: '',
-      location: '',
+      printerType: printerTypes.length ? printerTypes[0].uri : null,
+      loading: true,
+      location: locations.length ? locations[0].uri : null,
       modeler: '',
     };
 
@@ -29,15 +32,26 @@ class PrinterContainer extends Component {
 
   componentDidMount() {
     const { dispatch, uuid } = this.props;
-    dispatch(Actions.Api.wyatt.location.list());
-    dispatch(Actions.Api.wyatt['printer-type'].list());
-    if (uuid) {
-      dispatch(Actions.Api.wyatt.printer.get(uuid));
+    const locationRequest = dispatch(Actions.Api.wyatt.location.list());
+    const printerTypeRequest = dispatch(Actions.Api.wyatt['printer-type'].list())
+    const printerRequest = uuid ? dispatch(Actions.Api.wyatt.printer.get(uuid)) : null;
+    Promise.all([locationRequest, printerTypeRequest, printerRequest]).then(() => {
+      this.setState({ loading: false });
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.locations.length === 0 && nextProps.locations.length > 0) {
+      this.setState({ location: nextProps.locations[0].uri })
+    }
+    if (this.props.printerTypes.length === 0 && nextProps.printerTypes.length > 0) {
+      this.setState({ printerType: nextProps.printerTypes[0].uri })
     }
   }
 
   handleDelete() {
-    console.log('delete');
+    const { dispatch, uuid} = this.props;
+    dispatch(Actions.Api.wyatt.printer.delete(uuid)).then(redirect);
   }
 
   handleInputChange(event) {
@@ -45,8 +59,30 @@ class PrinterContainer extends Component {
     this.setState({ [name]: value });
   }
 
-  handleSubmit() {
-    console.log('submit');
+  handleSubmit(event) {
+    event.preventDefault();
+    const { dispatch, uuid } = this.props;
+    const {
+      name,
+      printerType,
+      location,
+      modeler,
+    } = this.state;
+    const payload = {
+      name,
+      printer_type: printerType,
+      location,
+      modeler,
+    };
+    if (uuid) {
+      dispatch(
+        Actions.Api.wyatt.printer.put(uuid, payload)
+      ).then(redirect);
+    } else {
+      dispatch(Actions.Api.wyatt.printer.post(payload)).then(
+        redirect
+      );
+    }
   }
 
   render() {
@@ -69,39 +105,14 @@ PrinterContainer.defaultProps = {
 
 PrinterContainer.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  locations: PropTypes.arrayOf(PropTypes.object).isRequired,
   uuid: PropTypes.string,
 };
-
-
-function mapDispatchToProps(dispatch) {
-  return {
-    onSubmit: payload => {
-      const validatedPayload = Object.assign({}, payload, {
-        modeler: payload.modeler ? payload.modeler : '',
-      });
-      if (payload.uuid) {
-        dispatch(
-          Actions.Api.wyatt.printer.put(payload.uuid, validatedPayload)
-        ).then(redirect);
-      } else {
-        dispatch(Actions.Api.wyatt.printer.post(validatedPayload)).then(
-          redirect
-        );
-      }
-    },
-    onDelete: uuid => {
-      if (uuid) {
-        dispatch(Actions.Api.wyatt.printer.delete(uuid)).then(redirect);
-      }
-    },
-  };
-}
 
 function mapStateToProps(state, props) {
   return {
     uuid: Selectors.getRoute(state, props).uuid,
     initialValues: Selectors.getRouteResource(state, props),
-    submitting: Selectors.getResourceFetching(state, 'wyatt.printer'),
     locations: Selectors.getLocations(state),
     printerTypes: Selectors.getPrinterTypes(state),
   };
