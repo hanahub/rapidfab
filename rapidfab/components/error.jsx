62c4765c239d80cc1849bc963bce253cd9ce48d2
Ian Wright <ian@authentise.com>
2017-12-05 13:12:37 -0700

    Disable no-array-index-key line error on Error
    cause errors do not have a unique identifier so we need to use the index
    
    AE-5383 #time 5m

--- rapidfab/components/error.jsx
+++ rapidfab/components/error.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Alert } from 'react-bootstrap';
 
+/* eslint-disable react/no-array-index-key */
 const Errors = ({ errors }) => (
   <div>
     {errors.map((error, index) => (
-      <Alert bsStyle="danger" className="error-alert" key={index}>
+      <Alert
+        bsStyle="danger"
+        className="error-alert"
+        key={`${index}-${error.code}`}
+      >
         <p>{error.title}</p>
       </Alert>
     ))}
   </div>
 );
+/* eslint-enable react/no-array-index-key */
 
 Errors.defaultProps = { errors: [] };
 
