868c2c10be1a2d75bc6f5595a98f58ec6508e81f
Ian Wright <ian@authentise.com>
2017-12-06 12:01:25 -0700

    Create isFetchingInitial util function
    and refactor postProcessorTypes to use it
    
    was getting tired of typing the same conditional everywhere,
    that's what functions are for!
    
    AE-5397 #time 30m

---
+++ rapidfab/utils/isFetchingInitial.js
@@ -0,0 +1,8 @@
+// arguments should be ui resource request objects from redux store
+// e.g. state.ui.wyatt.order.get
+
+export default function isFetchingInitial() {
+  return Array.from(arguments).some(api =>
+    api.count === 0 || (api.count === 1 && api.fetching)
+  );
+};
