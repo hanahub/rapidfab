e281db8b8eb097d603b07e0894dc4946621d098d
Eli Ribble <eli@authentise.com>
2017-12-19 10:31:23 -0700

    Add API requests to get run queue for printer in order
    This should help in displaying the current schedule for the printer so
    the user can make an educated decision about when to schedule this new
    run
    
    AE-5511 #time 1h

--- rapidfab/containers/records/run/RunRecordScheduleContainer.jsx
+++ rapidfab/containers/records/run/RunRecordScheduleContainer.jsx
@@ -3,17 +3,49 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import { getRouteUUIDResource } from 'rapidfab/selectors';
+import extractUuid from 'rapidfab/utils/extractUuid';
 import Actions from 'rapidfab/actions';
 
+import RunRecordSchedule from 'rapidfab/components/records/run/RunRecordSchedule';
+
 class RunRecordScheduleContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      runQueue: null,
+    };
+  }
   componentWillMount() {
     const { dispatch, route: { uuid } } = this.props;
     dispatch(Actions.RouteUUID.setRouteUUID(uuid));
-    dispatch(Actions.Api.wyatt.run.get(uuid));
+    dispatch(Actions.Api.wyatt.run.get(uuid)).then(runResponse => {
+      const printer = runResponse.json.printer;
+      const printerUUID = extractUuid(printer);
+      const onPrinterResponse = dispatch(
+        Actions.Api.wyatt.printer.get(printerUUID)
+      );
+      const onRunsResponse = dispatch(
+        Actions.Api.wyatt.run.list({
+          printer,
+        })
+      );
+      Promise.all([
+        onPrinterResponse,
+        onRunsResponse,
+      ]).then((responses) => {
+        const printerResponse = responses[0];
+        const runsResponse = responses[1];
+        const runQueue = printerResponse.json.queue.map(runURI =>
+          runsResponse.json.resources.find(run => run.uri === runURI)
+        );
+        this.setState({ runQueue });
+      });
+    });
   }
 
   render() {
-    return <p>yo</p>;
+    return <RunRecordSchedule {...this.props} {...this.state} />;
   }
 }
 
