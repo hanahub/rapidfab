351aafa777aa43ebf81685c6826f7f662de1f39c
Ian Wright <ian@authentise.com>
2017-12-19 10:46:31 -0700

    Add form to RunRecordSchedule
    AE-5511 #time 1h

--- rapidfab/components/records/run/RunRecordSchedule.jsx
+++ rapidfab/components/records/run/RunRecordSchedule.jsx
@@ -1,5 +1,98 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const RunRecordSchedule = () => <p>yo</p>;
+import { FormattedMessage } from 'react-intl';
+import {
+  Button,
+  ControlLabel,
+  Form,
+  FormControl,
+  FormGroup,
+  Grid,
+  HelpBlock,
+  PageHeader,
+} from 'react-bootstrap';
+
+import { RUN_OPERATION_MAP } from 'rapidfab/mappings';
+
+import BreadcrumbNav from 'rapidfab/components/BreadcrumbNav';
+import FlashMessages from 'rapidfab/components/FlashMessages';
+
+const RunRecordSchedule = ({
+  handleInputChange,
+  handleSubmit,
+  id,
+  isStartValid,
+  operation,
+  startDate,
+  startTime,
+}) => (
+  <Grid fluid>
+    <BreadcrumbNav breadcrumbs={['runs', id]} />
+    <PageHeader>
+      Rescheduling
+      {` `}
+      {operation ? RUN_OPERATION_MAP[operation] : null}
+      {` `}
+      Run
+    </PageHeader>
+    <FlashMessages />
+    <Form onSubmit={handleSubmit}>
+      <FormGroup
+        controlId="uxName"
+        validationState={isStartValid ? null : 'error'}
+      >
+        <ControlLabel>Start Date</ControlLabel>
+        <FormControl
+          name="startDate"
+          onChange={handleInputChange}
+          required
+          type="date"
+          value={startDate}
+        />
+      </FormGroup>
+      <FormGroup
+        controlId="uxName"
+        validationState={isStartValid ? null : 'error'}
+      >
+        <ControlLabel>Start Time</ControlLabel>
+        <FormControl
+          name="startTime"
+          onChange={handleInputChange}
+          required
+          type="time"
+          value={startTime}
+        />
+        {!isStartValid && (
+          <HelpBlock>
+            <FormattedMessage
+              id="validateStartTime"
+              defaultValue="Must begin in the future"
+            />
+          </HelpBlock>
+        )}
+      </FormGroup>
+      <Button block disabled={!isStartValid} type="submit">
+        Reschedule Run
+      </Button>
+    </Form>
+  </Grid>
+);
+
+RunRecordSchedule.defaultProps = {
+  isStartValid: true,
+  startDate: null,
+  startTime: null,
+};
+
+RunRecordSchedule.propTypes = {
+  handleInputChange: PropTypes.func.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  isStartValid: PropTypes.bool,
+  id: PropTypes.string.isRequired,
+  operation: PropTypes.string.isRequired,
+  startDate: PropTypes.string,
+  startTime: PropTypes.string,
+};
 
 export default RunRecordSchedule;
--- rapidfab/containers/records/run/RunRecordScheduleContainer.jsx
+++ rapidfab/containers/records/run/RunRecordScheduleContainer.jsx
@@ -5,22 +5,52 @@ import { connect } from 'react-redux';
 import { getRouteUUIDResource } from 'rapidfab/selectors';
 import Actions from 'rapidfab/actions';
 
+import Loading from 'rapidfab/components/Loading';
 import RunRecordSchedule from 'rapidfab/components/records/run/RunRecordSchedule';
 
 class RunRecordScheduleContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      startTime: null,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
   componentWillMount() {
     const { dispatch, route: { uuid } } = this.props;
     dispatch(Actions.RouteUUID.setRouteUUID(uuid));
     dispatch(Actions.Api.wyatt.run.get(uuid));
   }
 
+  handleInputChange(event) {
+    const { value, name } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit() {
+    console.log('submit');
+  }
+
   render() {
-    return <RunRecordSchedule />;
+    return this.props.isRunFetching ? (
+      <Loading />
+    ) : (
+      <RunRecordSchedule
+        {...this.props}
+        {...this.state}
+        handleSubmit={this.handleSubmit}
+        handleInputChange={this.handleInputChange}
+      />
+    );
   }
 }
 
 RunRecordScheduleContainer.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  isRunFetching: PropTypes.bool.isRequired,
   route: PropTypes.shape({ uuid: PropTypes.string }).isRequired,
 };
 
