987e817af4444c2fe4af661daffbc3ede568a032
Eli Ribble <eli@authentise.com>
2017-12-22 16:10:06 -0700

    Add default config for test time

--- rapidfab/config.js
+++ rapidfab/config.js
@@ -1,1 +1,12 @@
-module.exports = window.Config;
+module.exports = window.Config || {
+  SENTRY_DSN: null,
+  HOST: {
+    PAO: 'https://users.authentise.test',
+    WYATT: 'https://erp.authentise.test',
+    EVENT: 'https://events.authentise.test/',
+    HOTH: 'https://models.authentise.test/',
+    SCYLLA: 'https://app.authentise.test',
+    NAUTILUS: 'https://data.authentise.test',
+  },
+  TOS_URL: 'https://authentise.com/tech/etos.html',
+};
