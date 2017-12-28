8005e095bfc1d8385e12c991075104a7f8649ad4
Ian Wright <ian@authentise.com>
2017-12-28 11:15:11 -0700

    Remove unused code from order/new
    This logic is in NewOrder.jsx now, so this is all vestigial code
    
    I got burned by this still in here =(
    
    AE-5571 #time 30m

--- rapidfab/containers/records/order/new.jsx
+++ rapidfab/containers/records/order/new.jsx
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import Actions from 'rapidfab/actions';
-import { extractUuid } from 'rapidfab/reducers/makeApiReducers';
 import isFetchingInitial from 'rapidfab/utils/isFetchingInitial';
 import * as Selectors from 'rapidfab/selectors';
 
@@ -71,57 +70,8 @@ function mapDispatchToProps(dispatch) {
       dispatch(Actions.Api.wyatt.template.list({ bureau: uri }));
       dispatch(Actions.Api.pao.users.list());
     },
-    onSaveOrder: payload => {
-      if (payload) {
-        dispatch(Actions.UploadModel.storePayload(null));
-        dispatch(Actions.Api.wyatt.order.post(payload)).then(args => {
-          window.location.hash = `#/records/order/${extractUuid(
-            args.headers.location
-          )}`;
-        });
-      }
-    },
     onUnmount: () => {
       dispatch(Actions.UploadModel.clearState());
-    },
-    onSubmit: payload => {
-      const validatedPayload = Object.assign({}, payload);
-      if (!!validatedPayload.materials.support === false)
-        delete validatedPayload.materials.support;
-      if (!!validatedPayload.shipping.name === false)
-        delete validatedPayload.shipping.name;
-      if (!!validatedPayload.shipping.address === false)
-        delete validatedPayload.shipping.address;
-      if (!!validatedPayload.shipping.tracking === false)
-        delete validatedPayload.shipping.tracking;
-      if (!!validatedPayload.third_party_provider === false)
-        delete validatedPayload.third_party_provider;
-      if (!!validatedPayload.post_processor_type === false)
-        delete validatedPayload.post_processor_type;
-
-      if (!validatedPayload.model) {
-        dispatch(Actions.Api.wyatt.order.post(validatedPayload)).then(args => {
-          window.location.hash = `#/records/order/${extractUuid(
-            args.headers.location
-          )}`;
-        });
-      } else {
-        dispatch(
-          Actions.Api.hoth.model.post({
-            name: validatedPayload.name,
-            type: 'stl',
-          })
-        ).then(args => {
-          dispatch(
-            Actions.UploadModel.upload(
-              args.headers.uploadLocation,
-              validatedPayload.model[0]
-            )
-          );
-          validatedPayload.model = args.headers.location;
-          dispatch(Actions.UploadModel.storePayload(validatedPayload));
-        });
-      }
     },
   };
 }
