583bb387f231078f8b78783bb81361b27c716b1d
Eli Ribble <eli@authentise.com>
2017-12-29 14:25:44 -0700

    Update dev default config to work better
    The problem is that we don't want to have to run all of our own copies
    of the service to test our own code locally
    
    AE-5601 #time 10m

--- rapidfab/config.js
+++ rapidfab/config.js
@@ -1,12 +1,12 @@
 module.exports = window.Config || {
   SENTRY_DSN: null,
   HOST: {
-    PAO: 'https://users.authentise.test',
-    WYATT: 'https://erp.authentise.test',
-    EVENT: 'https://events.authentise.test/',
-    HOTH: 'https://models.authentise.test/',
-    SCYLLA: 'https://app.authentise.test',
-    NAUTILUS: 'https://data.authentise.test',
+    PAO: 'https://users.dev-auth.com',
+    WYATT: 'https://erp.dev-auth.com',
+    EVENT: 'https://events.dev-auth.com/',
+    HOTH: 'https://models.dev-auth.com/',
+    SCYLLA: 'https://app.dev-auth.com',
+    NAUTILUS: 'https://data.dev-auth.com',
   },
   TOS_URL: 'https://authentise.com/tech/etos.html',
 };
