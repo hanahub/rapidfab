3f0d92bd2c052e56e179835cc4af4a3b2e4aaf3a
Eli Ribble <eli@authentise.com>
2017-12-29 14:26:21 -0700

    Remove model resource LIST from print page
    This was added a really long time ago by Scott Thorpe to support showing
    the model information on a given print. We don't actually need it any
    more because we load up the model individuall rather than by getting it
    out of the list of resources
    
    AE-5601 #time 1h

--- rapidfab/containers/records/print.jsx
+++ rapidfab/containers/records/print.jsx
@@ -42,7 +42,6 @@ function mapDispatchToProps(dispatch) {
     onInitialize: props => {
       const { bureau } = props;
       dispatch(Actions.RouteUUID.setRouteUUID(props.route.uuid));
-      dispatch(Actions.Api.hoth.model.list());
       dispatch(Actions.Api.wyatt.material.list({ bureau: bureau.uri }));
       dispatch(Actions.Api.wyatt.template.list({ bureau: bureau.uri }));
       dispatch(Actions.Api.wyatt.shipping.list({ bureau: bureau.uri }));
