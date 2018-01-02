56c3958bd9cf3120f103d097ec998332bf7eef4c
Eli Ribble <eli@authentise.com>
2018-01-02 15:28:26 -0700

    Show more information on about page
    Like all the environment variables :)
    
    Should make it easier to debug what is going wrong with the deployment

--- rapidfab/components/about.jsx
+++ rapidfab/components/about.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import Version from 'rapidfab/version';
 
 const About = () => (
-  <dl>
-    <dt>Version:</dt>
-    <dd>{Version}</dd>
-  </dl>
+  <div>
+    <dl>
+      <dt>Commit hash:</dt>
+      <dd>{process.env.COMMIT_HASH}</dd>
+    </dl>
+    <dl>
+      <dt>Git describe:</dt>
+      <dd>{process.env.GITDESCRIBE}</dd>
+    </dl>
+    <dl>
+      <dt>Node environment:</dt>
+      <dd>{process.env.NODE_ENV}</dd>
+    </dl>
+  </div>
 );
 
 export default About;
