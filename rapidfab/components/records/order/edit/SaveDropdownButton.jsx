662766ff2bd20ca06c0fdac55f98646bcb481047
Ian Wright <ian@authentise.com>
2018-01-19 10:49:41 -0700

    Add modal confirmation to SaveDropdownButton
    so taht users don't accidentally do actions they can't take back
    
    AE-5771 #time 1h

--- rapidfab/components/records/order/edit/SaveDropdownButton.jsx
+++ rapidfab/components/records/order/edit/SaveDropdownButton.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Fa from 'react-fontawesome';
 import { FormattedMessage } from 'react-intl';
 import {
+  Button,
   ButtonToolbar,
   MenuItem,
+  Modal,
   OverlayTrigger,
   SplitButton,
   Tooltip,
@@ -14,7 +16,13 @@ import SaveButtonTitle from 'rapidfab/components/SaveButtonTitle';
 
 const SaveTooltip = <Tooltip id="saved">Saved</Tooltip>;
 
-const SaveDropdownButton = ({ onCancel, onDelete, onSubmit }) => (
+const SaveDropdownButton = ({
+  cancellable,
+  handleConfirm,
+  handleOpen,
+  onSubmit,
+  modal,
+}) => (
   <ButtonToolbar className="clearfix">
     <div className="pull-right">
       <OverlayTrigger
@@ -32,14 +40,14 @@ const SaveDropdownButton = ({ onCancel, onDelete, onSubmit }) => (
           title={<SaveButtonTitle />}
           onClick={onSubmit}
         >
-          {onCancel && (
-            <MenuItem eventKey={0} onClick={onCancel}>
+          {cancellable && (
+            <MenuItem eventKey={0} onClick={() => handleOpen('cancel')}>
               <Fa name="ban" />
               {` `}
               <FormattedMessage id="button.cancel" defaultMessage="Cancel" />
             </MenuItem>
           )}
-          <MenuItem eventKey={1} onClick={onDelete}>
+          <MenuItem eventKey={1} onClick={() => handleOpen('delete')}>
             <Fa name="times" />
             {` `}
             <FormattedMessage id="button.delete" defaultMessage="Delete" />
@@ -47,17 +55,80 @@ const SaveDropdownButton = ({ onCancel, onDelete, onSubmit }) => (
         </SplitButton>
       </OverlayTrigger>
     </div>
+    <Modal show={modal} onHide={() => handleOpen('')}>
+      <Modal.Body>
+        <p>Are you really sure you want to {modal}?</p>
+        <p>This cannot be undone.</p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button bsStyle="danger" onClick={handleConfirm}>
+          {modal}
+        </Button>
+        <Button bsStyle="primary" onClick={() => handleOpen('')}>
+          Back
+        </Button>
+      </Modal.Footer>
+    </Modal>
   </ButtonToolbar>
 );
 
 SaveDropdownButton.defaultProps = {
-  onCancel: null,
+  onSubmit: null,
 };
 
 SaveDropdownButton.propTypes = {
-  onCancel: PropTypes.func,
-  onDelete: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+  cancellable: PropTypes.bool.isRequired,
+  handleConfirm: PropTypes.func.isRequired,
+  handleOpen: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func,
+  modal: PropTypes.string.isRequired,
 };
 
-export default SaveDropdownButton;
+class SaveDropdownButtonContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { modal: '' };
+    this.handleConfirm = this.handleConfirm.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
+  }
+
+  handleConfirm() {
+    const { modal } = this.state;
+    if (modal === 'cancel') {
+      this.props.onCancel();
+    } else if (modal === 'delete') {
+      this.props.onDelete();
+    }
+    this.handleOpen('');
+  }
+
+  handleOpen(modal) {
+    this.setState({ modal });
+  }
+
+  render() {
+    return (
+      <SaveDropdownButton
+        {...this.props}
+        {...this.state}
+        cancellable={!!this.props.onCancel}
+        handleConfirm={this.handleConfirm}
+        handleOpen={this.handleOpen}
+      />
+    );
+  }
+}
+
+SaveDropdownButtonContainer.defaultProps = {
+  onCancel: null,
+  onSubmit: null,
+};
+
+SaveDropdownButtonContainer.propTypes = {
+  onCancel: PropTypes.func,
+  onDelete: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func,
+};
+
+export default SaveDropdownButtonContainer;
