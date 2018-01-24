845e81972e1af2a288f5a42ae05f4f8ab80d68d4
Ian Wright <ian@authentise.com>
2018-01-24 14:35:04 -0700

    Add redirect to QuotingContainer
    so that users without the feature are redirecting back to home if they
    go to that route
    
    AE-5811 #time 1h

--- rapidfab/containers/quoting/QuotingContainer.jsx
+++ rapidfab/containers/quoting/QuotingContainer.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Actions from 'rapidfab/actions';
+import { isQuotingFeatureEnabled } from 'rapidfab/selectors';
+
 import Quoting from 'rapidfab/components/quoting/Quoting';
 
 class QuotingContainer extends Component {
+  componentDidMount() {
+    this.props.dispatch(Actions.Api.wyatt.feature.list());
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (!nextProps.loading && !nextProps.isQuotingFeatureEnabled) {
+      window.location.hash = '';
+    }
+  }
+
   render() {
     return <Quoting {...this.props} />;
   }
 }
 
-export default QuotingContainer;
+QuotingContainer.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  isQuotingFeatureEnabled: PropTypes.bool.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
+
+export default connect(state => ({
+  isQuotingFeatureEnabled: isQuotingFeatureEnabled(state),
+  loading:
+    state.ui.wyatt.feature.list.count > 0 ||
+    state.ui.wyatt.feature.list.fetching,
+}))(QuotingContainer);
