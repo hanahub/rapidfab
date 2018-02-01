d82a92bafd2af44c725ecbc02958cc4fe48f639f
Ian Wright <ian@authentise.com>
2018-02-01 14:14:08 -0700

    Fix default volumeUnits state
    changed this in a previous commit and missed this part
    
    AE-5788 #time 5m

--- rapidfab/reducers/volumeUnits.js
+++ rapidfab/reducers/volumeUnits.js
@@ -1,6 +1,6 @@
 import Constants, { VOLUME_UNITS } from 'rapidfab/constants';
 
-const volumeUnitsReducer = (state = 'cm3', action) => {
+const volumeUnitsReducer = (state = 'cm', action) => {
   switch (action.type) {
     case Constants.VOLUME_UNITS_CHANGE_CM:
       return VOLUME_UNITS.cm;
