1ef8ce0582772edaec097dd030b2b67daa1802cf
Ian Wright <ian@authentise.com>
2018-03-05 14:27:30 -0700

    Remove LineItem from NewOrder
    cause we want to get rid of this workflow because it causes a billion
    requests that don't have a lot of great error handling
    
    Now, you can only add line-items from the EditOrder page
    
    AE-6031 #time 1h

--- rapidfab/components/records/order/new/NewOrder.jsx
+++ rapidfab/components/records/order/new/NewOrder.jsx
@@ -21,8 +21,6 @@ import Loading from 'rapidfab/components/Loading';
 import NewOrderFormContainer from 'rapidfab/containers/records/order/NewOrderFormContainer';
 import SaveButtonTitle from 'rapidfab/components/SaveButtonTitle';
 
-import LineItem from './LineItem';
-
 const SaveOrderButton = ({ submitting }) => (
   <Button
     disabled={submitting}
@@ -40,12 +38,7 @@ SaveOrderButton.propTypes = { submitting: PropTypes.bool.isRequired };
 const NewOrderComponent = ({
   bannerMessage,
   bannerLink,
-  handleDeleteLineItem,
-  handleLineItemModelChange,
-  handleLineItemChange,
   isUserRestricted,
-  lineItems,
-  onAddLineItem,
   onSubmit,
   submitting,
 }) => {
@@ -78,30 +71,13 @@ const NewOrderComponent = ({
         <Panel header="Order">
           <NewOrderFormContainer />
         </Panel>
-
-        <Panel header="Line Items">
-          {lineItems.map((lineItem, index) => (
-            <LineItem
-              key={lineItem.uri}
-              handleDeleteLineItem={handleDeleteLineItem}
-              handleLineItemModelChange={handleLineItemModelChange}
-              handleLineItemChange={handleLineItemChange}
-              index={index}
-              lineItem={lineItem}
-            />
-          ))}
-
-          <hr />
-
-          <Button bsSize="small" onClick={() => onAddLineItem()}>
-            <Fa name="plus" />
-            {` `}
-            <FormattedMessage id="newLineItem" defaultMessage="New Line Item" />
-          </Button>
-        </Panel>
-        <div className="pull-right" style={{ marginBottom: '3rem' }}>
-          <SaveOrderButton submitting={submitting} />
-        </div>
+        <ButtonToolbar className="clearfix" style={{ marginBottom: '3rem' }}>
+          <div className="pull-right">
+            <Button type="submit" bsStyle="success">
+              Add Line Items
+            </Button>
+          </div>
+        </ButtonToolbar>
       </Form>
     </Grid>
   );
@@ -115,12 +91,7 @@ NewOrderComponent.defaultProps = {
 NewOrderComponent.propTypes = {
   bannerMessage: PropTypes.string,
   bannerLink: PropTypes.string,
-  handleDeleteLineItem: PropTypes.func.isRequired,
-  handleLineItemModelChange: PropTypes.func.isRequired,
-  handleLineItemChange: PropTypes.func.isRequired,
   isUserRestricted: PropTypes.bool.isRequired,
-  lineItems: PropTypes.arrayOf(PropTypes.object).isRequired,
-  onAddLineItem: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
   submitting: PropTypes.bool.isRequired,
 };
@@ -129,14 +100,6 @@ class NewOrder extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      lineItems: [],
-    };
-
-    this.handleDeleteLineItem = this.handleDeleteLineItem.bind(this);
-    this.handleLineItemModelChange = this.handleLineItemModelChange.bind(this);
-    this.handleLineItemChange = this.handleLineItemChange.bind(this);
-    this.onAddLineItem = this.onAddLineItem.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
   }
 
@@ -145,198 +108,52 @@ class NewOrder extends Component {
 
     const { bureau, dispatch, orderForm } = this.props;
 
-    const { lineItems } = this.state;
-
-    const modelPosts = lineItems.map(lineItem => {
-      const modelName = lineItem.itar ? 'na' : lineItem.model.name;
-      const modelUnits =
-        lineItem.modelUnits === 'auto' ? null : lineItem.modelUnits;
-      return dispatch(
-        Actions.Api.hoth.model.post({
-          name: modelName,
-          unit: modelUnits,
-          type: 'stl',
-        })
-      );
-    });
-    Promise.all(modelPosts).then(responses => {
-      const modelLocations = responses.map(response => {
-        const { location, uploadLocation } = response.headers;
-        return { location, uploadLocation };
-      });
-      const updatedLineItems = lineItems.map((lineItem, index) =>
-        Object.assign(
-          {},
-          lineItem,
-          { modelLocation: modelLocations[index].location },
-          { uploadLocation: modelLocations[index].uploadLocation }
-        )
-      );
-
-      updatedLineItems.forEach(lineItem => {
-        const { model, uploadLocation } = lineItem;
-        dispatch(Actions.UploadModel.upload(uploadLocation, model));
-      });
-
-      // Prepare the payload
-      const lineItemsPosts = updatedLineItems.map(lineItem => {
-        const {
-          baseMaterial,
-          itar,
-          modelLocation,
-          supportMaterial,
-          quantity,
-          template,
-          thirdPartyProvider,
-        } = lineItem;
-        const payload = {
-          bureau: bureau.uri,
-          itar,
-          materials: {
-            base: baseMaterial,
-            support: supportMaterial,
-          },
-          model: modelLocation,
-          quantity: parseInt(quantity, 10),
-          template,
-          third_party_provider: thirdPartyProvider,
-        };
-
-        if (itar) delete payload.model;
-        if (!payload.materials.support) delete payload.materials.support;
-        if (!payload.thirdPartyProvider) delete payload.thirdPartyProvider;
-
-        return dispatch(Actions.Api.wyatt['line-item'].post(payload));
-      });
-      Promise.all(lineItemsPosts).then(lineItemResponses => {
-        const lineItemUris = lineItemResponses.map(
-          response => response.payload.uri
-        );
-
-        const orderPayload = {
-          bureau: bureau.uri,
-          business_segment: orderForm.business_segment.value,
-          channel_representative: orderForm.channel_representative.value,
-          currency: orderForm.currency.value,
-          due_date: orderForm.due_date.value
-            ? new Date(orderForm.due_date.value).toISOString()
-            : null,
-          customer_email: orderForm.customer_email.value,
-          customer_name: orderForm.customer_name.value,
-          ip_sensitivity: orderForm.ip_sensitivity.value,
-          line_items: lineItemUris,
-          name: orderForm.name.value,
-          notes: orderForm.notes.value,
-          order_owner: orderForm.order_owner.value,
-          order_type: orderForm.order_type.value,
-          region: orderForm.region.value,
-          sales_representative: orderForm.sales_representative.value,
-          sales_status: orderForm.sales_status.value,
-          shipping: {
-            address: orderForm.shipping.address.value,
-            name: orderForm.shipping.name.value,
-            tracking: orderForm.shipping.tracking.value,
-            uri: orderForm.shipping.uri.value,
-          },
-        };
-
-        Object.keys(orderPayload).forEach(key => {
-          if (orderPayload[key] == null || orderPayload[key] === 'none') {
-            delete orderPayload[key];
-          }
-        });
-
-        dispatch(Actions.Api.wyatt.order.post(orderPayload)).then(response => {
-          const orderUuid = extractUuid(response.payload.uri);
-          window.location = `/#/records/order/${orderUuid}`;
-        });
-      });
-    });
-  }
-
-  onAddLineItem() {
-    const { lineItems } = this.state;
-    const { baseMaterials, supportMaterials, templates } = this.props;
-
-    const initialBaseMaterial = baseMaterials[0] ? baseMaterials[0].uri : null;
-    const initialSupportMaterial = supportMaterials[0]
-      ? supportMaterials[0].uri
-      : null;
-    const initialTemplate = templates[0] ? templates[0].uri : null;
-
-    const initialLineItemState = {
-      baseMaterial: initialBaseMaterial,
-      itar: false,
-      modelUnits: 'auto',
-      quantity: '1',
-      supportMaterial: initialSupportMaterial,
-      template: initialTemplate,
+    const orderPayload = {
+      bureau: bureau.uri,
+      business_segment: orderForm.business_segment.value,
+      channel_representative: orderForm.channel_representative.value,
+      currency: orderForm.currency.value,
+      due_date: orderForm.due_date.value
+        ? new Date(orderForm.due_date.value).toISOString()
+        : null,
+      customer_email: orderForm.customer_email.value,
+      customer_name: orderForm.customer_name.value,
+      ip_sensitivity: orderForm.ip_sensitivity.value,
+      line_items: [],
+      name: orderForm.name.value,
+      notes: orderForm.notes.value,
+      order_owner: orderForm.order_owner.value,
+      order_type: orderForm.order_type.value,
+      region: orderForm.region.value,
+      sales_representative: orderForm.sales_representative.value,
+      sales_status: orderForm.sales_status.value,
+      shipping: {
+        address: orderForm.shipping.address.value,
+        name: orderForm.shipping.name.value,
+        tracking: orderForm.shipping.tracking.value,
+        uri: orderForm.shipping.uri.value,
+      },
     };
 
-    const updatedLineItems = [...lineItems, initialLineItemState];
-    this.setState({ lineItems: updatedLineItems });
-  }
-
-  handleDeleteLineItem(deletedLineItemIndex) {
-    const { lineItems } = this.state;
-
-    const updatedLineItems = lineItems.filter(
-      (lineItem, index) => index !== deletedLineItemIndex
-    );
-
-    this.setState({ lineItems: updatedLineItems });
-  }
-
-  handleLineItemModelChange(lineItemChange) {
-    const { lineItems } = this.state;
-    const { updatedLineItemIndex, model } = lineItemChange;
-
-    const updatedLineItems = lineItems.map((lineItem, index) => {
-      if (index === updatedLineItemIndex) {
-        return Object.assign({}, lineItem, { model });
+    Object.keys(orderPayload).forEach(key => {
+      if (orderPayload[key] == null || orderPayload[key] === 'none') {
+        delete orderPayload[key];
       }
-      return lineItem;
     });
 
-    this.setState({ lineItems: updatedLineItems });
-  }
-
-  handleLineItemChange(lineItemChange) {
-    const { lineItems } = this.state;
-    const { updatedLineItemIndex, name } = lineItemChange;
-    const value = lineItemChange.value === 'none' ? null : lineItemChange.value;
-
-    const updatedLineItems = lineItems.map((lineItem, index) => {
-      if (index === updatedLineItemIndex) {
-        return Object.assign({}, lineItem, { [name]: value });
-      }
-      return lineItem;
+    dispatch(Actions.Api.wyatt.order.post(orderPayload)).then(response => {
+      const orderUuid = extractUuid(response.payload.uri);
+      window.location = `/#/records/order/${orderUuid}`;
     });
-
-    this.setState({ lineItems: updatedLineItems });
   }
 
   render() {
-    const {
-      handleDeleteLineItem,
-      handleLineItemModelChange,
-      handleLineItemChange,
-      onAddLineItem,
-      onSubmit,
-    } = this;
-    const { lineItems } = this.state;
-
     return (
       <NewOrderComponent
         {...this.props}
         bannerLink={this.props.bureau.order_banner.link}
         bannerMessage={this.props.bureau.order_banner.message}
-        handleDeleteLineItem={handleDeleteLineItem}
-        handleLineItemModelChange={handleLineItemModelChange}
-        handleLineItemChange={handleLineItemChange}
-        lineItems={lineItems}
-        onAddLineItem={onAddLineItem}
-        onSubmit={onSubmit}
+        onSubmit={this.onSubmit}
       />
     );
   }
@@ -361,25 +178,13 @@ const mapStateToProps = state => {
   const bureau = Selectors.getBureau(state);
   const isUserRestricted = Selectors.isCurrentUserRestricted(state);
   const orderForm = state.form['record.order'];
-  const materials = Selectors.getMaterials(state);
-  const baseMaterials = materials.filter(material => material.type === 'base');
   const submitting =
-    state.ui.hoth.model.post.fetching ||
-    state.ui.wyatt['line-item'].post.fetching ||
-    state.ui.wyatt.order.post.fetching;
-  const supportMaterials = materials.filter(
-    material => material.type === 'success'
-  );
-  const templates = Selectors.getTemplates(state);
-
+    state.ui.hoth.model.post.fetching || state.ui.wyatt.order.post.fetching;
   return {
-    baseMaterials,
     bureau,
     isUserRestricted,
     orderForm,
     submitting,
-    supportMaterials,
-    templates,
   };
 };
 
