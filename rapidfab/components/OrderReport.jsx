c23e08c0ac637da00e5b8807b8d28c33dbcb6000
Ian Wright <ian@authentise.com>
2018-03-08 14:36:55 -0700

    Add modal to OrderReport
    AE-5967 #time 30m

--- rapidfab/components/OrderReport.jsx
+++ rapidfab/components/OrderReport.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 import { FormattedMessage } from 'rapidfab/i18n';
 
-const OrderReport = ({ reportUrl }) => (
-  <Button bsStyle="primary" bsSize="small" href={reportUrl}>
-    <FormattedMessage
-      id="downloadOrderReport"
-      defaultMessage="Download Order Report"
-    />
-  </Button>
+const OrderReport = ({ handleHide, handleShow, reportUrl, show }) => (
+  <div>
+    <Button bsStyle="primary" bsSize="small" onClick={handleShow}>
+      <FormattedMessage
+        id="downloadOrderReport"
+        defaultMessage="Download Order Report"
+      />
+    </Button>
+    <Modal show={show} onHide={handleHide}>
+      <Modal.Header closeButton />
+      <Modal.Body />
+      <Modal.Footer>
+        <Button bsStyle="success" href={reportUrl}>
+          Download
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  </div>
 );
 
 OrderReport.propTypes = {
+  handleHide: PropTypes.func.isRequired,
+  handleShow: PropTypes.func.isRequired,
   reportUrl: PropTypes.string.isRequired,
+  show: PropTypes.bool.isRequired,
 };
 
 export default OrderReport;
--- rapidfab/containers/OrderReportContainer.jsx
+++ rapidfab/containers/OrderReportContainer.jsx
@@ -2,22 +2,41 @@ import React, { Component } from 'react';
 import CONFIG from 'rapidfab/config';
 import OrderReport from '../components/OrderReport';
 
-const queryParams = (start, end) => `?start=${start}&end=${end}`;
+const generateUrl = (start, end) =>
+  `${CONFIG.HOST.NAUTILUS}/order-report/?start=${start}&end=${end}`;
 
 class OrderReportContainer extends Component {
   constructor(props) {
     super(props);
 
-    this.start = '';
-    this.end = '';
+    this.state = {
+      end: '',
+      start: '',
+      show: false,
+    };
+
+    this.handleHide = this.handleHide.bind(this);
+    this.handleShow = this.handleShow.bind(this);
+  }
+
+  handleHide() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
   }
 
   render() {
-    const reportUrl = `${CONFIG.HOST.NAUTILUS}/order-report/${queryParams(
-      this.start,
-      this.end
-    )}`;
-    return <OrderReport reportUrl={reportUrl} />;
+    const { end, start, show } = this.state;
+    return (
+      <OrderReport
+        handleHide={this.handleHide}
+        handleShow={this.handleShow}
+        reportUrl={generateUrl(start, end)}
+        show={show}
+      />
+    );
   }
 }
 
