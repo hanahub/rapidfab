6e658973143bf3e8ca399be66e8ea3a1765a1d73
Ian Wright <ian@authentise.com>
2018-03-08 14:14:39 -0700

    Add reportUrl to OrderReport
    does not have parameters yet, will build next
    
    AE-5967 #time 30m

--- rapidfab/components/OrderReport.jsx
+++ rapidfab/components/OrderReport.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 import { FormattedMessage } from 'rapidfab/i18n';
 
-const OrderReport = () => (
-  <Button bsStyle="primary" bsSize="small" onClick={() => {}}>
+const OrderReport = ({ reportUrl }) => (
+  <Button bsStyle="primary" bsSize="small" href={reportUrl}>
     <FormattedMessage
       id="downloadOrderReport"
       defaultMessage="Download Order Report"
     />
   </Button>
 );
+
+OrderReport.propTypes = {
+  reportUrl: PropTypes.string.isRequired,
+};
 
 export default OrderReport;
--- rapidfab/containers/OrderReportContainer.jsx
+++ rapidfab/containers/OrderReportContainer.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import CONFIG from 'rapidfab/config';
 import OrderReport from '../components/OrderReport';
 
+const queryParams = (start, end) => `?start=${start}&end=${end}`;
+
 class OrderReportContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.start = '';
+    this.end = '';
+  }
+
   render() {
-    return <OrderReport />;
+    const reportUrl = `${CONFIG.HOST.NAUTILUS}/order-report/${queryParams(
+      this.start,
+      this.end
+    )}`;
+    return <OrderReport reportUrl={reportUrl} />;
   }
 }
 
