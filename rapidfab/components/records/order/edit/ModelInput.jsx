6f62bb722d3c6d2f6dda2d2288164730a9f84bf6
Ian Wright <ian@authentise.com>
2018-03-12 11:17:13 -0600

    Add custom styling to ModelInput
    cause the default browser file input sucks
    
    also, we need to display the file name based on form state, so that it
    disappears when we reset the form
    
    AE-5839 #time 30m

--- rapidfab/components/records/order/edit/ModelInput.jsx
+++ rapidfab/components/records/order/edit/ModelInput.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
-import { ControlLabel, FormControl, FormGroup } from 'react-bootstrap';
+import { ControlLabel, FormControl, FormGroup, Label } from 'react-bootstrap';
 
-const ModelInput = ({ handleFileChange }) => (
+const ModelInput = ({ handleFileChange, name }) => (
   <FormGroup>
-    <ControlLabel>
-      <FormattedMessage id="field.model" defaultMessage="Model" />:
+    <ControlLabel htmlFor="modelInput" className="btn btn-default">
+      <FormattedMessage
+        id="chooseModelFile"
+        defaultMessage="Choose Model File"
+      />
     </ControlLabel>
+    <Label bsStyle="success" style={{ marginLeft: '1rem' }}>
+      {name}
+    </Label>
     <FormControl
+      id="modelInput"
       name="model"
       type="file"
       accept=".stl"
       required
       onChange={handleFileChange}
+      style={{ opacity: '0' }}
     />
   </FormGroup>
 );
 
+ModelInput.defaultProps = { name: '' };
+
 ModelInput.propTypes = {
   handleFileChange: PropTypes.func.isRequired,
+  name: PropTypes.string,
 };
 
 export default ModelInput;
