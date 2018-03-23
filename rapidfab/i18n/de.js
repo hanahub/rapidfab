283d68fac5d4cee83cb5118b1ca6c90d28b6e415
Ian Wright <ian@authentise.com>
2018-03-23 12:58:39 -0600

    Add i18n data for materialCost
    AE-6096 #time 15m

--- rapidfab/i18n/de.js
+++ rapidfab/i18n/de.js
@@ -202,6 +202,7 @@ export default {
   "estimates.bureauCost": "Bureau Cost",
   "estimates.cost": "Kosten",
   "estimates.endTime": "Endzeit",
+  "estimates.materialCost": "Material Cost",
   "estimates.materialUsed": "Verwendetes Material",
   "estimates.postProcessingCost": "Nachbearbeitungskosten",
   "estimates.postProcessingTime": "Nachbearbeitungszeit",
