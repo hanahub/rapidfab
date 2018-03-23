21a2c2812e917e372f5b56d65c658114cf5df9c7
Ian Wright <ian@authentise.com>
2018-03-23 11:28:45 -0600

    Fix CostTooltip
    functions =D
    
    AE-6079 #time 15m

--- rapidfab/components/CostTooltip.jsx
+++ rapidfab/components/CostTooltip.jsx
@@ -3,7 +3,7 @@ import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import Fa from 'react-fontawesome';
 import { FormattedMessage } from 'rapidfab/i18n';
 
-const CostTooltip = (
+const CostTooltip = () => (
   <OverlayTrigger
     placement="top"
     overlay={
