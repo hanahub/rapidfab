b4e847f7248dcb1fda9e62298943dd2c0363d29f
Ian Wright <ian@authentise.com>
2018-05-08 12:32:25 -0600

    Create NetfabbImportButton
    AE-6214 #time 30m

---
+++ rapidfab/components/NetfabbImportButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import Actions from 'rapidfab/actions';
+
+const NetfabbImportButton = ({ dispatch, lineItemUri }) => (
+  <Button
+    block
+    onClick={() => {
+      dispatch(Actions.Api.wyatt.netfabb.get({ line_item: lineItemUri }));
+    }}
+  >
+    Import to Netfabb
+  </Button>
+);
+
+NetfabbImportButton.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  lineItemUri: PropTypes.string.isRequired,
+};
+
+export default connect()(NetfabbImportButton);
