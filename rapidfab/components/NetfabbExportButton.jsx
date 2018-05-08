7b0b1f3132257d6c7b62a3befa46b6213ca9510c
Ian Wright <ian@authentise.com>
2018-05-08 11:20:23 -0600

    Create NetfabbExportButton
    AE-6210 #time 30m

---
+++ rapidfab/components/NetfabbExportButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Button } from 'react-bootstrap';
+import Actions from 'rapidfab/actions';
+
+const NetfabbExportButton = ({ dispatch, lineItemUri }) => (
+  <Button
+    block
+    onClick={() => {
+      dispatch(Actions.Api.wyatt.netfabb.post({ line_item: lineItemUri }));
+    }}
+  >
+    Export to Netfabb
+  </Button>
+);
+
+NetfabbExportButton.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  lineItemUri: PropTypes.string.isRequired,
+};
+
+export default connect()(NetfabbExportButton);
